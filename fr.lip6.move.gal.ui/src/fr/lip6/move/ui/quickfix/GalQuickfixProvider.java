dd506215f10519a298e18fe371e39c0ac244d41b
Yao Kaoudio <etu@upmc.fr>
2012-04-18 16:17:55 +0000

    QuickFix : - Corrige les noms de variables (ou liste ou arrays) similaires - Les noms corrigés contiennent des lettres aléatoires, de 3 caractères

--- fr.lip6.move.gal.ui/src/fr/lip6/move/ui/quickfix/GalQuickfixProvider.java
+++ fr.lip6.move.gal.ui/src/fr/lip6/move/ui/quickfix/GalQuickfixProvider.java
@@ -1,19 +1,84 @@
 
 package fr.lip6.move.ui.quickfix;
 
-import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
+import java.util.Random;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
+import org.eclipse.xtext.ui.editor.model.edit.ISemanticModification;
+import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
+import org.eclipse.xtext.ui.editor.quickfix.Fix;
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.validation.Issue;
+
+import fr.lip6.move.gal.ArrayPrefix;
+import fr.lip6.move.gal.List;
+import fr.lip6.move.gal.Variable;
+import fr.lip6.move.validation.GalJavaValidator;
+
+/**
+ * Quick Fixer
+ * @author steph
+ *
+ */
 public class GalQuickfixProvider extends DefaultQuickfixProvider {
 
-//	@Fix(MyJavaValidator.INVALID_NAME)
-//	public void capitalizeName(final Issue issue, IssueResolutionAcceptor acceptor) {
-//		acceptor.accept(issue, "Capitalize name", "Capitalize the name.", "upcase.png", new IModification() {
-//			public void apply(IModificationContext context) throws BadLocationException {
-//				IXtextDocument xtextDocument = context.getXtextDocument();
-//				String firstLetter = xtextDocument.get(issue.getOffset(), 1);
-//				xtextDocument.replace(issue.getOffset(), 1, firstLetter.toUpperCase());
-//			}
-//		});
-//	}
+	
+	/**
+	 * Cette méthode renvoie une chaine aléatoire de longueur length
+	 * et met en majuscule le premier caractère
+	 * @param length
+	 * @return
+	 */
+	public String generateRandomString(int length)
+	{
+		StringBuilder sb = new StringBuilder(length);
+		char chars[] = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+		Random r = new Random();
+		
+		for(int i=0; i<length; i++)
+		{
+			sb.append(chars[r.nextInt(chars.length)]);
+		}
+		// Met en majuscule le premier caractère
+		sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+		return sb.toString();
+	}
+	
+	/** Génère un nouveau nom à partir de l'ancien */
+	public String generateNewNameFromOld(String oldName) {
+		
+		return oldName + "_" + generateRandomString(3);
+	}
+	
+	
+	@Fix(GalJavaValidator.GAL_ERROR_NAME_EXISTS)
+	public void suggestOtherVariableName(final Issue issue, IssueResolutionAcceptor acceptor)
+	{
+		acceptor.accept(issue, "Change name", "Choose other name", null, new ISemanticModification() {
+			
+			@Override
+			public void apply(EObject element, IModificationContext context)
+					throws Exception 
+			{
+				if(element instanceof Variable) 
+				{
+					Variable var = (Variable) element ; 
+					var.setName(generateNewNameFromOld(var.getName()));
+				}
+				else if(element instanceof ArrayPrefix)
+				{
+					ArrayPrefix array = (ArrayPrefix) element ; 
+					array.setName(generateNewNameFromOld(array.getName())) ; 
+				}
+				else if(element instanceof List)
+				{
+					List l = (List) element ; 
+					l.setName(generateNewNameFromOld(l.getName()));
+				}
+			}
 
+			
+		});
+	}
 }
