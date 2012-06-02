c13b4ebc5b25770d2835d0378e584c293820f905
Yao Kaoudio <etu@upmc.fr>
2012-06-02 00:30:58 +0000

    QuickFix: - Fixed the bug of initializing an empty array with QuickFix (using Factory) - Added 1 more quickfix for each existing one : now, we can choose to change the array size, instead of adding/removing items -Generated names (when conflits) are now UNIQUE. Random letters are never used anymore, it simply add numbers just after the name, which increments theirselves ;-)

--- fr.lip6.move.gal.ui/src/fr/lip6/move/ui/quickfix/GalQuickfixProvider.java
+++ fr.lip6.move.gal.ui/src/fr/lip6/move/ui/quickfix/GalQuickfixProvider.java
@@ -1,7 +1,7 @@
 
 package fr.lip6.move.ui.quickfix;
 
-import java.util.Random;
+import java.util.HashMap;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
@@ -12,10 +12,15 @@ import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
 
 import fr.lip6.move.gal.ArrayPrefix;
+import fr.lip6.move.gal.GalFactory;
 import fr.lip6.move.gal.List;
 import fr.lip6.move.gal.Transition;
 import fr.lip6.move.gal.Variable;
 import fr.lip6.move.validation.GalJavaValidator;
+import fr.lip6.move.gal.InitValues;
+
+
+
 
 /**
  * Quick Fixer
@@ -25,42 +30,53 @@ import fr.lip6.move.validation.GalJavaValidator;
 public class GalQuickfixProvider extends DefaultQuickfixProvider {
 
 	
+	// Cette table de hashage associera un "nom" à un "compteur", qui sera incrementé
+	// a chaque conflit de nom
+	private HashMap<String, Integer> compteurMap = new HashMap<String, Integer>();
+	
+	
+	
 	/**
-	 * Cette méthode renvoie une chaine aléatoire de longueur length
-	 * et met en majuscule le premier caractère
-	 * @param length
-	 * @return
+	 * Renvoie un nouveau nom à partir de l'ancien. Le nom est généralement suivi d'un chiffre
+	 * et est unique dans le système
 	 */
-	public String generateRandomString(int length)
+	private String generateNewNameFromOld(String oldName)
 	{
-		StringBuilder sb = new StringBuilder(length);
-		char chars[] = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
-		Random r = new Random();
+		String retour = "";
+		int cpt = 1 ;
 		
-		for(int i=0; i<length; i++)
+		// Si c'est la première fois qu'on fais un quickFix
+		if(! compteurMap.containsKey(oldName))
 		{
-			sb.append(chars[r.nextInt(chars.length)]);
+			compteurMap.put(oldName, 1);
+			return oldName + 1 ; // Nom de la variable + 1
 		}
-		// Met en majuscule le premier caractère
-		sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
-		return sb.toString();
+		 
+		// Sinon : boucler jusqu'a tomber sur un nom inexistant
+		while(true)
+		{
+			cpt = cpt + compteurMap.get(oldName) ; 
+			// Si ce nouveau nom existe
+			if(GalJavaValidator.galElementsName.containsKey(oldName + cpt))
+			{
+				cpt ++ ; 
+				continue ; 
+			}
+			// Ici le nom n'est pas encore utilisé.
+			retour = oldName + cpt  ;
+			compteurMap.put(oldName, cpt);
+			return retour ; 
+		}
 	}
 	
-	/** Génère un nouveau nom à partir de l'ancien */
-	public String generateNewNameFromOld(String oldName) {
-		
-		return oldName + "_" + generateRandomString(3);
-	}
+	
 	
 	
 	
 	
 	
 	/**
-	 * Corrige le nom existant en lui rajoutant 3
-	 * lettres aléatoires
-	 * @param issue
-	 * @param acceptor
+	 * Corrige le nom existant en lui rajoutant un chiffre devant.
 	 */
 	@Fix(GalJavaValidator.GAL_ERROR_NAME_EXISTS)
 	public void suggestOtherVariableName(final Issue issue, IssueResolutionAcceptor acceptor)
@@ -106,11 +122,9 @@ public class GalQuickfixProvider extends DefaultQuickfixProvider {
 	
 	/**
 	 * Complète les elements manquant d'un tableau
-	 * @param issue
-	 * @param acceptor
 	 */
 	@Fix(GalJavaValidator.GAL_ERROR_MISSING_ELEMENTS)
-	public void completeMissingElements(final Issue issue, IssueResolutionAcceptor acceptor)
+	public void missing_CompleteElements(final Issue issue, IssueResolutionAcceptor acceptor)
 	{
 		acceptor.accept(issue, "Complete missing elements", "Complete missing elements with zeros", null, new ISemanticModification() {
 			
@@ -122,9 +136,29 @@ public class GalQuickfixProvider extends DefaultQuickfixProvider {
 				{
 					ArrayPrefix array = (ArrayPrefix) element ; 
 					int nbElementsToAdd = GalJavaValidator.arrayMissingValues.get(array.getName()) ; 
-					for(int i=0; i<nbElementsToAdd; i++)
+					
+					// Si null 
+					if(array.getValues() == null)
 					{
-						array.getValues().getValues().add(0);
+						GalFactory factory = GalFactory.eINSTANCE;
+						InitValues v = factory.createInitValues();
+						
+						array.setValues(v) ;
+						
+						for(int i=0; i<nbElementsToAdd; i++)
+						{
+							array.getValues().getValues().add(0);
+						}
+					}
+					else // tableau deja initialisé, mais pas en entier
+					{
+						if(nbElementsToAdd > 0)
+						{
+							for(int i=0; i<nbElementsToAdd; i++)
+							{
+								array.getValues().getValues().add(0);
+							}
+						}
 					}
 				}
 				else 
@@ -138,4 +172,115 @@ public class GalQuickfixProvider extends DefaultQuickfixProvider {
 		});
 	}
 	
+	
+	
+	
+	/**
+	 * Change la taille au lieu de rajouter les elements au tableauu
+	 */
+	@Fix(GalJavaValidator.GAL_ERROR_MISSING_ELEMENTS)
+	public void missing_ChangeArraySize(final Issue issue, IssueResolutionAcceptor acceptor)
+	{
+		acceptor.accept(issue, "Change array size", "Change array size", null, new ISemanticModification() {
+			
+			@Override
+			public void apply(EObject element, IModificationContext context)
+					throws Exception 
+			{
+				if(element instanceof ArrayPrefix)
+				{
+					ArrayPrefix array = (ArrayPrefix) element ; 
+					
+					// Si null 
+					if(array.getValues() == null)
+					{
+						array.setSize(0);
+					}
+					else
+					{
+						array.setSize(array.getValues().getValues().size());
+					}
+				}
+				else 
+				{
+					System.err.println("Not yet implemented");
+					System.out.println(element.getClass().getName());
+				}
+			}
+		});
+	}
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Supprime les elements en trop d'un tableau
+	 */
+	@Fix(GalJavaValidator.GAL_ERROR_EXCESS_ITEMS)
+	public void excess_RemoveItems(final Issue issue, IssueResolutionAcceptor acceptor)
+	{
+		acceptor.accept(issue, "Remove excess items", "Remove the excess last items", null, new ISemanticModification() {
+			
+			@Override
+			public void apply(EObject element, IModificationContext context)
+					throws Exception 
+			{
+				if(element instanceof ArrayPrefix)
+				{
+					ArrayPrefix array = (ArrayPrefix) element ; 
+					int nbElementsToRemove =  -GalJavaValidator.arrayMissingValues.get(array.getName()) ; 
+					int taille ; 
+					for(int i=0; i<nbElementsToRemove; i++)
+					{
+						taille = array.getValues().getValues().size();
+						array.getValues().getValues().remove(taille-1);
+					}
+				}
+				else 
+				{
+					System.err.println("Not yet implemented");
+					System.out.println(element.getClass().getName());
+				}
+			}
+		});
+	}
+
+	
+	
+	
+	/**
+	 * Dans le cas d'un surplus, change la taille du tableau, au lieu de 
+	 * supprimer les elements en trop
+	 */
+	@Fix(GalJavaValidator.GAL_ERROR_EXCESS_ITEMS)
+	public void excess_ChangeArraySize(final Issue issue, IssueResolutionAcceptor acceptor)
+	{
+		acceptor.accept(issue, "Change array size", 
+				               "Change array size, to keep the excess initialized items", 
+				                null, new ISemanticModification() {
+			
+			@Override
+			public void apply(EObject element, IModificationContext context)
+					throws Exception 
+			{
+				if(element instanceof ArrayPrefix)
+				{
+					ArrayPrefix array = (ArrayPrefix) element ; 
+					int nbElements =  array.getSize() - GalJavaValidator.arrayMissingValues.get(array.getName()) ;
+
+					array.setSize(nbElements);
+				}
+				else 
+				{
+					System.err.println("Not yet implemented");
+					System.out.println(element.getClass().getName());
+				}
+			}
+
+			
+		});
+	}
 }
