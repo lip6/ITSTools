d7f0ca4662f755dc6a364cd186fe91e113ad0157
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2013-02-22 13:18:15 +0000

    the resource adapter from EMF to Xtext

---
+++ fr.lip6.move.coloane.emf.xtext.model.resource/src/fr/lip6/move/coloane/emf/xtext/model/resource/ITSQualifiedNameProvider.java
@@ -0,0 +1,7 @@
+package fr.lip6.move.coloane.emf.xtext.model.resource;
+
+import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
+
+public class ITSQualifiedNameProvider extends DefaultDeclarativeQualifiedNameProvider {
+
+}
---
+++ fr.lip6.move.coloane.emf.xtext.model.resource/src/fr/lip6/move/coloane/emf/xtext/model/resource/ITSResourceDescriptionStrategy.java
@@ -0,0 +1,7 @@
+package fr.lip6.move.coloane.emf.xtext.model.resource;
+
+import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
+
+public class ITSResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
+
+}
