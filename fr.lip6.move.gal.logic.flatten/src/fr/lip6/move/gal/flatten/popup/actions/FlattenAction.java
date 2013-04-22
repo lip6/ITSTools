47d8f1ad07bd472dc1bdb04b31275c7cced076a2
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2013-04-22 19:06:39 +0000

    tying the action to resolving the resources from the XtextEditor. Inelegant, and based on the ui context, but at least crossrefs are correctly loaded this way.

--- fr.lip6.move.gal.logic.flatten/src/fr/lip6/move/gal/flatten/popup/actions/FlattenAction.java
+++ fr.lip6.move.gal.logic.flatten/src/fr/lip6/move/gal/flatten/popup/actions/FlattenAction.java
@@ -2,17 +2,33 @@ package fr.lip6.move.gal.flatten.popup.actions;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.IDocumentExtension;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.editor.utils.EditorUtils;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
 import fr.lip6.move.gal.logic.Properties;
 import fr.lip6.move.gal.simplify.LogicSimplifier;
@@ -41,33 +57,60 @@ public class FlattenAction implements IObjectActionDelegate {
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
+		
 		for (IFile file : files) {
 			if (file != null) {
+				IWorkbenchPage page = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow().getActivePage();
+				try {
+					IDE.openEditor(page, file );
+				} catch (PartInitException e1) {
+					e1.printStackTrace();
+				}
+
+//				IDocumentExtension.op
+				XtextEditor editor = EditorUtils.getActiveXtextEditor();
+				IXtextDocument myDocument = editor.getDocument();
+				Properties props =  myDocument.readOnly(
+						new IUnitOfWork<Properties, XtextResource>(){
+							public Properties exec(XtextResource resource) {
+								Properties type = (Properties)resource.getContents().get(0);
+								return EcoreUtil.copy(type);
+							}
+						});
+
 				
-				Properties p = SerializationUtil.fileToProperties(file.getRawLocationURI().getPath());
+//				Properties p = SerializationUtil.fileToProperties(file);
 
 				try {
 
 					
 
-					LogicSimplifier.simplify(p);
+					LogicSimplifier.simplify(props);
 
-					String path = file.getRawLocationURI().getPath().split(".prop")[0];
-					String outpath =  path+".flat.prop";
-
-					//String outpath =  file.getRawLocationURI().getPath()+".flat.gal";
-
-					FileOutputStream out = new FileOutputStream(new File(outpath));
-					out.write(0);
-					out.close();
-					SerializationUtil.systemToFile(p,outpath);
+//					String path = file.getRawLocationURI().getPath().split(".prop")[0];
+					IContainer outFold = file.getParent();
+					IPath newpath = file.getProjectRelativePath();
+					newpath = newpath.removeLastSegments(1);
+					newpath = newpath.append(file.getLocation().lastSegment().replace(".prop", ".flat.prop"));
+					IFile outfile = outFold.getFile(newpath);
+					SerializationUtil.systemToFile(props,outfile);
 					java.lang.System.err.println("On a passe la serialization");
+					
+//					//String outpath =  file.getRawLocationURI().getPath()+".flat.gal";
+//
+//					FileOutputStream out = new FileOutputStream(new File(outpath));
+//					out.write(0);
+//					out.close();
+//					SerializationUtil.systemToFile(p,outpath);
+//					java.lang.System.err.println("On a passe la serialization");
 
 				} catch (Exception e) {
 					MessageDialog.openWarning(
 							shell,
 							"Flatten",
 							"Flatten GAL operation raised an exception " + e.getMessage());
+					e.printStackTrace();
 					return;
 
 				}
