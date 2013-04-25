eb6da8b012d1fd2a913a08c25f143c11bea2a3cc
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2013-04-25 17:13:07 +0000

    refactorings, added implicit variable stuff (beta)

--- fr.lip6.move.gal/src/fr/lip6/move/gal/instantiate/Simplifier.java
+++ fr.lip6.move.gal/src/fr/lip6/move/gal/instantiate/Simplifier.java
@@ -2,9 +2,16 @@ package fr.lip6.move.gal.instantiate;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
 import fr.lip6.move.gal.Actions;
@@ -31,6 +38,17 @@ import fr.lip6.move.gal.VariableRef;
 public class Simplifier {
 
 	public static System simplify(System s) {
+		simplifyTypeParameters(s);
+
+		simplifyConstantOperations(s);
+
+		simplifyPetriStyleAssignments(s);
+
+		simplifyConstantVariables(s);
+		return s;
+	}
+
+	private static void simplifyTypeParameters(System s) {
 		for (Variable var : s.getVariables()) {
 			var.setValue(simplify(var.getValue()));
 		}
@@ -39,8 +57,9 @@ public class Simplifier {
 				EcoreUtil.replace(val,simplify(val));
 			}
 		}
+	}
 
-
+	private static void simplifyConstantOperations(System s) {
 		List<Transition> todel = new ArrayList<Transition>();
 		for (Transition t : s.getTransitions()) {
 			BooleanExpression newg = simplify(t.getGuard());
@@ -65,15 +84,80 @@ public class Simplifier {
 			i++;
 		}
 		java.lang.System.err.println("Removed "+i + " false transitions.");
-
-		simplifyPetriStyleAssignments(s);
-
-		return s;
+		
 	}
 
-	public static void simplifyPetriStyleAssignments(System s) {
+	private static void simplifyConstantVariables(System s) {
 
+		Set<Variable> constvars = new HashSet<Variable>(s.getVariables());
+		Map<ArrayPrefix, Set<Integer>> constantArrs = new HashMap<ArrayPrefix, Set<Integer>>();
+		for (ArrayPrefix ap : s.getArrays()) {
+			Set<Integer> vals = new HashSet<Integer>();
+			for (int i = 0 ; i < ap.getSize(); i++) {
+				vals.add(i);
+			}
+			constantArrs.put(ap, vals);
+		}
+		// compute constant vars
+		for (TreeIterator<EObject> it = s.eAllContents() ; it.hasNext() ; ) {
+			EObject obj = it.next();
+			if (obj instanceof Assignment) {
+				Assignment ass = (Assignment) obj;
+				VarAccess lhs = ass.getLeft();
+				if (lhs instanceof VariableRef) {
+					VariableRef va = (VariableRef) lhs;
+					constvars.remove(va.getReferencedVar());
+				} else if (lhs instanceof ArrayVarAccess) {
+					ArrayVarAccess av = (ArrayVarAccess) lhs;
+					constantArrs.get(av.getPrefix()).remove(((Constant) av.getIndex()).getValue());
+				}
+			}
+		}
 
+		StringBuilder sb = new StringBuilder();
+		int sum = constvars.size();
+		for (Variable var : constvars) {
+			sb.append(var.getName()+",");
+		}
+		for (Entry<ArrayPrefix, Set<Integer>> e : constantArrs.entrySet()) {
+			sum += e.getValue().size();
+			for (int val : e.getValue()) {
+				sb.append(e.getKey().getName() + "[" + val + "],");
+			}
+		}
+		if (sum != 0) {
+			java.lang.System.err.println("Found a total of " + sum + " constant array cells/variables :\n "+sb.toString() );
+		} else {
+			return;
+		}
+		
+		int totalexpr = 0;
+		// Substitute constants in guards and assignments
+		for (TreeIterator<EObject> it = s.eAllContents() ; it.hasNext() ; ) {
+			EObject obj = it.next();
+			if (obj instanceof VariableRef) {
+				VariableRef va = (VariableRef) obj;
+				if (constvars.contains(va.getReferencedVar())) {
+					EcoreUtil.replace(va, EcoreUtil.copy(va.getReferencedVar().getValue()));
+					totalexpr++;
+				} 
+			} else if (obj instanceof ArrayVarAccess) {
+				ArrayVarAccess av = (ArrayVarAccess) obj;
+				if ( constantArrs.get(av.getPrefix()).contains(((Constant) av.getIndex()).getValue()) ) {
+					EcoreUtil.replace(av, EcoreUtil.copy(av.getPrefix().getValues().getValues().get(((Constant) av.getIndex()).getValue())));						
+					totalexpr++;
+				}
+			}
+		}
+		if (totalexpr != 0) {
+			java.lang.System.err.println(" Simplified "+ totalexpr + " expressions due to constant valuations.");
+			simplifyConstantOperations(s);
+		}
+	}
+
+	public static boolean simplifyPetriStyleAssignments(System s) {
+
+		boolean isPetriStyle = true;
 		//simplify redundant assignments :
 		// suppose we have both : x = x + 1; and  x = x-1; without reading or writing to x in between.
 		// typically produced by test arc style petri nets
@@ -119,8 +203,12 @@ public class Simplifier {
 						} // if same lhs
 					}  // for j
 				}  // for i
-			} // if petri style tr
+			}  else { // if petri style tr
+				isPetriStyle = false;
+			}
 		} // for tr
+
+		return isPetriStyle;
 	}
 
 	private static boolean isPetriStyle(Transition tr) {
@@ -130,7 +218,7 @@ public class Simplifier {
 					&& ( 
 							( 		/// tab[cte] case
 									((Assignment) a).getLeft() instanceof ArrayVarAccess 
-									&& ( ((ArrayVarAccess) ((Assignment) a).getLeft()).getIndex() instanceof Constant  )
+									//&& ( ((ArrayVarAccess) ((Assignment) a).getLeft()).getIndex() instanceof Constant  )
 									)
 									||  
 									(       /// plain old variable
@@ -285,4 +373,112 @@ public class Simplifier {
 		return expr;
 	} 
 
+	
+	public static void simplifyImplicitVariables (System system) {
+		
+		Map<Transition,Boolean> tguards= new HashMap<Transition, Boolean>();
+				
+		Map<ArrayPrefix, Map<Transition, List<IntExpression>>> presets = new HashMap<ArrayPrefix, Map<Transition,List<IntExpression>>> ();
+		Map<ArrayPrefix, Map<Transition, List<IntExpression>>> postsets = new HashMap<ArrayPrefix, Map<Transition,List<IntExpression>>> ();
+		
+		for (Transition t : system.getTransitions()) {
+			tguards.put(t, false);
+			for (TreeIterator<EObject> it = t.getGuard().eAllContents(); it.hasNext();) {
+				EObject obj = it.next();
+				if (obj instanceof Comparison) {
+					Comparison cmp = (Comparison) obj;
+					if (!(cmp.getLeft() instanceof ArrayVarAccess || cmp.getRight() instanceof ArrayVarAccess)) {
+						tguards.put(t, true);
+						break;
+					}
+				}
+			}
+			for (Actions a : t.getActions()) {
+				if (a instanceof Assignment) {
+					Assignment ass = (Assignment) a;
+					VarAccess lhs = ass.getLeft();
+					BinaryIntExpression rhs = (BinaryIntExpression)ass.getRight();
+					if (lhs instanceof ArrayVarAccess && rhs instanceof BinaryIntExpression && rhs.getRight() instanceof Constant ) {
+						ArrayVarAccess av = (ArrayVarAccess) lhs ;
+						int val = ((Constant) rhs.getRight()).getValue();
+						if (val != 1) {
+							java.lang.System.err.println("Problem with variable value not 1");
+						}
+						if (rhs.getOp().equals("+")) {
+							addToSet (av.getPrefix(), t, av.getIndex(), presets);
+						} else {
+							addToSet (av.getPrefix(), t, av.getIndex(), postsets);							
+						}
+					}
+				}
+			}
+		}
+		
+		// basically we now have the colored PT structure loaded up: it is accessible through places + guards
+		// build a reverse index from t to p
+		Map<Transition, List<ArrayPrefix>> tpreset = new HashMap<Transition, List<ArrayPrefix>>();
+		Map<Transition, List<ArrayPrefix>> tpostet = new HashMap<Transition, List<ArrayPrefix>>();
+
+		for (Entry<ArrayPrefix, Map<Transition, List<IntExpression>>> entry : presets.entrySet()) {
+			for (Transition tr : entry.getValue().keySet()) {
+				List<ArrayPrefix> list = tpostet.get(tr);
+				if (list == null) {
+					list = new ArrayList<ArrayPrefix>();
+					tpostet.put(tr, list);
+				}
+				list.add(entry.getKey());
+			}
+		}
+		for (Entry<ArrayPrefix, Map<Transition, List<IntExpression>>> entry : postsets.entrySet()) {
+			for (Transition tr : entry.getValue().keySet()) {
+				List<ArrayPrefix> list = tpreset.get(tr);
+				if (list == null) {
+					list = new ArrayList<ArrayPrefix>();
+					tpreset.put(tr, list);
+				}
+				list.add(entry.getKey());
+			}
+		}
+		
+		int nbagglo= 0;
+		// look for places with a single preset and post set
+		for (ArrayPrefix ap : system.getArrays()) {
+			Map<Transition, List<IntExpression>> pre = presets.get(ap);
+			Map<Transition, List<IntExpression>> post = postsets.get(ap);
+			if (pre != null && post != null && pre.size() == 1 && post.size() == 1) {
+				Entry<Transition, List<IntExpression>> pret = pre.entrySet().iterator().next();
+				Entry<Transition, List<IntExpression>> postt = post.entrySet().iterator().next();
+				if (! tguards.get(pret.getKey()) && ! tguards.get(postt.getKey()) ) {
+					// so we have a single unguarded transitions on both sides
+					// check that we have a single token flow
+					if ( pret.getValue().size() == 1 && postt.getValue().size() == 1) {
+					
+						// check that F (postset) is not enabled by other than current p
+						if ( tpreset.get(postt.getKey()).size() == 1) {
+							java.lang.System.err.println("This place :" + ap.getName() + " is candidate for agglomeration.");
+							nbagglo++;
+						}
+					}
+				}
+			}
+		}
+		java.lang.System.err.println("Total places to agglo " + nbagglo);
+		
+		
+		
+	}
+
+	private static void addToSet(ArrayPrefix prefix, Transition t, IntExpression index, Map<ArrayPrefix, Map<Transition,List<IntExpression> >> presets) {
+		Map<Transition, List<IntExpression>> transMap = presets.get(prefix);
+		if (transMap == null) {
+			transMap = new HashMap<Transition, List<IntExpression>> ();
+			presets.put(prefix, transMap);
+		}
+		List<IntExpression> list = transMap.get(t);
+		if (list == null) {
+			list = new ArrayList<IntExpression>();
+			transMap.put(t, list);
+		}
+		list.add(index);
+	}
 }
