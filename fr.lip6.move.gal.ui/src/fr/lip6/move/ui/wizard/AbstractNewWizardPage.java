8b27192d37122382eff5cac644d4f8c1c4f07a08
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2013-05-21 11:06:42 +0000

    code copied in with very slight modifs from ITS editor project, to have a new GAL file wizard

---
+++ fr.lip6.move.gal.ui/src/fr/lip6/move/ui/wizard/AbstractNewWizardPage.java
@@ -0,0 +1,256 @@
+/**
+ * Copyright (c) 2006-2010 MoVe - Laboratoire d'Informatique de Paris 6 (LIP6).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Jean-Baptiste VORON (LIP6) - Project Head / Initial contributor
+ *   Clément DÉMOULINS (LIP6) - Project Manager
+ *   Yann THIERRY-MIEG (LIP6)
+ *
+ * Official contacts:
+ *   coloane@lip6.fr
+ *   http://coloane.lip6.fr
+ */
+package fr.lip6.move.ui.wizard;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+
+/**
+ * The "New" wizard page allows setting the container for the new file as well
+ * as the file name. The page will only accept file name without the extension
+ * OR with the extension that matches the expected one
+ * (fr.lip6.move.coloane.its).
+ */
+public abstract class AbstractNewWizardPage extends WizardPage {
+	private Text containerText;
+
+	private Text fileText;
+
+	private ISelection selection;
+
+	/**
+	 * Constructor for ITSNewWizardPage.
+	 * 
+	 * @param selection
+	 *            the workspace item currently selected (should be a folder)
+	 */
+	public AbstractNewWizardPage(ISelection selection) {
+		super("wizardPage");
+		setTitle(getWizardTitle());
+		setDescription(getWizardDescription());
+		this.selection = selection;
+	}
+
+	/**
+	 * Implement to provide the wizard title.
+	 * 
+	 * @return a name to put in the wizard's title bar
+	 */
+	protected abstract String getWizardTitle();
+
+	/**
+	 * Implement to provide a helping text on the role of this wizard.
+	 * 
+	 * @return a wizard description for end users.
+	 */
+	protected abstract String getWizardDescription();
+
+	/**
+	 * Implement to provide the file extension this wizard manipulates.
+	 * 
+	 * @return a file extension
+	 */
+	protected abstract String getWizardFileExtension();
+
+	/**
+	 * Defines the contents (SWT widgets) of this wizard. Defaults to empty
+	 * wizard.
+	 * 
+	 * @param container
+	 *            the parent container.
+	 */
+	protected void addContent(Composite container) {
+	};
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see IDialogPage#createControl(Composite)
+	 */
+	public final void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 3;
+		layout.verticalSpacing = 9;
+		Label label = new Label(container, SWT.NULL);
+		label.setText("&Container:");
+
+		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		containerText.setLayoutData(gd);
+		containerText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		Button button = new Button(container, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse();
+			}
+		});
+		label = new Label(container, SWT.NULL);
+		label.setText("&File name:");
+
+		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		fileText.setLayoutData(gd);
+		fileText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+		addContent(container);
+		initialize();
+		dialogChanged();
+		setControl(container);
+	}
+
+	/**
+	 * Tests if the current workbench selection is a suitable container to use.
+	 */
+	private void initialize() {
+		if (selection != null && !selection.isEmpty()
+				&& selection instanceof IStructuredSelection) {
+			IStructuredSelection ssel = (IStructuredSelection) selection;
+			if (ssel.size() > 1) {
+				return;
+			}
+			Object obj = ssel.getFirstElement();
+			if (obj instanceof IResource) {
+				IContainer container;
+				if (obj instanceof IContainer) {
+					container = (IContainer) obj;
+				} else {
+					container = ((IResource) obj).getParent();
+				}
+				containerText.setText(container.getFullPath().toString());
+			}
+		}
+		fileText.setText("new_file." + getWizardFileExtension());
+	}
+
+	/**
+	 * Uses the standard container selection dialog to choose the new value for
+	 * the container field.
+	 */
+
+	private void handleBrowse() {
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
+				"Select new file container");
+		if (dialog.open() == Window.OK) {
+			Object[] result = dialog.getResult();
+			if (result.length == 1) {
+				containerText.setText(((Path) result[0]).toString());
+			}
+		}
+	}
+
+	/**
+	 * Ensures that both text fields are set.
+	 */
+
+	private void dialogChanged() {
+		IResource container = ResourcesPlugin.getWorkspace().getRoot()
+				.findMember(new Path(getContainerName()));
+		String fileName = getFileName();
+
+		if (getContainerName().length() == 0) {
+			updateStatus("File container must be specified");
+			return;
+		}
+		if (container == null
+				|| (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
+			updateStatus("File container must exist");
+			return;
+		}
+		if (!container.isAccessible()) {
+			updateStatus("Project must be writable");
+			return;
+		}
+		if (fileName.length() == 0) {
+			updateStatus("File name must be specified");
+			return;
+		}
+		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
+			updateStatus("File name must be valid");
+			return;
+		}
+		int dotLoc = fileName.lastIndexOf('.');
+		if (dotLoc != -1) {
+			String ext = fileName.substring(dotLoc + 1);
+			if (!ext.equalsIgnoreCase(getWizardFileExtension())) {
+				updateStatus("File extension must be \""
+						+ getWizardFileExtension() + "\"");
+				return;
+			}
+		}
+		updateStatus(null);
+	}
+
+	/**
+	 * Set the current status + error message
+	 * 
+	 * @param message
+	 *            the message to display or null for ok status
+	 */
+	private void updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+	/**
+	 * Obtain the folder to create the file in
+	 * 
+	 * @return the container path
+	 */
+	public final String getContainerName() {
+		return containerText.getText();
+	}
+
+	/**
+	 * Obtain the name chosen for the new file
+	 * 
+	 * @return the file name without path
+	 */
+	public final String getFileName() {
+		return fileText.getText();
+	}
+}
