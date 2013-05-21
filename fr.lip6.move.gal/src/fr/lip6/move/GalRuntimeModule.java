d8c29b95e6a17d4c069456e0b2ecba894a0204e8
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2013-05-21 10:23:47 +0000

    dropped support for the breakpoints / java integration

--- fr.lip6.move.gal/src/fr/lip6/move/GalRuntimeModule.java
+++ fr.lip6.move.gal/src/fr/lip6/move/GalRuntimeModule.java
@@ -3,11 +3,11 @@
  */
 package fr.lip6.move;
 
-import org.eclipse.xtext.debug.IStratumBreakpointSupport;
+//import org.eclipse.xtext.debug.IStratumBreakpointSupport;
 import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.scoping.IScopeProvider;
 
-import fr.lip6.move.debug.GalStratumBreakpointSupport;
+//import fr.lip6.move.debug.GalStratumBreakpointSupport;
 import fr.lip6.move.scoping.GalNameConverter;
 import fr.lip6.move.scoping.GalScopeProvider;
 
@@ -16,10 +16,11 @@ import fr.lip6.move.scoping.GalScopeProvider;
  * Equinox extension registry.
  */
 public class GalRuntimeModule extends fr.lip6.move.AbstractGalRuntimeModule {
-	@Override
-	public Class<? extends IStratumBreakpointSupport> bindIStratumBreakpointSupport() {
-		return GalStratumBreakpointSupport.class;
-	}
+
+//	@Override
+//	public Class<? extends IStratumBreakpointSupport> bindIStratumBreakpointSupport() {
+//		return GalStratumBreakpointSupport.class;
+//	}
 	
 	
 	@Override
