271f78ad45346762bc04f57b8a9903058eb71478
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2014-03-06 19:00:23 +0000

    a new action that decomposes

---
+++ fr.lip6.move.gal.ui/src/fr/lip6/move/gal/flatten/popup/actions/DecomposeAction.java
@@ -0,0 +1,49 @@
+package fr.lip6.move.gal.flatten.popup.actions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import fr.lip6.move.gal.GALTypeDeclaration;
+import fr.lip6.move.gal.Specification;
+import fr.lip6.move.gal.TypeDeclaration;
+import fr.lip6.move.gal.instantiate.CompositeBuilder;
+import fr.lip6.move.gal.instantiate.GALRewriter;
+
+
+public class DecomposeAction extends GalAction {
+
+
+	@Override
+	protected String getServiceName() {		
+		return "Decompose (BETA)";
+	}
+
+	@Override
+	public void workWithSystem(Specification spec) throws Exception {				
+		GALRewriter.flatten(spec, true);
+		
+		
+		for (TypeDeclaration td : spec.getTypes()) {
+			if (td instanceof GALTypeDeclaration) {
+				GALTypeDeclaration gal = (GALTypeDeclaration) td;
+				Specification newspec = CompositeBuilder.getInstance().buildComposite(gal);
+				spec.getTypes().remove(td);
+				spec.getTypes().addAll(newspec.getTypes());
+				spec.setMain(spec.getTypes().get(spec.getTypes().size()-1));
+				return;
+			}
+		}
+	}
+	
+	
+	@Override
+	protected String getAdditionalExtension() {		
+		return ".mod";
+	}
+
+	@Override
+	protected List<String> getForbiddenExtension() {
+		return Arrays.asList("mod");
+	}
+
+}
