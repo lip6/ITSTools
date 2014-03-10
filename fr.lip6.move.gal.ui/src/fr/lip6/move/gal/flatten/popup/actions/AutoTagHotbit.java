21c0b0bc737583e58065a920c9556dc5e8933327
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2014-03-10 14:38:51 +0000

    a new menu entry to control hotbit tagging behavior

---
+++ fr.lip6.move.gal.ui/src/fr/lip6/move/gal/flatten/popup/actions/AutoTagHotbit.java
@@ -0,0 +1,36 @@
+package fr.lip6.move.gal.flatten.popup.actions;
+
+import java.util.Map;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.commands.IElementUpdater;
+import org.eclipse.ui.menus.UIElement;
+
+import fr.lip6.move.gal.instantiate.GALRewriter;
+
+public class AutoTagHotbit implements IObjectActionDelegate, IElementUpdater {
+
+	
+
+	@Override
+	public void run(IAction action) {
+		GALRewriter.autoTagHotbit = ! GALRewriter.autoTagHotbit;
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	@Override
+	public void updateElement(UIElement element, Map parameters) {
+		element.setChecked(GALRewriter.autoTagHotbit);
+	}
+
+}
