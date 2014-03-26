984dd48078020de51a42be9442f1c067f3a255c6
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2014-03-26 15:14:38 +0000

    patched scripts

--- fr.lip6.move.xta.benchmarks/CSMA/runITSone.sh
+++ fr.lip6.move.xta.benchmarks/CSMA/runITSone.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
 echo `date` >> trace.out
-for j in 1 2 3 4 5 ; do for i in *input_*$j.one.flat.gal ; do (/data/ythierry/download/memtime-1.3/memtime -m 4000000 -c 600 /data/ythierry/DDD/libits/bin/its-reach -i $i -t GAL --gen-order FOLLOW --quiet) >> trace.out 2>> trace.out ; done ; done ;
+
+HEUR=--gen-order FOLLOW
+
+for j in 1 2 3 4 5 ; do 
+    for i in *input_*$j.one.flat.gal ; do 
+	($ITSREACHCMD $i $HEUR) >> trace.out 2>> trace.out ; 
+    done ; 
+done ;
