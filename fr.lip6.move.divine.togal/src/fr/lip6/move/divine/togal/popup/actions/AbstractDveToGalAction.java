6836d0650b97d0aa82b78c07a30edef114a363c2
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2014-10-28 15:33:05 +0000

    copying in from Promela version where code was more robust

--- fr.lip6.move.divine.togal/src/fr/lip6/move/divine/togal/popup/actions/AbstractDveToGalAction.java
+++ fr.lip6.move.divine.togal/src/fr/lip6/move/divine/togal/popup/actions/AbstractDveToGalAction.java
@@ -2,8 +2,12 @@ package fr.lip6.move.divine.togal.popup.actions;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
@@ -14,21 +18,26 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
 import fr.lip6.move.divine.divine.DivineSpecification;
 import fr.lip6.move.divine.serialization.SerializationUtil;
-import fr.lip6.move.gal.GALTypeDeclaration;
-import fr.lip6.move.gal.GalFactory;
 import fr.lip6.move.gal.Specification;
 import fr.lip6.move.gal.instantiate.GALRewriter;
 
 public abstract class AbstractDveToGalAction implements IObjectActionDelegate {
 
-
-	/** STUFF BELOW IS WRAPPINGTHE FUNCTIONALITY I A BUTTON */
 	
+	// MAYBE: PAss to Command and Handler
+	// TODO: apparait que dans projet ou ya des fichiers promela? (ajouter une
+	// nature prom)
+
+	// TODO: action depuis un clic dans le fichier GAL!!
+
+	/** STUFF BELOW IS WRAPPING THE FUNCTIONALITY IN A BUTTON */
+
 	private Shell shell;
 	private List<IFile> files = new ArrayList<IFile>();
 
@@ -50,63 +59,107 @@ public abstract class AbstractDveToGalAction implements IObjectActionDelegate {
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction a) {
-		StringBuilder sb = new StringBuilder();
-		for (IFile file: files) {
+
+		if (files.isEmpty()) {
+			MessageDialog.openWarning(shell, "Convertion Error",
+					"No Promela file to Be converted");
+			return;
+		}
+
+		List<String> producedFiles = new ArrayList<String>();
+		Map<String, Exception> errors = new HashMap<String, Exception>();
+
+		for (IFile file : files) {
 			if (file != null) {
-				DivineSpecification s = SerializationUtil.fileToDivineSpecification(file.getRawLocationURI().getPath());
-			
+				final String galName = file.getName().replace(".dve", "").replace(".", "_").replace(" ", "_");
 				try {
-					String galName = file.getName().replace(".dve", "");
-					GALTypeDeclaration gal = doTransformation(s, galName);
-					Specification spec = GalFactory.eINSTANCE.createSpecification();
-					spec.getTypes().add(gal);
-					
-					String path = file.getRawLocationURI().getPath();
-					if (path.endsWith(".dve")) {
-						path = path.substring(0,path.length()-4);
+					// Parsage du fichier promela
+					final DivineSpecification s = SerializationUtil.fileToDivineSpecification(file.getRawLocationURI().getPath());
+
+
+					// Pour éviter Gal se plaint des nom mon toto.34_glat
+
+
+
+					// Transformation de DVE vers Gal
+					final Specification galSpec = doTransformation(s, galName);
+
+					// Sauvegarde du résultat
+
+					// Premier fichier GAL:
+					String tmpPath = file.getRawLocationURI().getPath();
+					if (tmpPath.endsWith(".dve")) {// It is the case!!
+						tmpPath = tmpPath.substring(0, tmpPath.length() - 4);
 					}
-					String outpath = path + getExtension() + ".gal";
-				
-					FileOutputStream out = new FileOutputStream(new File(outpath));
-					out.write(0);
-					out.close();					
-					fr.lip6.move.serialization.SerializationUtil.systemToFile(spec, outpath);
-					java.lang.System.err.println("GAL model written to file: " + outpath);
-					sb.append(" " + outpath);
-					
-//					GALRewriter.flatten(spec, true);
-//					
-//					String outpath2 = path + getExtension() +".flat" + ".gal";
-//					FileOutputStream out2 = new FileOutputStream(new File(outpath2));
-//					out2.write(0);
-//					out2.close();					
-//					fr.lip6.move.serialization.SerializationUtil.systemToFile(spec, outpath2);
-//					java.lang.System.err.println("GAL flat model written to file: " + outpath2);
-//					sb.append(" " + outpath2);
-					
-					
+
+					String galFileName = tmpPath + getExtension();
+					saveGalFile(galFileName, galSpec);
+
+					producedFiles.add(galName + getExtension());
+
+					// Fichier Gal applati
+					GALRewriter.flatten(galSpec, true);
+
+					String galFlatFileName = tmpPath + getExtension() + ".flat";
+					saveGalFile(galFlatFileName, galSpec);
+
+					producedFiles.add(galName + getExtension() + ".flat");
+
 				} catch (Exception e) {
-					MessageDialog.openWarning(
-						shell,
-						"Transform DVE to GAL operation raised an exception " + e.getMessage(), null);
+					errors.put(galName, e);
+					// MessageDialog.openWarning(shell, "Convertion Error",
+					// "Transform Promela to GAL operation raised an exception :\n"
+					// + e.getMessage());
 					e.printStackTrace();
-					return;
+					// return;
 				}
 			}
 		}
-		
-		MessageDialog.openInformation(
-				shell,
-				"XTA to GAL transformation result",
-				"operation successfully produced files: " + sb.toString());
-		
+
+		// Message succès conditionné... (au vrai succès)
+
+		StringBuffer sb = new StringBuffer();
+		if (! producedFiles.isEmpty()) {
+			sb.append("Operation successfully produced "+ producedFiles.size() +" files:\n");
+			for (String f : producedFiles) {
+				sb.append(f).append(", ");
+			}
+			sb.setLength(sb.length() - 2);
+			sb.append(".");
+		}
+
+		if (!errors.isEmpty()) {
+			sb.append("\n Some files were not transformed due to errors ("+ errors.size() +" files)  : \n");
+
+			for (Entry<String, Exception> e : errors.entrySet()) {
+				sb.append(e.getKey() + " : " + e.getValue().getMessage() +"\n");
+				e.getValue().printStackTrace();
+			}
+		}
+
+		MessageDialog.openInformation(shell,"Promela to GAL transformation result", sb.toString());
+
+
 		files.clear();
 	}
+	
+	public static void saveGalFile(String name, Specification galSpec)
+			throws IOException {
+		String outPath = name + ".gal";
 
-	public abstract String getExtension();
+		FileOutputStream out = new FileOutputStream(new File(outPath));
+		out.write(0);
+		out.close();
 
-	public abstract GALTypeDeclaration doTransformation(DivineSpecification s, String galName) ;
+		fr.lip6.move.serialization.SerializationUtil.systemToFile(galSpec, outPath);
+		java.lang.System.err.println("GAL model written to file: " + outPath);
+	}
 
+	
+	public static boolean isDVEFile(IFile file) {
+		return file.getFileExtension() != null
+				&& file.getFileExtension().equals("dve");
+	}
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
@@ -115,14 +168,15 @@ public abstract class AbstractDveToGalAction implements IObjectActionDelegate {
 		if (sel instanceof IStructuredSelection) {
 			IStructuredSelection selection = (IStructuredSelection) sel;
 			for (Object elt : selection.toArray()) {
-				if (elt instanceof IResource  && ((IResource) elt).isAccessible()) {
+				if (elt instanceof IResource) {
 					try {
 						((IResource) elt).accept(new IResourceVisitor() {
 							@Override
-							public boolean visit(IResource resource) throws CoreException {
+							public boolean visit(IResource resource)
+									throws CoreException {
 								if (resource instanceof IFile) {
 									IFile file = (IFile) resource;
-									if (file.getFileExtension() != null && file.getFileExtension().equals("dve")) {
+									if (isDVEFile(file)) {
 										files.add(file);
 									}
 								}
@@ -137,5 +191,87 @@ public abstract class AbstractDveToGalAction implements IObjectActionDelegate {
 			}
 		}
 	}
+
+
+//	/**
+//	 * @see IActionDelegate#run(IAction)
+//	 */
+//	public void run(IAction a) {
+//		StringBuilder sb = new StringBuilder();
+//		for (IFile file: files) {
+//			if (file != null) {
+//				DivineSpecification s = SerializationUtil.fileToDivineSpecification(file.getRawLocationURI().getPath());
+//			
+//				try {
+//					String galName = file.getName().replace(".dve", "");
+//					GALTypeDeclaration gal = doTransformation(s, galName);
+//					Specification spec = GalFactory.eINSTANCE.createSpecification();
+//					spec.getTypes().add(gal);
+//					
+//					String path = file.getRawLocationURI().getPath();
+//					if (path.endsWith(".dve")) {
+//						path = path.substring(0,path.length()-4);
+//					}
+//					String outpath = path + getExtension() + ".gal";
+//					String tmpPath = file.getRawLocationURI().getPath();
+//					if (tmpPath.endsWith(".pml")) {// It is the case!!
+//						tmpPath = tmpPath.substring(0, tmpPath.length() - 4);
+//					}
+//
+//					String galFileName = tmpPath + getExtension();
+//					saveGalFile(galFileName, galSpec);
+//
+//					producedFiles.add(galName + getExtension());
+//
+//					// Fichier Gal applati
+//					GALRewriter.flatten(galSpec, true);
+//
+//					String galFlatFileName = tmpPath + getExtension() + ".flat";
+//					saveGalFile(galFlatFileName, galSpec);
+//
+//					producedFiles.add(galName + getExtension() + ".flat");
+//
+//					
+//					FileOutputStream out = new FileOutputStream(new File(outpath));
+//					out.write(0);
+//					out.close();					
+//					fr.lip6.move.serialization.SerializationUtil.systemToFile(spec, outpath);
+//					java.lang.System.err.println("GAL model written to file: " + outpath);
+//					sb.append(" " + outpath);
+//					
+////					GALRewriter.flatten(spec, true);
+////					
+////					String outpath2 = path + getExtension() +".flat" + ".gal";
+////					FileOutputStream out2 = new FileOutputStream(new File(outpath2));
+////					out2.write(0);
+////					out2.close();					
+////					fr.lip6.move.serialization.SerializationUtil.systemToFile(spec, outpath2);
+////					java.lang.System.err.println("GAL flat model written to file: " + outpath2);
+////					sb.append(" " + outpath2);
+//					
+//					
+//				} catch (Exception e) {
+//					MessageDialog.openWarning(
+//						shell,
+//						"Transform DVE to GAL operation raised an exception " + e.getMessage(), null);
+//					e.printStackTrace();
+//					return;
+//				}
+//			}
+//		}
+//		
+//		MessageDialog.openInformation(
+//				shell,
+//				"XTA to GAL transformation result",
+//				"operation successfully produced files: " + sb.toString());
+//		
+//		files.clear();
+//	}
+
+	public abstract String getExtension();
+
+	public abstract Specification doTransformation(DivineSpecification s, String galName) ;
+
+	
 
 }
--- fr.lip6.move.divine.togal/src/fr/lip6/move/divine/togal/popup/actions/DveToGalAction.java
+++ fr.lip6.move.divine.togal/src/fr/lip6/move/divine/togal/popup/actions/DveToGalAction.java
@@ -3,15 +3,19 @@ package fr.lip6.move.divine.togal.popup.actions;
 import fr.lip6.move.divine.divine.DivineSpecification;
 import fr.lip6.move.divine.togal.transform.DveToGALTransformer;
 import fr.lip6.move.gal.GALTypeDeclaration;
+import fr.lip6.move.gal.GalFactory;
+import fr.lip6.move.gal.Specification;
 
 public class DveToGalAction extends AbstractDveToGalAction {
 
 	@Override
-	public GALTypeDeclaration doTransformation(DivineSpecification s, String galName) 
+	public Specification doTransformation(DivineSpecification s, String galName) 
 	{
 		DveToGALTransformer trans = new DveToGALTransformer();
 		GALTypeDeclaration gal = trans.transformToGAL (s,galName);
-		return gal;
+		Specification spec = GalFactory.eINSTANCE.createSpecification();
+		spec.getTypes().add(gal);
+		return spec;
 	}
 	
 	@Override
