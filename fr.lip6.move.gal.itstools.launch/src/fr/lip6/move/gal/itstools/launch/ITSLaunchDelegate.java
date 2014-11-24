927ecd15cff661311a6dc6f1df8ef1c6b1c59b50
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2014-11-24 17:57:28 +0000

    it runs !!

--- fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/ITSLaunchDelegate.java
+++ fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/ITSLaunchDelegate.java
@@ -1,11 +1,18 @@
 package fr.lip6.move.gal.itstools.launch;
 
+import java.io.File;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate2;
+import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+
+import fr.lip6.move.gal.itstools.preference.GalPreferencesActivator;
+import fr.lip6.move.gal.itstools.preference.PreferenceConstants;
 
 public class ITSLaunchDelegate extends LaunchConfigurationDelegate implements
 		ILaunchConfigurationDelegate2 {
@@ -13,7 +20,22 @@ public class ITSLaunchDelegate extends LaunchConfigurationDelegate implements
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,	ILaunch launch, IProgressMonitor monitor) throws CoreException {
 		System.out.println("Yay, it runs ! " + configuration.getAttribute(LaunchConstants.MODEL_FILE, "model.gal"));
-
+		
+		
+		
+		String[] cmdLine = { configuration.getAttribute(PreferenceConstants.ITSREACH_EXE, GalPreferencesActivator.getDefault().getPreferenceStore().getString(PreferenceConstants.ITSREACH_EXE)),
+				"-i",
+				configuration.getAttribute(LaunchConstants.MODEL_FILE, "model.gal"),
+				"-t",
+				"GAL"
+		};
+		File workingDirectory = new File(configuration.getAttribute(LaunchConstants.MODEL_FILE, "model.gal")).getParentFile();
+		Process p = DebugPlugin.exec(cmdLine, workingDirectory );
+		
+		IProcess proc = DebugPlugin.newProcess(launch, p, "ITS runner");
+		
+		System.out.println("done!");
+		
 	}
 
 }
