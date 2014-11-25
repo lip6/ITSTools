c458ca95b500c8db814990d62e211b2651044b73
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2014-11-25 11:34:30 +0000

    a launch shortcut, in progress

---
+++ fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/ITSLaunchShortcut.java
@@ -0,0 +1,25 @@
+package fr.lip6.move.gal.itstools.launch;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.debug.ui.ILaunchShortcut2;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+
+public class ITSLaunchShortcut implements ILaunchShortcut {
+
+	@Override
+	public void launch(ISelection selection, String mode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void launch(IEditorPart editor, String mode) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
