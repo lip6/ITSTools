fe9938108ce62cc35ea158ab62a1c31cd8848455
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-01-27 15:59:03 +0000

    add VarDecl case for refs from properties

--- fr.lip6.move.gal/src/fr/lip6/move/scoping/GALCrossReferenceSerializer.java
+++ fr.lip6.move.gal/src/fr/lip6/move/scoping/GALCrossReferenceSerializer.java
@@ -11,25 +11,30 @@ import org.eclipse.xtext.serializer.tokens.ICrossReferenceSerializer;
 import fr.lip6.move.gal.AbstractInstance;
 import fr.lip6.move.gal.AbstractParameter;
 import fr.lip6.move.gal.Label;
+import fr.lip6.move.gal.VarDecl;
 
 public class GALCrossReferenceSerializer extends CrossReferenceSerializer implements ICrossReferenceSerializer {
 
-	
-@Override
-public String serializeCrossRef(EObject semanticObject, CrossReference crossref, EObject target, INode node,
-		Acceptor errors) {
-	if (target instanceof Label) {
-		Label lab = (Label) target;
-		return "\"" + lab.getName() + "\"";
+
+	@Override
+	public String serializeCrossRef(EObject semanticObject, CrossReference crossref, EObject target, INode node,
+			Acceptor errors) {
+		if (target instanceof Label) {
+			Label lab = (Label) target;
+			return "\"" + lab.getName() + "\"";
+		}
+		if (target instanceof AbstractInstance) {
+			AbstractInstance ai = (AbstractInstance) target;
+			return ai.getName();
+		}
+		if (target instanceof AbstractParameter) {
+			AbstractParameter param = (AbstractParameter) target;
+			return param.getName();
+		}
+		if (target instanceof VarDecl) {
+			VarDecl var = (VarDecl) target;
+			return var.getName();
+		}
+		return super.serializeCrossRef(semanticObject, crossref, target, node, errors);
 	}
-	if (target instanceof AbstractInstance) {
-		AbstractInstance ai = (AbstractInstance) target;
-		return ai.getName();
-	}
-	if (target instanceof AbstractParameter) {
-		AbstractParameter param = (AbstractParameter) target;
-		return param.getName();
-	}
-	return super.serializeCrossRef(semanticObject, crossref, target, node, errors);
-}
 }
