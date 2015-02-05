0a5665fd37f5476dc02cc2101de9ae60f0240082
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-02-05 12:34:04 +0000

    lots of updates to cope with new instance and call API Some code changes non trivial

--- fr.lip6.move.gal/src/fr/lip6/move/gal/instantiate/TypeFuser.java
+++ fr.lip6.move.gal/src/fr/lip6/move/gal/instantiate/TypeFuser.java
@@ -9,46 +9,51 @@ import java.util.Map;
 import java.util.Set;
 
 
+
+
+
+
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
-import fr.lip6.move.gal.AbstractInstance;
-import fr.lip6.move.gal.Action;
+import fr.lip6.move.gal.InstanceDecl;
+import fr.lip6.move.gal.InstanceDeclaration;
 import fr.lip6.move.gal.CompositeTypeDeclaration;
 import fr.lip6.move.gal.GALParamDef;
 import fr.lip6.move.gal.GF2;
 import fr.lip6.move.gal.GalFactory;
-import fr.lip6.move.gal.GalInstance;
 import fr.lip6.move.gal.InstanceCall;
-import fr.lip6.move.gal.ItsInstance;
 import fr.lip6.move.gal.Label;
+import fr.lip6.move.gal.Reference;
 import fr.lip6.move.gal.SelfCall;
 import fr.lip6.move.gal.Specification;
+import fr.lip6.move.gal.Statement;
 import fr.lip6.move.gal.Synchronization;
 import fr.lip6.move.gal.TypeDeclaration;
+import fr.lip6.move.gal.VariableReference;
 
 public class TypeFuser {
 
-	
+
 	public static void fuseSimulatedTypes (Specification spec) {
-		
+
 		sortTypes(spec);
 		sortInstances(spec);
-		
+
 		buildArchetypes(spec);
 	}
-	
+
 	private static void sortInstances(Specification spec) {
 		for (TypeDeclaration td : spec.getTypes()) {
 			if (td instanceof CompositeTypeDeclaration) {
 				CompositeTypeDeclaration ctd = (CompositeTypeDeclaration) td;
-				
-				List<AbstractInstance> insts = new ArrayList<AbstractInstance>(ctd.getInstances());
+
+				List<InstanceDecl> insts = new ArrayList<InstanceDecl>(ctd.getInstances());
 				Collections.sort(insts,new InstanceComparator(spec));
 				ctd.getInstances().clear();
 				ctd.getInstances().addAll(insts);
-				
+
 				List<Synchronization> syncs = new ArrayList<Synchronization>(ctd.getSynchronizations());
 				Collections.sort(syncs,new SynchronizationComparator());
 				ctd.getSynchronizations().clear();
@@ -69,9 +74,9 @@ public class TypeFuser {
 	private static void buildArchetypes(Specification spec) {
 		Map<Label,Label> labMap = new HashMap<Label, Label>();
 		Map<CompositeTypeDeclaration,CompositeTypeDeclaration> typeMap = new HashMap<CompositeTypeDeclaration, CompositeTypeDeclaration>();
-		
+
 		Map<String,ArcheType> archeTypes = new HashMap<String, ArcheType>();
-		
+
 		DepthFathomer df = new DepthFathomer();
 		int maxdepth = 1;
 		for (int depth = 1; depth <= maxdepth ; depth++) {
@@ -80,79 +85,89 @@ public class TypeFuser {
 			typeMap.clear();
 			labMap.clear();
 
-		for (TypeDeclaration td : new ArrayList<TypeDeclaration>(spec.getTypes())) {
-			int dt = df.getDepth(td);
-			maxdepth = Math.max(dt, maxdepth);
-			if (dt == depth) {
-				if (td instanceof CompositeTypeDeclaration) {
-					CompositeTypeDeclaration ctd = (CompositeTypeDeclaration) td;
-					if (! hasSingleNonLocalLabels(ctd)) {
-						continue;
-					}
-					String tdesc = computeTypeString(ctd);
-					ArcheType arch = archeTypes.get(tdesc);
-					if (arch == null) {
-						arch = new ArcheType();
-						arch.type = GalFactory.eINSTANCE.createCompositeTypeDeclaration();
-						arch.type.setName("Archetype"+archeTypes.size());
-						int i =0;
-						for (AbstractInstance ai : ctd.getInstances()) {
-							AbstractInstance bi = EcoreUtil.copy(ai);
-							bi.setName("i"+ i++);
-							arch.type.getInstances().add(bi);
+			for (TypeDeclaration td : new ArrayList<TypeDeclaration>(spec.getTypes())) {
+				int dt = df.getDepth(td);
+				maxdepth = Math.max(dt, maxdepth);
+				if (dt == depth) {
+					if (td instanceof CompositeTypeDeclaration) {
+						CompositeTypeDeclaration ctd = (CompositeTypeDeclaration) td;
+						if (! hasSingleNonLocalLabels(ctd)) {
+							continue;
 						}
-						archeTypes.put(tdesc, arch);
-						spec.getTypes().add(arch.type);
-					}
-					spec.getTypes().remove(ctd);
-					
-					for (Synchronization sync : ctd.getSynchronizations()) {
-						String sdesc = computeSyncString(sync);
-						Synchronization archsync = arch.archSyncs.get(sdesc);
-						if (archsync == null) {
-							archsync = GalFactory.eINSTANCE.createSynchronization();
-							archsync.setName("s" + arch.type.getSynchronizations().size());
-							archsync.setLabel(GF2.createLabel(sdesc));
-							for (Action act : sync.getActions()) {
-								if (act instanceof InstanceCall) {
-									InstanceCall icall = (InstanceCall) act;
-									InstanceCall icall2 = GalFactory.eINSTANCE.createInstanceCall();
-									icall2.setInstance(arch.type.getInstances().get(getIndex(icall.getInstance())));
-									icall2.setLabel(icall.getLabel());
-									archsync.getActions().add(icall2);
-								}
+						String tdesc = computeTypeString(ctd);
+						ArcheType arch = archeTypes.get(tdesc);
+						if (arch == null) {
+							arch = new ArcheType();
+							arch.type = GalFactory.eINSTANCE.createCompositeTypeDeclaration();
+							arch.type.setName("Archetype"+archeTypes.size());
+							int i =0;
+							for (InstanceDecl ai : ctd.getInstances()) {
+								InstanceDecl bi = EcoreUtil.copy(ai);
+								bi.setName("i"+ i++);
+								arch.type.getInstances().add(bi);
 							}
-							arch.archSyncs.put(sdesc, archsync);
-							arch.type.getSynchronizations().add(archsync);
+							archeTypes.put(tdesc, arch);
+							spec.getTypes().add(arch.type);
 						}
-						labMap.put(sync.getLabel(), archsync.getLabel());
+						spec.getTypes().remove(ctd);
+
+						for (Synchronization sync : ctd.getSynchronizations()) {
+							String sdesc = computeSyncString(sync);
+							Synchronization archsync = arch.archSyncs.get(sdesc);
+							if (archsync == null) {
+								archsync = GalFactory.eINSTANCE.createSynchronization();
+								archsync.setName("s" + arch.type.getSynchronizations().size());
+								archsync.setLabel(GF2.createLabel(sdesc));
+								for (Statement act : sync.getActions()) {
+									if (act instanceof InstanceCall) {
+										InstanceCall icall = (InstanceCall) act;
+										InstanceCall icall2 = GalFactory.eINSTANCE.createInstanceCall();
+										icall2.setInstance(GF2.createVariableRef(arch.type.getInstances().get(getIndex(icall.getInstance()))));
+										icall2.setLabel(icall.getLabel());
+										archsync.getActions().add(icall2);
+									}
+								}
+								arch.archSyncs.put(sdesc, archsync);
+								arch.type.getSynchronizations().add(archsync);
+							}
+							labMap.put(sync.getLabel(), archsync.getLabel());
+						}
+
+						typeMap.put(ctd, arch.type);
 					}
-					
-					typeMap.put(ctd, arch.type);
+				}
+			}
+
+			System.out.println("Fusing "+ typeMap.size() + " types at depth" + depth);
+
+			for (TreeIterator<EObject> it = spec.eAllContents(); it.hasNext() ; ) {
+				EObject obj = it.next();
+				if (obj instanceof InstanceDecl && ((InstanceDecl) obj).getType() instanceof CompositeTypeDeclaration) {
+					CompositeTypeDeclaration td = (CompositeTypeDeclaration) ((InstanceDecl) obj).getType();
+					CompositeTypeDeclaration target = typeMap.get(td);
+					if (target != null) {
+						((InstanceDecl) obj).setType(target);
+					}
+				} else if (obj instanceof InstanceCall) {
+					InstanceCall icall = (InstanceCall) obj;
+					Label target = labMap.get(icall.getLabel());
+					if (target != null) {
+						icall.setLabel(target);
+					}
 				}
 			}
 		}
-		
-		System.out.println("Fusing "+ typeMap.size() + " types at depth" + depth);
-		
-		for (TreeIterator<EObject> it = spec.eAllContents(); it.hasNext() ; ) {
-			EObject obj = it.next();
-			if (obj instanceof ItsInstance) {
-				ItsInstance itsi = (ItsInstance) obj;
-				CompositeTypeDeclaration target = typeMap.get(itsi.getType());
-				if (target != null) {
-					itsi.setType(target);
-				}
-			} else if (obj instanceof InstanceCall) {
-				InstanceCall icall = (InstanceCall) obj;
-				Label target = labMap.get(icall.getLabel());
-				if (target != null) {
-					icall.setLabel(target);
-				}
+
+	}
+
+	private static int getIndex(Reference instance) {
+		if (instance instanceof VariableReference) {
+			VariableReference ref = (VariableReference) instance;
+			if (ref.getRef() instanceof InstanceDeclaration) {
+				return getIndex((InstanceDecl)ref.getRef());
 			}
 		}
-		}
-		
+		return 0;
 	}
 
 	private static boolean hasSingleNonLocalLabels(CompositeTypeDeclaration ctd) {
@@ -163,7 +178,7 @@ public class TypeFuser {
 				return false;
 			}
 			labs.add(lab);
-			for (Action act : sync.getActions()) {
+			for (Statement act : sync.getActions()) {
 				if (act instanceof SelfCall) {
 					return false;
 				}
@@ -174,19 +189,19 @@ public class TypeFuser {
 
 	public static String computeSyncString (Synchronization sync) {
 		StringBuilder sb = new StringBuilder();
-		for (Action act : sync.getActions()) {
+		for (Statement act : sync.getActions()) {
 			if (act instanceof InstanceCall) {
 				InstanceCall icall = (InstanceCall) act;
 				if (icall.getLabel() instanceof Label) {
 					Label lab = (Label) icall.getLabel();
-					sb.append(getIndex(icall.getInstance())+"."+lab.getName());
+					sb.append(getIndex((InstanceDecl) ((VariableReference) icall.getInstance()).getRef())+"."+lab.getName());
 				}
 			}
 		}
 		return sb.toString();
 	}
-	
-	private static int getIndex(AbstractInstance instance) {
+
+	private static int getIndex(InstanceDecl instance) {
 		if (instance.eContainer() instanceof CompositeTypeDeclaration) {
 			CompositeTypeDeclaration ctd = (CompositeTypeDeclaration) instance.eContainer();
 			return ctd.getInstances().indexOf(instance);
@@ -196,28 +211,17 @@ public class TypeFuser {
 
 	public static String computeTypeString (CompositeTypeDeclaration ctd) {
 		StringBuilder sb = new StringBuilder();
-		for (AbstractInstance ai : ctd.getInstances()) {
+		for (InstanceDecl ai : ctd.getInstances()) {
 			sb.append(computeInstanceTypeString(ai));
 		}
 		return sb.toString();
 	}
-	
-	public static String computeInstanceTypeString(AbstractInstance ai) {
+
+	public static String computeInstanceTypeString(InstanceDecl ai) {
 		StringBuilder sb = new StringBuilder();
-		if (ai instanceof GalInstance) {
-			GalInstance gali = (GalInstance) ai;
-			sb.append(gali.getType().getName());
-			for (GALParamDef pdef : gali.getParamDefs()) {
-				sb.append(pdef.getParam().getName().replace("$", "")+pdef.getValue());
-			}
-		} else if (ai instanceof ItsInstance) {
-			ItsInstance itsi = (ItsInstance) ai;
-			sb.append(itsi.getType().getName());
-//		} else if (ai instanceof OtherInstance) {
-//			OtherInstance otheri = (OtherInstance) ai;
-//			sb.append("other");				
-		} else {
-			sb.append("template");
+		sb.append(ai.getType().getName());
+		for (GALParamDef pdef : ai.getParamDefs()) {
+			sb.append(pdef.getParam().getName().replace("$", "")+pdef.getValue());
 		}
 		return sb.toString();
 	}
@@ -226,28 +230,15 @@ public class TypeFuser {
 
 		if (type instanceof CompositeTypeDeclaration) {
 			CompositeTypeDeclaration ctd = (CompositeTypeDeclaration) type;
-			for (AbstractInstance ai : ctd.getInstances()) {
-				if (ai instanceof GalInstance) {
-					GalInstance gali = (GalInstance) ai;
-					if (!todo.contains(gali.getType())) {
-						getDependentTypes(types, gali.getType(), todo, res);
-					}
+			for (InstanceDecl ai : ctd.getInstances()) {
+				if (!todo.contains(ai.getType())) {
+					getDependentTypes(types, ai.getType(), todo, res);
 				}
-				if (ai instanceof ItsInstance) {
-					ItsInstance gali = (ItsInstance) ai;
-					if (!todo.contains(gali.getType())) {
-						getDependentTypes(types, gali.getType(), todo, res);
-					}
-				}
-//				if (ai instanceof OtherInstance) {
-//					OtherInstance gali = (OtherInstance) ai;
-//				}
 			}
 		}
 		res.add(type);
 		todo.add(type);
 	}
-
 }
 
 
