e61f65e1bc0feea3c32f8b7f4ae2638f36996bc0
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-03-03 18:58:11 +0000

    most of the behavior is now delegated to CommandLineBuilder

--- itstools/fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/ITSLaunchDelegate.java
+++ itstools/fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/ITSLaunchDelegate.java
@@ -1,34 +1,17 @@
 package fr.lip6.move.gal.itstools.launch;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate2;
 import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 
-import fr.lip6.move.gal.Property;
-import fr.lip6.move.gal.Specification;
 import fr.lip6.move.gal.flatten.popup.actions.ConsoleAdder;
-import fr.lip6.move.gal.instantiate.GALRewriter;
-import fr.lip6.move.gal.itstools.preference.GalPreferencesActivator;
-import fr.lip6.move.gal.itstools.preference.PreferenceConstants;
-import fr.lip6.move.serialization.SerializationUtil;
-import fr.lip6.move.ui.labeling.ToStringUtils;
 
 public class ITSLaunchDelegate extends LaunchConfigurationDelegate implements
 ILaunchConfigurationDelegate2 {
@@ -36,139 +19,22 @@ ILaunchConfigurationDelegate2 {
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,	ILaunch launch, IProgressMonitor monitor) throws CoreException {
-
-
-		List<String> args = new ArrayList<String>();
-
 		ConsoleAdder.startConsole();
 
-		// Path to source model file
-		String oriString = configuration.getAttribute(LaunchConstants.MODEL_FILE, "model.gal");		
-
-		// Path to ITS-reach exe				
-		String itsReachPath = configuration.getAttribute(PreferenceConstants.ITSREACH_EXE, GalPreferencesActivator.getDefault().getPreferenceStore().getString(PreferenceConstants.ITSREACH_EXE));
-
-		args.add(itsReachPath);
-
-		// Produce a GAL file to give to its-tools
-		IPath oriPath = Path.fromPortableString(oriString);
-
-		// work folder
-		File workingDirectory ;
-
-		
-		String cegarProp = configuration.getAttribute(LaunchConstants.CEGAR_RUN, "");
-		if (! "".equals(cegarProp)) {
-			workingDirectory = new File (oriPath.removeLastSegments(1).toString());
-			// Input file options
-			args.add("-i") ;
-			args.add(oriPath.lastSegment());
-			// Model type option
-			args.add("-t");
-			args.add("CGAL");
-
-			String doTrace = configuration.getAttribute(LaunchConstants.PLAY_TRACE, "");
-			if (! "".equals(doTrace)) {
-				args.add("-trace");
-				args.add(doTrace);
-			} else {
-				args.add("-reachable");
-				args.add(cegarProp);
-			}
-			// limit verbosity
-			args.add("--quiet");
-			
-		} else {
-			// parse it
-			Specification spec = SerializationUtil.fileToGalSystem(oriString);
-
-			// copy spec 
-			Specification specNoProp = EcoreUtil.copy(spec);
-
-			// clear properties : they will be fed separately
-			specNoProp.getProperties().clear();
-			// flatten it
-			GALRewriter.flatten(specNoProp, true);
-
-			
-			workingDirectory = new File (oriPath.removeLastSegments(1).append("/work/").toString());
-
-			try {
-				workingDirectory.mkdir();
-			} catch (SecurityException e) {
-				e.printStackTrace();
-				throw new CoreException(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Unable to create work folder :"+workingDirectory+". Please check location is open to write in.",e));
-			}
-
-			String tmpPath = workingDirectory.getPath() + "/" +oriPath.lastSegment();		
-			File modelff = new File(tmpPath);
-
-			try {
-				SerializationUtil.systemToFile(specNoProp, tmpPath);
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw new CoreException(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Unable to create working file :"+tmpPath+". Please check location is open to write in.",e));
-			}
-
-			// Input file options
-			args.add("-i") ;
-			args.add(modelff.getName());
-
-			// Model type option
-			args.add("-t");
-			if (specNoProp.getMain() != null)
-				args.add("CGAL");
-			else 
-				args.add("GAL");
-
-			// test for and handle properties		
-			if (! spec.getProperties().isEmpty()) {
-
-				// We will put properties in a file
-				String propPath = workingDirectory.getPath() + "/" + oriPath.removeFileExtension().lastSegment() + ".prop";
-
-				try {
-					// create file
-					File propFile = new File(propPath);
-					PrintWriter out = new PrintWriter(propFile);
-
-					// first line is removed anyway : reference source model
-					out.write("import  \"" + modelff.getName() + "\";\n");
-
-					// Add one line per property
-					for (Property prop : spec.getProperties()) {
-						out.write(ToStringUtils.getTextString(prop) + "\n") ;
-					}
-					// 
-					out.flush();
-					out.close();
-
-					// property file arguments
-					args.add("-reachable-file");
-					args.add(propFile.getName());
-
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					throw new CoreException(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Unable to create file to hold properties :"+tmpPath+". Please check location is open to write in.",e));
-				}
-
-
-			}
-		}
-
+		CommandLine cl = CommandLineBuilder.buildCommand(configuration);
 
 		// Bring it all together for the invocation
 		// full argument list
-		String [] cmdLine = args.toArray(new String[args.size()]);		
-
+		
+		Logger.getLogger("fr.lip6.move.gal").fine("Running command line " + cl);
 
 		// Define the process
-		Process p = DebugPlugin.exec(cmdLine, workingDirectory.getAbsoluteFile() );
+		Process p = DebugPlugin.exec(cl.getArgs(), cl.getWorkingDir().getAbsoluteFile() );
 
 		// Let the DebugPlugin manage running the process
 		IProcess proc = DebugPlugin.newProcess(launch, p, "ITS runner");
 		// System.out.println("done!");
-
+		
 
 	//	ConsoleAdder.stopconsole();
 
