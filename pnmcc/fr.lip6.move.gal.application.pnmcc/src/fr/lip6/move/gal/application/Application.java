332205582e7aa5ccf5ef1fc842eed64fb10fa503
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-05-06 19:49:20 +0000

    integrating code from various sources, its.launch, pnmcc risght click action... This version should handle reachability with the default basic approach

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/Application.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/Application.java
@@ -1,27 +1,38 @@
 package fr.lip6.move.gal.application;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.net.URI;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
 import fr.lip6.move.gal.GALTypeDeclaration;
+import fr.lip6.move.gal.Property;
 import fr.lip6.move.gal.Specification;
 import fr.lip6.move.gal.instantiate.CompositeBuilder;
 import fr.lip6.move.gal.instantiate.GALRewriter;
+import fr.lip6.move.gal.instantiate.Instantiator;
 import fr.lip6.move.gal.instantiate.Simplifier;
 import fr.lip6.move.gal.itstools.BinaryToolsPlugin;
 import fr.lip6.move.gal.itstools.BinaryToolsPlugin.Tool;
 import fr.lip6.move.gal.itstools.launch.CommandLine;
 import fr.lip6.move.gal.itstools.launch.ProcessController;
 import fr.lip6.move.gal.itstools.launch.ProcessController.TimeOutException;
+import fr.lip6.move.gal.logic.Properties;
+import fr.lip6.move.gal.logic.saxparse.PropertyParser;
+import fr.lip6.move.gal.logic.togal.ToGalTransformer;
+import fr.lip6.move.gal.order.IOrder;
 import fr.lip6.move.gal.pnml.togal.PnmlToGalTransformer;
+import fr.lip6.move.gal.support.Support;
 import fr.lip6.move.serialization.SerializationUtil;
+import fr.lip6.move.ui.labeling.ToStringUtils;
 
 /**
  * This class controls all aspects of the application's execution
@@ -36,8 +47,6 @@ public class Application implements IApplication {
 
 	private static final String EXAMINATION = "-examination";
 	
-	private enum ExamType { STATESPACE, REACH, CTL, LTL }
-
 	private ByteArrayOutputStream errorOutput;
 
 	private ByteArrayOutputStream stdOutput;;
@@ -49,40 +58,95 @@ public class Application implements IApplication {
 		String [] args = (String[]) context.getArguments().get(APPARGS);
 		
 		String pwd = null;
-		ExamType examination = null;
+		String examination = null;
 		
 		for (int i=0; i < args.length ; i+=2) {
 			if (PNFOLDER.equals(args[i])) {
 				pwd = args[i+1];
 			} else if (EXAMINATION.equals(args[i])) {
-				if ( "StateSpace".equals(args[i+1])) {
-					examination = ExamType.STATESPACE;
-				}
+				examination = args[i+1]; 
 			}
 		}
 		
-				
-		String pnmlff = pwd + "/model.pnml"; 		
-		URI fileuri = new java.net.URI("file://" +pnmlff.replace('\\', '/'));
+		transformPNML(pwd);
 		
-		getLog().info("Handling file : " + fileuri.getPath());
-		
-		PnmlToGalTransformer trans = new PnmlToGalTransformer();
-		Specification spec = trans.transform(fileuri);
-		if (trans.getOrder() != null) {
-			getLog().info("Applying decomposition : " + trans.getOrder());
-			CompositeBuilder.getInstance().decomposeWithOrder((GALTypeDeclaration) spec.getTypes().get(0), trans.getOrder());
-		} 
-		Simplifier.simplify(spec);
-		
-		String outpath = fileuri.getPath() + ".gal";
-
+		String outpath ;
 		if (spec.getMain() == null) {
 			spec.setMain(spec.getTypes().get(spec.getTypes().size()-1));
 		}
-		SerializationUtil.systemToFile(spec, outpath);
+
+		CommandLine cl =null;
+		if (examination.equals("StateSpace")) {
+			outpath =  pwd + "/model.pnml.gal";
+			
+			// compute constants
+			Support constants = GALRewriter.flatten(spec, true);
+				
+			SerializationUtil.systemToFile(spec, outpath);
+
+			cl = buildCommandLine(outpath);
+		} else if (examination.startsWith("Reachability")) {
+
+			//			Properties props = fr.lip6.move.gal.logic.util.SerializationUtil.fileToProperties(file.getLocationURI().getPath().toString());
+			// TODO : is the copy really useful ?
+			String propff = pwd +"/" +  examination + ".xml";
+			Properties props = PropertyParser.fileToProperties(propff , EcoreUtil.copy(spec));
+			
+			Specification specWithProps = ToGalTransformer.toGal(props);
+			
+			if (order != null) {
+				CompositeBuilder.getInstance().decomposeWithOrder((GALTypeDeclaration) specWithProps.getTypes().get(0), order.clone());
+			}
+			// compute constants
+			Support constants = GALRewriter.flatten(specWithProps, true);
+			ArrayList<Property> properties = new ArrayList<Property>(specWithProps.getProperties());
+
+			outpath = pwd +"/" + examination + ".gal" ;
+			specWithProps.getProperties().clear();
+			fr.lip6.move.serialization.SerializationUtil.systemToFile(specWithProps, outpath);
+			cl = buildCommandLine(outpath);
+			
+			// test for and handle properties		
+			if (! properties.isEmpty()) {
+
+				// We will put properties in a file
+				String propPath =pwd + "/" + examination + ".prop";
+
+				
+				// create file
+				File propFile = new File(propPath);
+				PrintWriter out = new PrintWriter(propFile);
+
+				// first line is removed anyway : reference source model
+				out.write("import  \"" + outpath + "\";\n");
+
+				// Add one line per property
+				for (Property prop : properties) {
+					out.write(ToStringUtils.getTextString(prop) + "\n") ;
+				}
+				// 
+				out.flush();
+				out.close();
+
+				// property file arguments
+				cl.addArg("-reachable-file");
+				cl.addArg(propFile.getName());
+
+				cl.addArg("--nowitness");
+			}
+			
+		}
+		cl.setWorkingDir(new File(pwd));
 		
-		CommandLine cl = buildCommandLine(outpath);
+		
+		
+		boolean withStructure = applyOrder(); 
+		
+		Simplifier.simplify(spec);
+		
+		
+
+		
 
 		try {		
 			run(3500, cl);
@@ -100,14 +164,36 @@ public class Application implements IApplication {
 		
 		for (String line : stdOutput.toString().split("\\n")) {
 			if ( line.matches("\\s*Total reachable state count.*")) {
-				System.out.println( "STATE_SPACE STATES " + line.split(":")[1] + " TECHNIQUES DECISION_DIAGRAMS USE_NUPN TOPOLOGICAL");
+				System.out.println( "STATE_SPACE STATES " + line.split(":")[1] + " TECHNIQUES DECISION_DIAGRAMS TOPOLOGICAL " + (withStructure?"USE_NUPN":"") );
+			}
+			if ( line.matches(".*"+examination+".*")) {
+				System.out.println(line);
+				String res;
+				String pname = line.split(" ")[2];
+				if (line.contains("does not hold")) {
+					res = "FALSE";
+				} else { 
+					res = "TRUE";
+				}
+				System.out.println("FORMULA "+pname+ " "+ res + " TECHNIQUES DECISION_DIAGRAMS TOPOLOGICAL " + (withStructure?"USE_NUPN":""));
 			}
 		}
 		
-	//	getLog().info(stdOutput.toString());
-	//	getLog().warning(errorOutput.toString());
+		getLog().info(stdOutput.toString());
+		getLog().warning(errorOutput.toString());
 		
 		return IApplication.EXIT_OK;
+	}
+
+
+	private boolean applyOrder() {
+		if (order != null) {
+			getLog().info("Applying decomposition ");
+			getLog().fine(order.toString());
+			CompositeBuilder.getInstance().decomposeWithOrder((GALTypeDeclaration) spec.getTypes().get(0), order);
+			return true;
+		}
+		return false;
 	}
 
 
@@ -152,6 +238,66 @@ public class Application implements IApplication {
 	private static Logger getLog() {
 		return Logger.getLogger("fr.lip6.move.gal");
 		
+	}
+	
+	
+	private Specification spec;
+	private IOrder order;
+
+	
+	/**
+	 * Sets the spec and order attributes.
+	 * @param folder
+	 * @throws Exception
+	 */
+	private void transformPNML(String folder) throws Exception {
+		File ff = new File(folder+ "/"+ "model.pnml");
+		if (ff != null && ff.exists()) {
+			getLog().info("Parsing pnml file : " + ff.getAbsolutePath());
+
+			PnmlToGalTransformer trans = new PnmlToGalTransformer();
+			spec = trans.transform(ff.toURI());
+			order = trans.getOrder();
+			// SerializationUtil.systemToFile(spec, ff.getPath() + ".gal");
+		}
+	}
+	
+	private void buildProperty (File file) throws IOException {
+		if (file.getName().endsWith(".xml") && file.getName().contains("Reachability") ) {
+			
+			// normal case
+			{
+//				Properties props = fr.lip6.move.gal.logic.util.SerializationUtil.fileToProperties(file.getLocationURI().getPath().toString());
+				// TODO : is the copy really useful ?
+				Properties props = PropertyParser.fileToProperties(file.getPath().toString(), EcoreUtil.copy(spec));
+				
+				Specification specWithProps = ToGalTransformer.toGal(props);
+
+				if (order != null) {
+					CompositeBuilder.getInstance().decomposeWithOrder((GALTypeDeclaration) specWithProps.getTypes().get(0), order.clone());
+				}
+				// compute constants
+				Support constants = GALRewriter.flatten(specWithProps, true);
+
+				File galout = new File( file.getParent() +"/" + file.getName().replace(".xml", ".gal"));
+				fr.lip6.move.serialization.SerializationUtil.systemToFile(specWithProps, galout.getAbsolutePath());
+			} 
+			// Abstraction case 
+			if (file.getParent().contains("-COL-")) {
+				ToGalTransformer.setWithAbstractColors(true);
+				Properties props = fr.lip6.move.gal.logic.util.SerializationUtil.fileToProperties(file.getAbsolutePath());
+
+				Specification specnocol = ToGalTransformer.toGal(props);
+				Instantiator.instantiateParametersWithAbstractColors(specnocol);
+				GALRewriter.flatten(specnocol, true);
+
+				File galout = new File( file.getParent() +"/" + file.getName().replace(".xml", ".nocol.gal"));
+				fr.lip6.move.serialization.SerializationUtil.systemToFile(specnocol, galout.getAbsolutePath());
+
+				ToGalTransformer.setWithAbstractColors(false);
+			}
+
+		}		
 	}
 
 	/* (non-Javadoc)
