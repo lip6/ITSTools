924d5081949afeb25f6063553e3063340493f4cb
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-05-07 12:25:05 +0000

    simplify / kill activator

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/Activator.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/Activator.java
@@ -1,62 +1,14 @@
 package fr.lip6.move.gal.gal2smt;
 
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
-
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator  {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "fr.lip6.move.gal.gal2smt"; //$NON-NLS-1$
 
-	// The shared instance
-	private static Activator plugin;
 	
 	public final static String PATH = "/home/lordkodo/MASTER/PSAR/z3-4.4.0.55ca6ce44b2f-x64-ubuntu-14.04/bin/z3";
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
 }
