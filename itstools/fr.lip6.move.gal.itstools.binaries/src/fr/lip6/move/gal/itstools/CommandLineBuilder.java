dbe4bc25362458004352357a2fbc6b512c6ed10c
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-05-11 16:46:28 +0000

    imported in : support for building a command line and running it without involving any third party libraries (just Java)

---
+++ itstools/fr.lip6.move.gal.itstools.binaries/src/fr/lip6/move/gal/itstools/CommandLineBuilder.java
@@ -0,0 +1,55 @@
+package fr.lip6.move.gal.itstools;
+
+import java.io.IOException;
+
+import fr.lip6.move.gal.itstools.BinaryToolsPlugin.Tool;
+
+public class CommandLineBuilder {
+
+	
+	private CommandLine cl;
+
+	
+	public CommandLineBuilder(Tool tool) throws IOException {
+		cl = new CommandLine();
+
+		// Path to ITS-reach exe				
+		String itsReachPath = BinaryToolsPlugin.getProgramURI(Tool.reach).getPath().toString();
+		cl.addArg(itsReachPath);
+
+		// 2 Gig
+		cl.addArg("--gc-threshold");
+		cl.addArg("2000000");
+		// quiet
+		cl.addArg("--quiet");
+	}
+	
+	public void setModelFile(String modelff) {
+		cl.addArg("-i");
+		cl.addArg(modelff);
+	}
+	
+	public void setModelType(String type) {
+		cl.addArg("-t");
+		cl.addArg(type);
+	}
+
+	public void setReachableProp (String prop) {
+		cl.addArg("-reachable");
+		cl.addArg(prop);
+	}
+
+	public void setPropFile (String propff) {
+		cl.addArg("-reachable-file");
+		cl.addArg(propff);
+	}
+	
+	public CommandLine getCommandLine() {
+		return cl;
+	}
+
+	public void setTrace(String trace) {
+		cl.addArg("-trace");
+		cl.addArg(trace);		
+	}
+}
