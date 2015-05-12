5b7de9c8bc6ec6a3723d46c1f527cbbc54f12e9d
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-05-12 16:51:18 +0000

    no hardcoded path anymore

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/Activator.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/Activator.java
@@ -8,7 +8,4 @@ public class Activator  {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "fr.lip6.move.gal.gal2smt"; //$NON-NLS-1$
 
-	
-	public final static String PATH = "/home/lordkodo/MASTER/PSAR/z3-4.4.0.55ca6ce44b2f-x64-ubuntu-14.04/bin/z3";
-
 }
