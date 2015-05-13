859b54cc49714a25c4d4bc4977b8677114d34dcd
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-05-13 20:57:20 +0000

    always just look for one state to satisfy predicate

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/PropertySMT.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/PropertySMT.java
@@ -8,7 +8,6 @@ import org.smtlib.IExpr;
 
 import fr.lip6.move.gal.LogicProp;
 import fr.lip6.move.gal.Property;
-import fr.lip6.move.gal.ReachableProp;
 
 public class PropertySMT {
 	private static IExpr.IFactory efactory = GalToSMT.getSMT().smtConfig.exprFactory;
@@ -21,14 +20,16 @@ public class PropertySMT {
 		
 		for (int i = 0; i < depth; i++) {
 			index = efactory.numeral(i);
+			// expression built varies depending on type of prop
+			// SAT = trace to state satisfying P for reach (verdict TRUE)
+			// SAT = trace to c-e satisfying !P for invariant (verdict FALSE)
+			// SAT = trace to c-e satisfying P for never (verdict FALSE)
 			exprs.add(ExpressionTranslator.translateProperty(body, index));
 		}
 		
-		if (body instanceof ReachableProp) {
-			commands.add(new org.smtlib.command.C_assert(efactory.fcn(efactory.symbol("or"), exprs)));	
-		}else{
-			commands.add(new org.smtlib.command.C_assert(efactory.fcn(efactory.symbol("and"), exprs)));
-		}
+		// test these expressions in any (or) state
+		commands.add(new org.smtlib.command.C_assert(efactory.fcn(efactory.symbol("or"), exprs)));	
+		
 	}
 
 }
