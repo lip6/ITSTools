087528fff2afe912fe695b4b7b0b1b8cedf8769c
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-05-14 14:46:35 +0000

    BUGFIX : for some reason true was added into the call semantics for free FEATURE : piggyback the traversal to collect a label -> trans list map

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/CallsSMT.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/CallsSMT.java
@@ -3,6 +3,7 @@ package fr.lip6.move.gal.gal2smt;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.EList;
 import org.smtlib.IExpr;
@@ -15,18 +16,30 @@ public class CallsSMT {
 	private static IExpr.IFactory efactory = GalToSMT.getSMT().smtConfig.exprFactory;	
 	private static HashMap<String, ArrayList<ISymbol>> transitions = new HashMap<String, ArrayList<ISymbol>>();
 	
-	public static void fillTransitionMap(EList<Transition> transitionsList){
-		for (Transition transit : transitionsList) {
+	public static Map<String, List<Transition>> fillTransitionMap(EList<Transition> transitionsList){
+		Map<String, List<Transition>> res = new HashMap<String, List<Transition>>();
+		
+		for (Transition trans : transitionsList) {
 			String label = "";
-			if(transit.getLabel() != null) label = transit.getLabel().getName();
-			addTransition(label, transit);
+			if(trans.getLabel() != null) {
+				label = trans.getLabel().getName();
+				List<Transition> list = res.get(label);
+				if (list == null) {
+					list = new ArrayList<Transition>();
+					res.put(label, list);
+				}
+				list.add(trans);
+			}
+			addTransition(label, trans);
 		}
+		return res ;
 	}
 	
 	public static IExpr generateSelfCalls(Label label, IExpr indexNow){
 		String lbl = "";
 		List<IExpr> exprs = new ArrayList<IExpr>();
-		exprs.add(efactory.symbol("true"));
+		// do not add this id to the call !
+		//exprs.add(efactory.symbol("true"));
 		
 		if(label != null) 
 			lbl = label.getName();
