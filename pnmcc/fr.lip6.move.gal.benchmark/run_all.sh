a4c0337714b92421ccc2c62cd3d3a4749c1e6c50
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-06-01 13:26:02 +0000

    wrap it all up

---
+++ pnmcc/fr.lip6.move.gal.benchmark/run_all.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+set -e
+
+
+./install_all.sh
+
+echo "##teamcity[testSuiteStarted name='PNMCC perfs']"
+
+for i in oracle/*.out ; do
+    ./run_test.pl $i ;
+done;
+
+echo "##teamcity[testSuiteFinished name='PNMCC perfs']"
+
