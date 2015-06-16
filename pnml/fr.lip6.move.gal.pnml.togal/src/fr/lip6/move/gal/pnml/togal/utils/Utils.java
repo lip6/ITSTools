67499393976960c41f915d35adee6b206d3b636e
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-06-16 17:02:59 +0000

    convenience concat of two Iterables without copying them

--- pnml/fr.lip6.move.gal.pnml.togal/src/fr/lip6/move/gal/pnml/togal/utils/Utils.java
+++ pnml/fr.lip6.move.gal.pnml.togal/src/fr/lip6/move/gal/pnml/togal/utils/Utils.java
@@ -1,5 +1,8 @@
 package fr.lip6.move.gal.pnml.togal.utils;
 
+import java.util.Iterator;
+
+
 public class Utils {
 	public static String normalizeName(String text) {
 		String res = text.replace(' ', '_');
@@ -9,6 +12,39 @@ public class Utils {
 		res = res.replace('=', '_');
 		
 		return res;
+	}
+
+	public static <T>
+	Iterable<T> concat(final Iterable<T> in1, final Iterable<T> in2) {
+		return new Iterable<T>() {
+			
+			@Override
+			public Iterator<T> iterator() {
+				return new Iterator<T>() {
+					
+					private Iterator<T> it1 = in1.iterator();
+					private Iterator<T> it2 = in2.iterator();
+					
+					@Override
+					public boolean hasNext() {
+						return it1.hasNext() || it2.hasNext();
+					}
+
+					@Override
+					public T next() {
+						if (it1.hasNext()) {
+							return it1.next();
+						}
+						return it2.next();
+					}
+
+					@Override
+					public void remove() {
+						throw new UnsupportedOperationException();
+					}
+				};
+			}
+		};
 	}
 
 }
