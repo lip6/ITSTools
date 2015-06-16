90812df10530f465686617f5b0c12cbd711c2718
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-06-16 02:09:00 +0000

    invocation mantras

--- pnmcc/fr.lip6.move.gal.benchmark/collect_data.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/collect_data.sh
@@ -10,3 +10,11 @@ for i in "$@" ; do
     $CMD >> $OUT
 done
 
+# for i in `ls -1 *SS* | sed 's/-SS.out//'` ; do cat verdicts.csv | grep "^$i" | grep ReachabilityFireabilitySimple ; done
+
+# grab lines with good confidence level
+# for i in `ls -1 *SS* | sed 's/-SS.out//'` ; do cat verdicts.csv | grep "^$i" | grep ReachabilityFireabilitySimple | ../csv_to_control.pl ; done
+
+
+# adapt PT results to COL
+# for i in *RFS* ; do j=`echo $i | sed 's/PT/COL/'` ; if [ ! -f $j ]; then k=`echo $j | sed 's/RFS/SS/'` ; if [ -f $k ]; then cat $i | sed 's/PT/COL/' | sed 's/TECHNIQUES/TECHNIQUES FROM_PT/' > $j ; fi ; fi ;done ;
