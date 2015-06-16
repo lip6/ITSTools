e4463e7a4fe0ba738299ea2e1aa896c4487c42f2
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-06-16 13:35:25 +0000

    make sure stack size is set appropriately high, many examples need a large stack to run.

--- pnmcc/fr.lip6.move.gal.benchmark/runeclipse.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/runeclipse.sh
@@ -2,6 +2,8 @@
 
 set -x
 
-java -Dosgi.requiredJavaVersion=1.6 -XX:MaxPermSize=512m -Xms40m -Xmx8192m -Declipse.pde.launch=true -Dfile.encoding=UTF-8 -classpath $BINDIR/eclipse/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar org.eclipse.equinox.launcher.Main -application fr.lip6.move.gal.application.pnmcc -data $BINDIR/workspace -os linux -ws gtk -arch x86_64 -nl en_US -consoleLog -pnfolder $1 -examination $2 -z3path $BINDIR/z3/bin/z3 -its
+ulimit -s 65536
+
+java -Dosgi.requiredJavaVersion=1.6 -XX:MaxPermSize=512m -Xss8m -Xms40m -Xmx8192m -Declipse.pde.launch=true -Dfile.encoding=UTF-8 -classpath $BINDIR/eclipse/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar org.eclipse.equinox.launcher.Main -application fr.lip6.move.gal.application.pnmcc -data $BINDIR/workspace -os linux -ws gtk -arch x86_64 -nl en_US -consoleLog -pnfolder $1 -examination $2 -z3path $BINDIR/z3/bin/z3 -its
 #-configuration file:/home/mcc/workspace/.metadata/.plugins/org.eclipse.pde.core/fr.lip6.move.gal.application/ 
 #-dev file:/data/ythierry/workspaces/luna2/.metadata/.plugins/org.eclipse.pde.core/fr.lip6.move.gal.application/dev.properties 
