15887f5e1b1126e2ac3878f39b87ade6f0d8fed5
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-07-06 12:10:59 +0000

    Flatten action is very verbose

--- fr.lip6.move.gal.ui/src/fr/lip6/move/gal/flatten/popup/actions/FlattenAction.java
+++ fr.lip6.move.gal.ui/src/fr/lip6/move/gal/flatten/popup/actions/FlattenAction.java
@@ -2,6 +2,8 @@ package fr.lip6.move.gal.flatten.popup.actions;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import fr.lip6.move.gal.Specification;
 import fr.lip6.move.gal.instantiate.GALRewriter;
@@ -16,7 +18,8 @@ public class FlattenAction extends GalAction {
 	}
 
 	@Override
-	public void workWithSystem(Specification spec) throws Exception {				
+	public void workWithSystem(Specification spec) throws Exception {
+		Logger.getLogger("fr.lip6.move.gal").setLevel(Level.FINE);
 		GALRewriter.flatten(spec, true);	
 	}
 	
