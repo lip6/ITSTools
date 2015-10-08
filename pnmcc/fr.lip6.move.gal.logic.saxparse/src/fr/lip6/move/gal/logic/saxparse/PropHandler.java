9e26e3052075c9ab5da6c00bf8eafbd9d5ec1f50
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-10-08 16:26:45 +0000

    typos, better logging

--- pnmcc/fr.lip6.move.gal.logic.saxparse/src/fr/lip6/move/gal/logic/saxparse/PropHandler.java
+++ pnmcc/fr.lip6.move.gal.logic.saxparse/src/fr/lip6/move/gal/logic/saxparse/PropHandler.java
@@ -81,7 +81,7 @@ public class PropHandler extends DefaultHandler {
 			// NOTHING
 		} else if ("finally".equals(baliseName)) { //$NON-NLS-1$
 			// NOTHING
-		} else if ("exits-path".equals(baliseName)) { //$NON-NLS-1$
+		} else if ("exists-path".equals(baliseName)) { //$NON-NLS-1$
 			// NOTHING
 		} else if ("negation".equals(baliseName)) { //$NON-NLS-1$
 			// NOTHING
@@ -107,6 +107,8 @@ public class PropHandler extends DefaultHandler {
 			dotext = true;
 		} else if ("transition".equals(baliseName)) { //$NON-NLS-1$
 			dotext = true;
+		} else {
+			getLog().warning("Unexpected XML tag in property file :" + baliseName);
 		}
 	}
 
