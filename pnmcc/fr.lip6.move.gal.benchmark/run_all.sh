bb7f7ecf60d7a5ed120a899c5b04880b722d767a
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-10-12 17:13:27 +0000

    split SMT from ITS tests

--- pnmcc/fr.lip6.move.gal.benchmark/run_all.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/run_all.sh
@@ -5,14 +5,6 @@ set -e
 
 ./install_all.sh
 
-echo "##teamcity[testSuiteStarted name='PNMCC SMT/Z3 perfs']"
-
-for i in smt/*.out ; do
-    ./run_test.pl $i -smt;
-done;
-
-echo "##teamcity[testSuiteFinished name='PNMCC SMT/Z3 perfs']"
-
 
 echo "##teamcity[testSuiteStarted name='PNMCC perfs']"
 
