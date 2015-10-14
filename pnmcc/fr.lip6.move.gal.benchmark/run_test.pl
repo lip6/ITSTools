ab92dece4ce9fb9185ef173b93c1bfa4b8821093
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-10-14 14:33:47 +0000

    cleanup of legacy code

--- pnmcc/fr.lip6.move.gal.benchmark/run_test.pl
+++ pnmcc/fr.lip6.move.gal.benchmark/run_test.pl
@@ -17,88 +17,61 @@ chomp $call;
 $call= $call." ".$ARGV[1];
 
 my $header;
-my @nominals = ();
 my %verdicts = ();
 
 while (my $line = <IN>) {
-	if ($line =~ /STATE\_SPACE/ || $line =~ /FORMULA/ ) {
-	  my @words = split /\s+/,$line;
-	  $verdicts{@words[1]} = @words[2];
-	}
-
+  if ($line =~ /STATE\_SPACE/ || $line =~ /FORMULA/ ) {
+    my @words = split /\s+/,$line;
     # Note that the final reported Statistic is what will be taken
-#    if ($line =~ /Model ,\|S\| /) {
-#	$header = $line;
-#	$line = <IN> or die "Unexpected end of file after stats readout";
-#	chomp $line;
-#	@nominal = split (/\,/,$line);
-#    }
+    $verdicts{@words[1]} = @words[2];
+  }
 }
 
 close IN;
 
-my $nbtests = $#nominals + 1 + keys(%verdicts);
+my $nbtests = keys(%verdicts);
 
 print "Test : $title ; ".$nbtests." values to test \n";
-# print "@outputs\n";
+
 print "Control values :\n";
 foreach my $key (sort keys %verdicts) {
   print "$key=$verdicts{$key}\n";
 }
- 
 
 if ($nbtests == 0) {
-    print "\n##teamcity[testFailed name='$title' message='Oracle file empty or otherwise incorrect' details='Was reading : $title' expected='greater than 0' actual='$nbtests'] \n";
+  print "\n##teamcity[testStarted name='$title-oracle-integrity']\n";
+  print "\n##teamcity[testFailed name='$title' message='Oracle file empty or otherwise incorrect' details='Was reading : $title' expected='greater than 0' actual='$nbtests'] \n";
+  print "\n##teamcity[testFinished name='$title-oracle-integrity']\n";
+  exit 1;
 }
 
-
 # Now run the tool
-
 my $tmpfile = "$ARGV[0].tmp";
 
-
 # print "syscalling : $call \n";
-
-
-my @tested;
-
-my @outputs = ();
 my %formouts = ();
 
 open IN, "($call) |" or die "An exception was raised when attempting to run "+$call+"\n";
 while (my $line = <IN>) {
   print $line;
-	if ($line =~ /STATE\_SPACE/ || $line =~ /FORMULA/ )  {
-	  my @words = split /\s+/,$line;
-	  $formouts{@words[1]} = @words[2];
-		
-	  my $out = @words[2];
-	  my $exp =  $verdicts{@words[1]};
-	  my $tname = $title."-".@words[1];
-	  print "##teamcity[testStarted name='$tname']\n";
-	  if (! defined $exp) {
-	      print "\n Formula @words[1] : no verdict in oracle !! expected/real : $exp /  $out\n";
-	      print "\n##teamcity[testFailed name='$tname' message='oracle incomplete : formula ( @words[1] )' details='' expected='$exp' actual='$out'] \n";
-	  } elsif ( $out !~ /$exp/ ) {
-	      print "\n##teamcity[testFailed name='$tname$' message='regression detected : formula ( @words[1] )' details='' expected='$exp' actual='$out'] \n";
-	  } else {
-	      print "\n Formula @words[1] test succesful expected/real : $exp /  $out\n";
-	  }
-	  print "##teamcity[testFinished name='$tname']\n";
-
-	}
-
-
-##  print "$line";
-#  push (@outputs,$line);
-#  if ($line =~ /Model ,\|S\| /) {
-#    $line = <IN> or die "Unexpected end of file after stats readout";
-#    push (@outputs,$line);
-#    chomp $line;
-#    @tested = split (/\,/,$line);
-
-##    last;
-#  }
+  if ($line =~ /STATE\_SPACE/ || $line =~ /FORMULA/ )  {
+    my @words = split /\s+/,$line;
+    $formouts{@words[1]} = @words[2];
+    
+    my $out = @words[2];
+    my $exp =  $verdicts{@words[1]};
+    my $tname = $title."-".@words[1];
+    print "##teamcity[testStarted name='$tname']\n";
+    if (! defined $exp) {
+      print "\n Formula @words[1] : no verdict in oracle !! expected/real : $exp /  $out\n";
+      print "\n##teamcity[testFailed name='$tname' message='oracle incomplete : formula ( @words[1] )' details='' expected='$exp' actual='$out'] \n";
+    } elsif ( $out !~ /$exp/ ) {
+      print "\n##teamcity[testFailed name='$tname$' message='regression detected : formula ( @words[1] )' details='' expected='$exp' actual='$out'] \n";
+    } else {
+      print "\n Formula @words[1] test succesful expected/real : $exp /  $out\n";
+    }
+    print "##teamcity[testFinished name='$tname']\n";
+  }
 }
 
 close IN;
@@ -108,71 +81,15 @@ foreach my $key (sort keys %formouts) {
   print "$key=$formouts{$key}\n";
 }
 
-
-my $o = $#outputs;
-my $e = $#nominals;
-if ( $o != $e ) {
-    print "\n##teamcity[testFailed name='$title' message='regression detected : less results than expected ( $o / $e )' details='' expected='$e' actual='$o'] \n";
-}
-
 $o = keys (%formouts);
 $e = keys (%verdicts);
 if ( $o != $e ) {
-    print "\n##teamcity[testFailed name='$title' message='regression detected : less results than expected ( $o / $e )' details='' expected='$e' actual='$o'] \n";
-  } elsif ($o > 0) {
-    print "Test successful : $title";
-  }
-
-
-#foreach my $i  (0..$#outputs) {
-#    my $lo = @outputs[$i];
-#    my $ln = @nominals[$i];
-#
-#    print "Test : $title ; (output $i) \n"; 
-#    print "Output  : $lo \n";
-#    print "Control : $ln \n";
-#
-#    if ($lo =~ /STATE_SPACE/) {
-#	my $sso =  (split /\s+/, $lo) [2]; 
-#	my $ssn =  (split /\s+/, $ln) [2]; 
-#
-#	if ( $sso !~ /$ssn/ ) {
-#	    print "\n##teamcity[testFailed name='$title' message='regression detected on STATE_SPACE' details='' expected='$ssn' actual='$sso'] \n";
-#	} else {
-##	print "##teamcity[buildStatisticValue key='testDuration' value='@tested[2]']\n";
-##	print "##teamcity[buildStatisticValue key='testMemory' value='@tested[3]']\n";
-#	    print "Test successful : $title \n";
-#	}
-#    } else {
-#	print "please write an oracle for this case, it is not handled.";
-#    }
-#}
-#
-
-
-
-#if ($call =~ /-reachable/) {
-#    # so state count may not be accurate if we got interruption. However 0 != positive integer
-#    if ( (@nominal[1]==0 && @tested[1] > 0 ) || (@nominal[1]>0 && @tested[1]==0 ) ) {
-#	  print "\n##teamcity[testFailed name='$title' message='reachability test regression detected' details='' expected='@nominal[1]' actual='@tested[1]'] \n";
-#	  print "Expected :  @nominal[1]  Obtained :  @tested[1] \n";
-#    } else {
-#	print "##teamcity[buildStatisticValue key='testDuration' value='@tested[2]']\n";
-#	print "##teamcity[buildStatisticValue key='testMemory' value='@tested[3]']\n";
-#	print "Test successful : $title \n";
-#	print "Control Values/Obtained : \n$title\n@nominal\n@tested\n";
-#    }
-#} else {
-#    if ( @nominal[1] != @tested[1] ) {
-#	print "\n##teamcity[testFailed name='$title' message='regression detected' details='' expected='@nominal[1]' actual='@tested[1]'] \n";
-#	print "Expected :  @nominal[1]  Obtained :  @tested[1] \n";
-#    } else {
-#	print "##teamcity[buildStatisticValue key='testDuration' value='@tested[2]']\n";
-#	print "##teamcity[buildStatisticValue key='testMemory' value='@tested[3]']\n";
-#	print "Test successful : $title \n";
-#	print "Control Values/Obtained : \n$title\n@nominal\n@tested\n";
-#    }
-#}
+  print "\n##teamcity[testStarted name='$title-all']\n";
+  print "\n##teamcity[testFailed name='$title' message='regression detected : less results than expected ( $o / $e )' details='' expected='$e' actual='$o'] \n";
+  print "\n##teamcity[testFinished name='$title-all']\n";
+} elsif ($o > 0) {
+  print "All $o tests successful in suite : $title";
+}
 
 print "##teamcity[testSuiteFinished name='$title']\n";
 
