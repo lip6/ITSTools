9b3de1faf9af7ede04cb41d175918f9eb14f93d8
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-10-21 21:32:09 +0000

    label code deactivated atm, in favor of throwing exceptions. Possibly works.

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/BMCSolver.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/BMCSolver.java
@@ -1,26 +1,26 @@
 package fr.lip6.move.gal.gal2smt.bmc;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 
-import org.eclipse.emf.common.util.Enumerator;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.smtlib.IExpr.ISymbol;
 import org.smtlib.IExpr;
+import org.smtlib.IPrinter;
 import org.smtlib.IResponse;
 import org.smtlib.ISolver;
 import org.smtlib.ISort;
 import org.smtlib.IExpr.IFactory;
 import org.smtlib.ISort.IApplication;
 import org.smtlib.SMT.Configuration;
+import org.smtlib.command.C_assert;
 import org.smtlib.command.C_define_fun;
 import org.smtlib.impl.Script;
 import org.smtlib.impl.Sort;
@@ -31,7 +31,7 @@ import fr.lip6.move.gal.Assignment;
 import fr.lip6.move.gal.BooleanExpression;
 import fr.lip6.move.gal.Constant;
 import fr.lip6.move.gal.GALTypeDeclaration;
-import fr.lip6.move.gal.IntExpression;
+import fr.lip6.move.gal.InvariantProp;
 import fr.lip6.move.gal.LabelCall;
 import fr.lip6.move.gal.Property;
 import fr.lip6.move.gal.SelfCall;
@@ -40,21 +40,20 @@ import fr.lip6.move.gal.Statement;
 import fr.lip6.move.gal.Transition;
 import fr.lip6.move.gal.True;
 import fr.lip6.move.gal.Variable;
-import fr.lip6.move.gal.gal2smt.ArraySMT;
 import fr.lip6.move.gal.gal2smt.ExpressionTranslator;
-import fr.lip6.move.gal.gal2smt.GalToSMT;
 import fr.lip6.move.gal.gal2smt.Result;
 import fr.lip6.move.gal.gal2smt.Solver;
-import fr.lip6.move.gal.gal2smt.VariableSMT;
 
 public class BMCSolver implements IBMCSolver {
 
 
+	private static final String NEXT = "_Next__";
 	private final Solver engine;
 	private final Configuration conf;
 	private ISolver solver;
 	private final IFactory efactory;
 	private final ISort.IFactory sortfactory ;
+	private int depth = 0;
 
 	public BMCSolver(Configuration smtConfig, Solver engine) {
 		this.engine = engine;
@@ -70,32 +69,32 @@ public class BMCSolver implements IBMCSolver {
 	 */
 	@Override
 	public void init(Specification spec, boolean withInitialState) {
-		
+
 		solver = engine.getSolver(conf);
 		// start the solver
 		IResponse err = solver.start();
 		if (err.isError()) {
 			throw new RuntimeException("Could not start solver "+ engine+" from path "+ conf.executable);
 		}
-		
+
 		// declare logic + headers
 		Script script = new Script();
-		
-		
+
+
 		// Logic + options
 		script.commands().add(new org.smtlib.command.C_set_option(efactory .keyword(":produce-models"), efactory.symbol("true")));
 		script.commands().add(new org.smtlib.command.C_set_logic(efactory.symbol("QF_AUFLIA")));
 
 		if (spec.getMain() instanceof GALTypeDeclaration) {
 			GALTypeDeclaration gal = (GALTypeDeclaration) spec.getMain();
-			
+
 			// integer sort
 			ISort ints = sortfactory.createSortExpression(efactory.symbol("Int"));
 			// an array, indexed by integers, containing integers : (Array Int Int) 
 			IApplication arraySort = sortfactory.createSortExpression(efactory.symbol("Array"), ints, ints);
 			// an array, indexed by ints of such arrays : (Array Int (Array Int Int)) 
 			IApplication arrayArraySort = sortfactory.createSortExpression(efactory.symbol("Array"), ints, arraySort);
-			
+
 			// Declare variables
 			/* VARIABLES */				
 			for (Variable var : gal.getVariables()) {
@@ -106,7 +105,7 @@ public class BMCSolver implements IBMCSolver {
 								Collections.EMPTY_LIST,
 								arraySort								
 								)
-						
+
 						);
 			}
 			/* ARRAYS */
@@ -118,10 +117,10 @@ public class BMCSolver implements IBMCSolver {
 								Collections.EMPTY_LIST,
 								arrayArraySort								
 								)
-						
+
 						);
 			}
-			
+
 			/* TRANS */
 			// collect labels
 			Map<String, List<Transition>> labmap = new HashMap<String, List<Transition>>();
@@ -138,111 +137,118 @@ public class BMCSolver implements IBMCSolver {
 				}
 				list.add(tr);
 			}
-			// compute an order on labels respecting call order (callees before callers) 
-			Set<String> todo = new LinkedHashSet<String>();
-			handlePrecedence(gal,todo,labmap,"");
-			
-			// iterate over labels and output transitions
-			for (String lab : todo) {
-				// define a boolean function with single parameter (step) for each transition
-				ISymbol sstep = efactory.symbol("step");
-				IExpr snext = efactory.fcn(efactory.symbol("+"),sstep,efactory.numeral("1"));
-				for (Transition tr : labmap.get(lab)) {
-					List<IExpr> conds = new ArrayList<IExpr>();
-					
-					if (! (tr.getGuard() instanceof True)) {
-						conds.add(ExpressionTranslator.translateBool(tr.getGuard(), sstep));
-					}
-					// to keep track of modified vars and array cells
-					Set<Variable> vars = new HashSet<Variable>();
-					Map<ArrayPrefix, Set<Integer>> arrays = new HashMap<ArrayPrefix, Set<Integer>>();
 
-					// handle statements
-					for (Statement st : tr.getActions()) {
-						
-						if (st instanceof Assignment) {
-							Assignment ass = (Assignment) st;
 
-							IExpr lhs = ExpressionTranslator.translate(ass.getLeft(), snext);
-							IExpr rhs = ExpressionTranslator.translate(ass.getRight(), sstep);							
-							if (ass.getType() == AssignType.INCR ) {
-								rhs = efactory.fcn(efactory.symbol("+"), 
-										ExpressionTranslator.translate(ass.getLeft(), sstep),
-										rhs
-										);							
-							} else if (ass.getType() == AssignType.DECR ) {
-								rhs = efactory.fcn(efactory.symbol("-"), 										
-										ExpressionTranslator.translate(ass.getLeft(), sstep),
-										rhs
-										);							
-							}
-							conds.add(efactory.fcn(efactory.symbol("="), lhs, rhs));
-							
-							// we have modifications we need to note
-							if (ass.getLeft().getIndex() == null) {
-								if (! vars.add((Variable) ass.getLeft().getRef())) {
-									throw new RuntimeException("Multiple assignments to a single variable are not supported in SMT translation currently. Was translating transition " + tr.getName());
-								}
-							} else {
-								Set<Integer> hit = arrays.get(ass.getLeft().getRef());
-								if (hit == null) { 
-									hit = new HashSet<Integer>(); 
-									arrays.put((ArrayPrefix) ass.getLeft().getRef(), hit);
-								}
-								if (! hit.add(((Constant) ass.getLeft().getIndex()).getValue())) {
-									throw new RuntimeException("Multiple assignments to a single variable are not supported in SMT translation currently. Was translating transition " + tr.getName());
-								}
-							}
-						} else if (st instanceof SelfCall) {
-							SelfCall scall = (SelfCall) st;
-							
-						}
-						
-						
-					}
-					
-					
-					
-					// add constraints on unmodified variables
-					for (Variable  var : gal.getVariables()) {
-						if (! vars.contains(var)) {
-							conds.add(efactory.fcn(efactory.symbol("="), 
-									accessVar(var, sstep),
-									accessVar(var, snext)));
-						}
-					}
-					
-					/* Array cell case */
-					for (ArrayPrefix array : gal.getArrays()) {
-						Set<Integer> indexes = arrays.get(array);
-						int size = ((Constant) array.getSize()).getValue();
-						/* On ajoute tout les index */
-						for (int i = 0; i < size; i++) {
-							if(indexes == null || !indexes.contains(i)){
-								conds.add(efactory.fcn(efactory.symbol("="), 
-										accessArray(array, i, sstep),
-										accessArray(array, i, sstep)
-										));
-							}
-						}
-					}
-					// 
-					C_define_fun deftr = new org.smtlib.command.C_define_fun(
-							efactory.symbol(tr.getName()),    // name
-							Collections.singletonList(efactory.declaration(sstep, ints), // param (int step) 
-							Sort.Bool(), // return type
-							bodyExpr); // actions : assertions over S[step] and S[step+1]
+			//			// Following code irrelevant until encoding scheme for calls is found
+			//			// compute an order on labels respecting call order (callees before callers) 
+			//			Set<String> todo = new LinkedHashSet<String>();
+			//			handlePrecedence(gal,todo,labmap,"");
+			//			
+			//			// iterate over labels and output transitions
+			//			for (String lab : todo) {
+			String lab = "";
+			// define a boolean function with single parameter (step) for each transition
+			ISymbol sstep = efactory.symbol("step");
+			IExpr snext = efactory.fcn(efactory.symbol("+"),sstep,efactory.numeral("1"));
+			for (Transition tr : labmap.get(lab)) {
+				List<IExpr> conds = new ArrayList<IExpr>();
+
+				if (! (tr.getGuard() instanceof True)) {
+					conds.add(ExpressionTranslator.translateBool(tr.getGuard(), sstep));
 				}
+				// to keep track of modified vars and array cells
+				Set<Variable> vars = new HashSet<Variable>();
+				Map<ArrayPrefix, Set<Integer>> arrays = new HashMap<ArrayPrefix, Set<Integer>>();
+
+				// handle statements
+				for (Statement st : tr.getActions()) {						
+					if (st instanceof Assignment) {
+						Assignment ass = (Assignment) st;
+
+						IExpr lhs = ExpressionTranslator.translate(ass.getLeft(), snext);
+						IExpr rhs = ExpressionTranslator.translate(ass.getRight(), sstep);							
+						if (ass.getType() == AssignType.INCR ) {
+							rhs = efactory.fcn(efactory.symbol("+"), 
+									ExpressionTranslator.translate(ass.getLeft(), sstep),
+									rhs
+									);							
+						} else if (ass.getType() == AssignType.DECR ) {
+							rhs = efactory.fcn(efactory.symbol("-"), 										
+									ExpressionTranslator.translate(ass.getLeft(), sstep),
+									rhs
+									);							
+						}
+						conds.add(efactory.fcn(efactory.symbol("="), lhs, rhs));
+
+						// we have modifications we need to note
+						if (ass.getLeft().getIndex() == null) {
+							if (! vars.add((Variable) ass.getLeft().getRef())) {
+								throw new RuntimeException("Multiple assignments to a single variable are not supported in SMT translation currently. Was translating transition " + tr.getName());
+							}
+						} else {
+							Set<Integer> hit = arrays.get(ass.getLeft().getRef());
+							if (hit == null) { 
+								hit = new HashSet<Integer>(); 
+								arrays.put((ArrayPrefix) ass.getLeft().getRef(), hit);
+							}
+							if (! hit.add(((Constant) ass.getLeft().getIndex()).getValue())) {
+								throw new RuntimeException("Multiple assignments to a single variable are not supported in SMT translation currently. Was translating transition " + tr.getName());
+							}
+						}
+					} else {
+						//							if (st instanceof SelfCall) {
+						throw new RuntimeException("Only assignments are supported for GAL currently. Was translating transition " + tr.getName());
+					}						
+				}
+
+
+
+				// add constraints on unmodified variables
+				for (Variable  var : gal.getVariables()) {
+					if (! vars.contains(var)) {
+						conds.add(efactory.fcn(efactory.symbol("="), 
+								accessVar(var, sstep),
+								accessVar(var, snext)));
+					}
+				}
+
+				/* Array cell case */
+				for (ArrayPrefix array : gal.getArrays()) {
+					Set<Integer> indexes = arrays.get(array);
+					int size = ((Constant) array.getSize()).getValue();
+					/* On ajoute tout les index */
+					for (int i = 0; i < size; i++) {
+						if(indexes == null || !indexes.contains(i)){
+							conds.add(efactory.fcn(efactory.symbol("="), 
+									accessArray(array, i, sstep),
+									accessArray(array, i, snext)
+									));
+						}
+					}
+				}
+				IExpr bodyExpr = efactory.fcn(efactory.symbol("and"), conds);
+				if (conds.size() == 1) {
+					bodyExpr = conds.get(0);
+				}
+				// 
+				C_define_fun deftr = new org.smtlib.command.C_define_fun(
+						efactory.symbol(tr.getName()),    // name
+						Collections.singletonList(efactory.declaration(sstep, ints)), // param (int step) 
+						Sort.Bool(), // return type
+						bodyExpr); // actions : assertions over S[step] and S[step+1]
+				script.commands().add(deftr);
 				//
 			}
-			
-			
+
+			IResponse res = script.execute(solver);
+			if (res.isError()) {
+				throw new RuntimeException("Specification could not be read correctly with by SMT Solver");				
+			}
 		} else {
 			throw new RuntimeException("Expected ITS Specification to have a GAL as main type for SMT solution !");
 		}
-		
 	}
-	
+
 	/**
 	 * Access array[index] at time "step"
 	 * @param array array to access
@@ -252,12 +258,12 @@ public class BMCSolver implements IBMCSolver {
 	 */
 	private IExpr accessArray (ArrayPrefix array, int index, IExpr step) {
 		ISymbol select = efactory.symbol("select");
-		
+
 		return efactory.fcn(select,
 				efactory.fcn(select,
 						efactory.symbol(array.getName()), efactory.numeral(index)), 
-				step);
-		
+						step);
+
 	}
 
 	public IExpr accessVar (Variable vr, IExpr step) {
@@ -281,7 +287,7 @@ public class BMCSolver implements IBMCSolver {
 			// find calls
 			for (TreeIterator<EObject> it = tr.eAllContents() ; it.hasNext() ; /*NOP*/) {
 				EObject obj = it.next();
-				
+
 				if (obj instanceof SelfCall) {
 					SelfCall self = (SelfCall) obj;
 					handlePrecedence(gal, todo, labmap, self.getLabel().getName());
@@ -296,22 +302,47 @@ public class BMCSolver implements IBMCSolver {
 
 	@Override
 	public int getDepth() {
-		return 0;
+		return depth ;
 	}
 
 	@Override
 	public void incrementDepth() {
-
+		new C_assert(efactory.fcn(efactory.symbol(NEXT),efactory.numeral(depth))).execute(solver);
+		depth++;
 	}
 
 	@Override
 	public Result checkProperty(Property prop) {
-		return null;
+		solver.push(1);
+		IExpr sprop = ExpressionTranslator.translateBool(prop.getBody().getPredicate(), efactory.numeral(depth));
+		if (prop.getBody() instanceof InvariantProp) {
+			sprop = efactory.fcn(efactory.symbol("not"), sprop);
+		}
+		solver.assertExpr(sprop);
+		IResponse res = solver.check_sat();
+		solver.pop(1);
+		if (res.isError()) {
+			throw new RuntimeException("SMT solver raised an exception or timeout.");
+		}
+		IPrinter printer = conf.defaultPrinter;
+	//	System.out.println(printer.toString(script));
+		String textReply = printer.toString(res);
+		System.out.println(printer.toString(res));
+		if ("sat".equals(textReply)) {
+			return Result.SAT;
+		} else if ("unsat".equals(textReply)) {
+			return Result.UNSAT;
+		} else {
+			throw new RuntimeException("SMT solver raised an error :" + textReply);
+		}
 	}
 
 	@Override
 	public void exit() {
-
+		IResponse res = solver.exit();
+		if (res.isError()) {
+			Logger.getLogger("fr.lip6.move.gal").info("SMT solver already quit.");
+		}
 	}
 
 }
