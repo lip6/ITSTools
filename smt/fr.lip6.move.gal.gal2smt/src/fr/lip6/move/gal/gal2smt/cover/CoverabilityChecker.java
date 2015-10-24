5440e19b31f7fc1ef511b6000a74cadb77166a9e
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2015-10-24 19:35:56 +0000

    refactoring to prepare "apply flow constraint at step" scenario

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/cover/CoverabilityChecker.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/cover/CoverabilityChecker.java
@@ -1,41 +1,26 @@
 package fr.lip6.move.gal.gal2smt.cover;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.logging.Logger;
 
-import org.smtlib.ICommand.IScript;
 import org.smtlib.IExpr;
-import org.smtlib.IExpr.IFcnExpr;
 import org.smtlib.IResponse;
 import org.smtlib.SMT.Configuration;
-import org.smtlib.command.C_assert;
 import org.smtlib.impl.Script;
 
-import fr.lip6.move.gal.ArrayPrefix;
-import fr.lip6.move.gal.AssignType;
-import fr.lip6.move.gal.Assignment;
-import fr.lip6.move.gal.BinaryIntExpression;
-import fr.lip6.move.gal.Constant;
 import fr.lip6.move.gal.GALTypeDeclaration;
 import fr.lip6.move.gal.InvariantProp;
 import fr.lip6.move.gal.LogicProp;
 import fr.lip6.move.gal.NeverProp;
 import fr.lip6.move.gal.Property;
 import fr.lip6.move.gal.ReachableProp;
-import fr.lip6.move.gal.SelfCall;
 import fr.lip6.move.gal.Specification;
-import fr.lip6.move.gal.Statement;
 import fr.lip6.move.gal.Transition;
 import fr.lip6.move.gal.TypeDeclaration;
-import fr.lip6.move.gal.Variable;
 import fr.lip6.move.gal.gal2smt.smt.SMTSolver;
 import fr.lip6.move.gal.gal2smt.Result;
 import fr.lip6.move.gal.gal2smt.Solver;
-import fr.lip6.move.gal.instantiate.Simplifier;
 
 public class CoverabilityChecker extends SMTSolver {
 	public CoverabilityChecker(Solver engine, Configuration smtConfig) {
@@ -51,122 +36,27 @@ public class CoverabilityChecker extends SMTSolver {
 		TypeDeclaration td = spec.getMain();
 		if (td instanceof GALTypeDeclaration) {
 			GALTypeDeclaration gal = (GALTypeDeclaration) td;
-			FlowMatrix fm = new FlowMatrix();			
-			isInit = true;
+			FlowMatrix fm = new FlowMatrix(conf,vh);			
+			isInit = fm.init(gal);
 			
-			Map<String,List<Transition> > callers=new HashMap<String, List<Transition>>();
-			Map<String,List<Transition>> labels=new HashMap<String, List<Transition>>();
-								
-			for (Transition tr : gal.getTransitions()) {
-				// define line t of flow Matrix (constants ??)
-				if (! Simplifier.isPetriStyle(tr)) {
-					isInit = false;
-					Logger.getLogger("fr.lip6.move.gal").warning("Could not use coverability on non petri style GAL.");
-					exit();
-					return ;
-				}
-				String lab;
-				if (tr.getLabel() == null) {
-					lab = "";
-				} else {
-					lab = tr.getLabel().getName();
-				}
-				List<Transition> ltrs = labels.get(lab);
-				if (ltrs == null) {
-					ltrs = new ArrayList<Transition>();
-					labels.put(lab, ltrs);
-				}
-				ltrs.add(tr);
-				
-				
-				for (Statement st : tr.getActions()) {
-					if (st instanceof Assignment) {
-						Assignment ass = (Assignment) st;
-						int val;
-						if (ass.getType() == AssignType.ASSIGN) {
-							BinaryIntExpression bin = (BinaryIntExpression) ass.getRight();
-							Constant c = (Constant) bin.getRight();
-							val = c.getValue();
-							if (bin.getOp().equals("-")) {
-								val = -val;
-							}
-						} else {
-							Constant c = (Constant) ass.getRight();
-							val = c.getValue();
-							if (ass.getType() == AssignType.DECR) {
-								val = -val;
-							}
-						}
-						fm.addEffect(tr, ass.getLeft(), val);
-					} else if (st instanceof SelfCall) {
-						SelfCall scall = (SelfCall) st;
-						String labname = scall.getLabel().getName();
-						List<Transition> calledLabs = callers.get(labname);
-						if (calledLabs == null) {
-							calledLabs = new ArrayList<Transition>();
-							callers.put(labname, calledLabs);
-						}
-						calledLabs.add(tr);
-					}					
-				}
-				
+			if (! isInit) {
+				exit();
+			}
+
+			Map<Transition,IExpr> trmap = new HashMap<Transition, IExpr>();
+			for (Transition tr : gal.getTransitions()) {				
 				// build Xi : Parikh number of occurrences of t
 				// assert Xi >= 0
-				getVH().declarePositiveIntegerVariable(fm.getTransParikhName(tr), script.commands(),true);
+				String tname = getTransParikhName(tr);
+				trmap.put(tr,efactory.symbol(tname));
+				getVH().declarePositiveIntegerVariable(tname, script.commands(),true);
 			}
 
-			
 			// build variables M 
 			// assert M >=0 positive
-			for (Variable var : gal.getVariables()) {
-				String symbol = var.getName();
-				int init = ((Constant) var.getValue()).getValue();
-				buildLineConstraint(fm, script, symbol, init);
-			}
-			for (ArrayPrefix arr : gal.getArrays()) {
-				int sz = ((Constant) arr.getSize()).getValue();
-				for (int i=0; i < sz; i++ ) {
-					String symbol = fm.getArrName(arr,i);
-					int init = ((Constant) arr.getValues().get(i)).getValue();
-					buildLineConstraint(fm, script, symbol, init);
-				}
-			}
-			
-			// add callee/caller constraints on Xi
-			// Ti calls (Tj,Tk) => Xj + Xk >= Xi
-			// Tj called by (Ti,Tb) => Xj <= Xi+Xb
-			// Label version :
-			// for each label l except "", sum{t.x | self.l \in t.act} = sum{ t.x | t.lab = l }
-
-			for (Entry<String, List<Transition>> e : callers.entrySet()) {
-				String labname = e.getKey();
-				// skip "private" label
-				if ("".equals(labname)) continue;
-				
-				// an expression for sum of call point occurrences
-				List<IExpr> tosum = new ArrayList<IExpr>();
-				for (Transition tr : e.getValue()) {
-					tosum.add(efactory.symbol(fm.getTransParikhName(tr)));
-				}
-				IFcnExpr sumcalls = efactory.fcn(efactory.symbol("+"), tosum);
-
-				// an expression for sum of occurrences of labs with that label
-				List<IExpr> tosum2 = new ArrayList<IExpr>();
-				for (Transition tr : labels.get(labname)) {
-					tosum2.add(efactory.symbol(fm.getTransParikhName(tr)));
-				}
-				IExpr sumlabs = efactory.fcn(efactory.symbol("+"), tosum2);
-				if (tosum2.isEmpty()) {
-					// should not happen due to GAL simplifications, we are calling non existent label here
-					sumlabs = efactory.numeral(0);
-				}
-
-				// assert caller constraint on X
-				script.commands().add(new C_assert(efactory.fcn(efactory.symbol("="), 
-						sumcalls,
-						sumlabs
-						)));
-			}
+			IExpr step = null;
+			fm.addLineConstraints(script,gal, step, trmap);
+			fm.addCallConstraints(trmap,script);
 
 			// check sat
 			IResponse res = script.execute(solver);
@@ -202,24 +92,12 @@ public class CoverabilityChecker extends SMTSolver {
 		return super.verifyAssertion(totest);
 	}
 	
-	public void buildLineConstraint(FlowMatrix fm, IScript script, String var, int init) {
-		// assert : x = m0.x + X0*C(t0,x) + ...+ XN*C(Tn,x)
-		List<IExpr> exprs = new ArrayList<IExpr>();
-		
-		// m0.x
-		exprs.add(efactory.numeral(init));
-		
-		//  Xi*C(ti,x)
-		for (Entry<Transition, Integer> ent : fm.getLine(var).entrySet()) {
-			exprs.add(efactory.fcn(efactory.symbol("*"), efactory.symbol(fm.getTransParikhName(ent.getKey())), efactory.numeral(ent.getValue())));
-		}
-		
-		script.commands().add(new C_assert(efactory.fcn(efactory.symbol("="),
-				efactory.symbol(var),
-				efactory.fcn(efactory.symbol("+"), exprs)
-				)));
-	}
 
+
+	
+	String getTransParikhName(Transition tr) {
+		return tr.getName()+".x";
+	}
 
 	
 }
--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/cover/FlowMatrix.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/cover/FlowMatrix.java
@@ -1,20 +1,108 @@
 package fr.lip6.move.gal.gal2smt.cover;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
+
+import org.smtlib.IExpr;
+import org.smtlib.ICommand.IScript;
+import org.smtlib.IExpr.IFactory;
+import org.smtlib.IExpr.IFcnExpr;
+import org.smtlib.SMT.Configuration;
+import org.smtlib.command.C_assert;
+import org.smtlib.impl.Script;
 
 import fr.lip6.move.gal.ArrayPrefix;
+import fr.lip6.move.gal.AssignType;
+import fr.lip6.move.gal.Assignment;
+import fr.lip6.move.gal.BinaryIntExpression;
 import fr.lip6.move.gal.Constant;
+import fr.lip6.move.gal.GALTypeDeclaration;
+import fr.lip6.move.gal.GF2;
+import fr.lip6.move.gal.SelfCall;
+import fr.lip6.move.gal.Statement;
 import fr.lip6.move.gal.Transition;
+import fr.lip6.move.gal.Variable;
 import fr.lip6.move.gal.VariableReference;
+import fr.lip6.move.gal.gal2smt.smt.IVariableHandler;
+import fr.lip6.move.gal.instantiate.Simplifier;
 
 public class FlowMatrix {
 
 	private Map<String, Map<Transition,Integer>> flow = new HashMap<String, Map<Transition,Integer>>();
+	private Map<String,List<Transition>> callers=new HashMap<String, List<Transition>>();
+	private Map<String,List<Transition>> labels=new HashMap<String, List<Transition>>();
+	private final IFactory efactory;
+	private final IVariableHandler vh;
+
+	public FlowMatrix(Configuration conf, IVariableHandler vh) {
+		this.efactory = conf.exprFactory;
+		this.vh = vh;
+	}
+	
+	public boolean init (GALTypeDeclaration gal) {
+
+		for (Transition tr : gal.getTransitions()) {
+			// define line t of flow Matrix (constants ??)
+			if (! Simplifier.isPetriStyle(tr)) {
+				Logger.getLogger("fr.lip6.move.gal").warning("Could not use coverability on non petri style GAL.");
+				return false;
+			}
+			String lab;
+			if (tr.getLabel() == null) {
+				lab = "";
+			} else {
+				lab = tr.getLabel().getName();
+			}
+			List<Transition> ltrs = labels.get(lab);
+			if (ltrs == null) {
+				ltrs = new ArrayList<Transition>();
+				labels.put(lab, ltrs);
+			}
+			ltrs.add(tr);
+
+
+			for (Statement st : tr.getActions()) {
+				if (st instanceof Assignment) {
+					Assignment ass = (Assignment) st;
+					int val;
+					if (ass.getType() == AssignType.ASSIGN) {
+						BinaryIntExpression bin = (BinaryIntExpression) ass.getRight();
+						Constant c = (Constant) bin.getRight();
+						val = c.getValue();
+						if (bin.getOp().equals("-")) {
+							val = -val;
+						}
+					} else {
+						Constant c = (Constant) ass.getRight();
+						val = c.getValue();
+						if (ass.getType() == AssignType.DECR) {
+							val = -val;
+						}
+					}
+					addEffect(tr, ass.getLeft(), val);
+				} else if (st instanceof SelfCall) {
+					SelfCall scall = (SelfCall) st;
+					String labname = scall.getLabel().getName();
+					List<Transition> calledLabs = callers.get(labname);
+					if (calledLabs == null) {
+						calledLabs = new ArrayList<Transition>();
+						callers.put(labname, calledLabs);
+					}
+					calledLabs.add(tr);
+				}					
+			}
+		}
+		return true;
+
+	}
+	
 	
 	void addEffect(Transition tr, VariableReference vref, int val) {
-		String symb = getSymbolName(vref);
-		Map<Transition, Integer> line = getLine(symb );
+		Map<Transition, Integer> line = getLine(vref);
 		Integer cur = line.get(tr);
 		if (cur==null) {
 			cur=0;
@@ -23,19 +111,16 @@ public class FlowMatrix {
 		line.put(tr, cur);
 	}
 
-	public Map<Transition, Integer> getLine(String symbol) {
-		Map<Transition, Integer> res = flow.get(symbol);
+	public Map<Transition, Integer> getLine(VariableReference var) {
+		String vname = getSymbolName(var);
+		Map<Transition, Integer> res = flow.get(vname);
 		if (res==null) {
 			res = new HashMap<Transition, Integer>();
-			flow.put(symbol, res);
+			flow.put(vname, res);
 		}
 		return res ;
 	}
 
-	
-	String getTransParikhName(Transition tr) {
-		return tr.getName()+".x";
-	}
 
 	public String getSymbolName(VariableReference vref) {
 		if (vref.getIndex()==null) {
@@ -46,6 +131,77 @@ public class FlowMatrix {
 	}
 	String getArrName(ArrayPrefix arr, int i) {
 		return "|"+arr.getName()+"["+i+"]|";
+	}
+
+
+	public void addCallConstraints(Map<Transition, IExpr> trmap, Script script) {
+		// add callee/caller constraints on Xi
+		// Ti calls (Tj,Tk) => Xj + Xk >= Xi
+		// Tj called by (Ti,Tb) => Xj <= Xi+Xb
+		// Label version :
+		// for each label l except "", sum{t.x | self.l \in t.act} = sum{ t.x | t.lab = l }
+		for (Entry<String, List<Transition>> e : callers.entrySet()) {
+			String labname = e.getKey();
+			// skip "private" label
+			if ("".equals(labname)) continue;
+			
+			// an expression for sum of call point occurrences
+			List<IExpr> tosum = new ArrayList<IExpr>();
+			for (Transition tr : e.getValue()) {
+				tosum.add(trmap.get(tr));
+			}
+			IFcnExpr sumcalls = efactory.fcn(efactory.symbol("+"), tosum);
+
+			// an expression for sum of occurrences of labs with that label
+			List<IExpr> tosum2 = new ArrayList<IExpr>();
+			for (Transition tr : labels.get(labname)) {
+				tosum2.add(trmap.get(tr));
+			}
+			IExpr sumlabs = efactory.fcn(efactory.symbol("+"), tosum2);
+			if (tosum2.isEmpty()) {
+				// should not happen due to GAL simplifications, we are calling non existent label here
+				sumlabs = efactory.numeral(0);
+			}
+
+			// assert caller constraint on X
+			script.commands().add(new C_assert(efactory.fcn(efactory.symbol("="), 
+					sumcalls,
+					sumlabs
+					)));
+		}		
+	}
+
+
+	public void addLineConstraints(Script script, GALTypeDeclaration gal, IExpr step, Map<Transition, IExpr> trmap) {
+		for (Variable var : gal.getVariables()) {
+			int init = ((Constant) var.getValue()).getValue();
+			buildLineConstraint(script, GF2.createVariableRef(var), init, step, trmap);
+		}
+		for (ArrayPrefix arr : gal.getArrays()) {
+			int sz = ((Constant) arr.getSize()).getValue();
+			for (int i=0; i < sz; i++ ) {
+				int init = ((Constant) arr.getValues().get(i)).getValue();
+				buildLineConstraint(script, GF2.createArrayVarAccess(arr, GF2.constant(i)), init,step, trmap);
+			}
+		}		
+	}
+
+	public void buildLineConstraint(IScript script, VariableReference var, int init, IExpr step, Map<Transition, IExpr> trmap) {
+		// assert : x = m0.x + X0*C(t0,x) + ...+ XN*C(Tn,x)
+		List<IExpr> exprs = new ArrayList<IExpr>();
+		
+		// m0.x
+		exprs.add(efactory.numeral(init));
+		
+		//  Xi*C(ti,x)
+		for (Entry<Transition, Integer> ent : getLine(var).entrySet()) {
+			exprs.add(efactory.fcn(efactory.symbol("*"), trmap.get(ent.getKey()), efactory.numeral(ent.getValue())));
+		}
+		
+		script.commands().add(new C_assert(efactory.fcn(efactory.symbol("="),
+				vh.translate(var, step),
+				efactory.fcn(efactory.symbol("+"), exprs)
+				)));
 	}
 
 }
