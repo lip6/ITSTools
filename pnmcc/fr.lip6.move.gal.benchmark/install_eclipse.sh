e8aef96614556e1014fd72c78b36baa0b653021f
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-01-22 13:58:27 +0000

    change to eclipse mars

--- pnmcc/fr.lip6.move.gal.benchmark/install_eclipse.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/install_eclipse.sh
@@ -2,12 +2,13 @@
 
 set -x
 
-if [ ! -f eclipse-platform-4.4.2-linux-gtk-x86_64.tar.gz ] ; then 
-    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.4.2-201502041700/eclipse-platform-4.4.2-linux-gtk-x86_64.tar.gz
+if [ ! -f eclipse-platform-4.5.1-linux-gtk-x86_64.tar.gz ] ; then 
+    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.5.1-201509040015/eclipse-platform-4.5.1-linux-gtk-x86_64.tar.gz
+#    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.4.2-201502041700/eclipse-platform-4.4.2-linux-gtk-x86_64.tar.gz
 fi
 
 if [ ! -d eclipse ] ; then 
-    tar xvzf eclipse-platform-4.4.2-linux-gtk-x86_64.tar.gz
+    tar xvzf eclipse-platform-4.5.1-linux-gtk-x86_64.tar.gz
 fi
 
 cd eclipse
@@ -25,6 +26,6 @@ cd eclipse
 -clean -purgeHistory \
 -application org.eclipse.equinox.p2.director \
 -noSplash \
--repository http://download.eclipse.org/releases/luna,http://ddd.lip6.fr/update-site \
+-repository http://download.eclipse.org/releases/mars,http://ddd.lip6.fr/update-site \
 -installIUs fr.lip6.move.gal.feature.pnmcc.feature.group
 
