83eb1c5325d12e1822014022307f743fc800aaf0
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-02-04 14:33:56 +0000

    invocation of marcie (broken STATESPACE option)

---
+++ pnmcc/fr.lip6.move.gal.benchmark/runmarcietest.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+export BINDIR=`pwd`
+
+cd INPUTS
+
+tar xzf $1.tgz
+
+cd $1
+
+export MODEL=`pwd`
+
+if [ "$2" != "StateSpace" ];
+then
+    $BINDIR/marcie/marcie --net-file=model.pnml --mcc-file=$2.xml --memory=6 --suppress --rs-algorithm=3 --place-order=5 ;
+else
+#    $BINDIR/marcie/marcie --net-file=model.pnml --memory=6 --suppress --rs-algorithm=3 --place-order=5 ;
+fi
+
+cd ..
+
+#\rm -rf $1
+
+cd ..
