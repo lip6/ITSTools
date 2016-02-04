2b2b71d55f55fcffaa275acd22ceb198915112bb
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-02-04 18:00:08 +0000

    add flag for state space in marcie

--- pnmcc/fr.lip6.move.gal.benchmark/runmarcietest.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/runmarcietest.sh
@@ -13,8 +13,8 @@ export MODEL=`pwd`
 if [ "$2" != "StateSpace" ];
 then
     $BINDIR/marcie/marcie --net-file=model.pnml --mcc-file=$2.xml --memory=6 --suppress --rs-algorithm=3 --place-order=5 ;
-#else
-#    $BINDIR/marcie/marcie --net-file=model.pnml --memory=6 --suppress --rs-algorithm=3 --place-order=5 ;
+else
+    $BINDIR/marcie/marcie --net-file=model.pnml --memory=6 --suppress --rs-algorithm=3 --place-order=5 --mcc-mode ;
 fi
 
 cd ..
