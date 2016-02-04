2532df709d6e8add8db5243fa562da19f3c16845
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-02-04 21:01:09 +0000

    new marcie grab script

--- pnmcc/fr.lip6.move.gal.benchmark/install_marcie.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/install_marcie.sh
@@ -6,7 +6,8 @@ if [ ! -d marcie ] ; then
     
     yname=marcie-linux64-2016-02-04.tar.gz
     if [ ! -f $yname ] ; then 
-	wget "http://www-dssz.informatik.tu-cottbus.de/track/download.php?id=153"
+	wget "http://www-dssz.informatik.tu-cottbus.de/track/download.php?id=153" ;
+	mv download* $yname
     fi
 
     tar xvzf $yname
