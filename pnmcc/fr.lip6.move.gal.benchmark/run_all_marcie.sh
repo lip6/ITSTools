d3af6f1a0d6d64819d21cce89955c9e863adc0c4
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-02-04 14:33:07 +0000

    marcie runner

---
+++ pnmcc/fr.lip6.move.gal.benchmark/run_all_marcie.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+set -e
+
+
+./install_marcie.sh
+./install_inputs.sh
+
+# echo "##teamcity[testSuiteStarted name='PNMCC perfs']"
+
+for i in oracle/*.out ; do
+    ./run_test.pl $i -marcie;
+done;
+
+for i in ctl/*.out ; do
+    ./run_test.pl $i -marcie;
+done;
+
+
+
+# echo "##teamcity[testSuiteFinished name='PNMCC perfs']"
+
