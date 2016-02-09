9f258eee3fbfe13a748020412946eb1a8d6f318e
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-02-09 17:14:48 +0000

    a hack to ensure we grab latest version from MArcie's web page

--- pnmcc/fr.lip6.move.gal.benchmark/install_marcie.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/install_marcie.sh
@@ -4,14 +4,19 @@ set -x
 
 if [ ! -d marcie ] ; then 
     
-    yname=marcie-linux64-2016-02-04.tar.gz
+    yname=marcie-linux64.tar.gz
+    export DLURL=`curl 'http://www-dssz.informatik.tu-cottbus.de/DSSZ/Software/Marcie' | grep 'Linux (64bit)' | cut -f 6 -d \' `
+
     if [ ! -f $yname ] ; then 
-	wget "http://www-dssz.informatik.tu-cottbus.de/track/download.php?id=153" ;
-	mv download* $yname
+	wget $DLURL -O $yname;
     fi
 
     tar xvzf $yname
     
     mv marcie*/ marcie/
 fi
+
+marcie/marcie 2> /dev/null  | grep Marcie
+
+
 
