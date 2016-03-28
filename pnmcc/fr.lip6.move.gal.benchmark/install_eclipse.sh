0631704b84dbe5245a9368926a43bf66167adfea
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-03-28 16:58:58 +0000

    mars.2 => 4.5.2

--- pnmcc/fr.lip6.move.gal.benchmark/install_eclipse.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/install_eclipse.sh
@@ -2,14 +2,15 @@
 
 set -x
 
-if [ ! -f eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz ] ; then 
-  wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.5.2-201602121500/eclipse-SDK-4.5.2-linux-gtk-x86_64.tar.gz
+if [ ! -d eclipse ] ; then 
+  if [ ! -f eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz ] ; then 
+    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.5.2-201602121500/eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz
 #    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.5.1-201509040015/eclipse-platform-4.5.1-linux-gtk-x86_64.tar.gz
 #    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.4.2-201502041700/eclipse-platform-4.4.2-linux-gtk-x86_64.tar.gz
-fi
+  fi
 
-if [ ! -d eclipse ] ; then 
-    tar xvzf eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz
+  tar xvzf eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz
+  rm eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz
 fi
 
 cd eclipse
