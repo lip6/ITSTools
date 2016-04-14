4ef8e6924760e9a1cdb3286df34b709f10aeb784
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-04-14 14:47:32 +0000

    runner for ltl tests

---
+++ pnmcc/fr.lip6.move.gal.benchmark/run_all_ltl.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+set -e
+
+./install_all.sh
+
+
+# echo "##teamcity[testSuiteStarted name='PNMCC perfs']"
+
+for i in ltl/*.out ; do
+    ./run_test.pl $i -its;
+done;
+
+# echo "##teamcity[testSuiteFinished name='PNMCC perfs']"
+
