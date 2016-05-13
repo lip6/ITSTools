46179d507a6256325d72dde4b1bc287c34685215
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2016-05-13 16:32:45 +0000

    add scalar set handling

--- pnmcc/fr.lip6.move.gal.benchmark/BenchKit_head.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/BenchKit_head.sh
@@ -15,5 +15,9 @@ export BINDIR=/home/mcc/BenchKit/
 
 export MODEL=`pwd`
 
+if [[ $BK_EXAMINATION = StateSpace && -f modelMain.xml ]] ; 
+then
+$BINDIR/its-reach-linux64 -i modelMain.xml -t ITSXML -ssDR 5 --stats | sed 's/Max variable value :/STATE_SPACE MAX_TOKEN_IN_PLACE /' | sed 's/Maximum sum along a path :/STATE_SPACE MAX_TOKEN_PER_MARKING /' | sed 's/Exact state count :/STATE_SPACE STATES /' | sed 's/Total edges in reachability graph :/STATE_SPACE TRANSITIONS /'
+else
 $BINDIR/runeclipse.sh $MODEL $BK_EXAMINATION -its
-
+fi
