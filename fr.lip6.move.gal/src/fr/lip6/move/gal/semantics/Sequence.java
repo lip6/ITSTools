ce96aacd05a991b5d0047fae2cf99a0f1a1621fe
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2017-02-09 14:18:54 +0000

    source->format + comments + factory method does more tests

--- fr.lip6.move.gal/src/fr/lip6/move/gal/semantics/Sequence.java
+++ fr.lip6.move.gal/src/fr/lip6/move/gal/semantics/Sequence.java
@@ -3,45 +3,56 @@ package fr.lip6.move.gal.semantics;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A semantic composition of behaviors.
+ * @author ythierry
+ *
+ */
 public class Sequence implements INext {
 
 	private List<INext> acts;
-	
+
 	private Sequence(List<INext> acts) {
 		this.acts = acts;
 	}
 
 	public static INext seq(List<INext> acts) {
-		if (acts.isEmpty()) {
+		List<INext> flat = new ArrayList<INext>(acts.size());
+		for (INext n : acts) {
+			if (n instanceof Sequence) {
+				Sequence seq = (Sequence) n;
+				flat.addAll(seq.getActions());
+			} else if (n == INext.ID) {
+				// continue
+			} else if (n == INext.EMPTY) {
+				return n;
+			} else {
+				flat.add(n);
+			}
+		}
+		if (flat.isEmpty()) {
 			return INext.ID;
-		} else if (acts.size() == 1) {
+		} else if (flat.size() == 1) {
 			return acts.get(0);
 		} else {
-			List<INext> flat = new ArrayList<INext>(acts.size());
-			for (INext n : acts) {
-				if (n instanceof Sequence) {
-					Sequence seq = (Sequence) n;
-					flat.addAll(seq.getActions());
-				} else {
-					flat.add(n);
-				}
-			}
 			return new Sequence(flat);
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		boolean first = true;
 		StringBuilder sb = new StringBuilder();
 		for (INext act : acts) {
-			if (first) first = false;
-			else sb.append(", ");
-			sb.append(act.toString()+";\n");
+			if (first)
+				first = false;
+			else
+				sb.append(", ");
+			sb.append(act.toString() + ";\n");
 		}
 		return sb.toString();
 	}
-	
+
 	public List<INext> getActions() {
 		return acts;
 	}
