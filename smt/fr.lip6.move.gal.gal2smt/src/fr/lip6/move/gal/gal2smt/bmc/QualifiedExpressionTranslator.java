7bb31f5c467e21a566df6170749437c1b48db780
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2017-02-15 18:55:09 +0000

    a concrete translator able to (partly) deal with qualified references, provided array indices are constants.

---
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/QualifiedExpressionTranslator.java
@@ -0,0 +1,25 @@
+package fr.lip6.move.gal.gal2smt.bmc;
+
+import org.smtlib.IExpr;
+import org.smtlib.SMT.Configuration;
+
+import fr.lip6.move.gal.Reference;
+import fr.lip6.move.gal.semantics.INextBuilder;
+
+public class QualifiedExpressionTranslator extends ExpressionTranslator {
+
+	public QualifiedExpressionTranslator(Configuration conf) {
+		super(conf);
+	}
+	
+	private INextBuilder nb;
+	public void setNb(INextBuilder nb) {
+		this.nb = nb;
+	}
+
+	@Override
+	protected IExpr handleReference(Reference expr, IExpr state) {
+		return efactory.fcn(efactory.symbol("select"), state, efactory.numeral(nb.getIndex(expr)));
+	}
+
+}
