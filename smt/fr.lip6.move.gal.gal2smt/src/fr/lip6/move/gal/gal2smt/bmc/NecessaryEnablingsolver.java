b97dd8bf9b6649ab534f3d12c05317a03bf8175d
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2017-02-22 21:51:09 +0000

    a version based on INecxt, that assumes we are talking about the transition items

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NecessaryEnablingsolver.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NecessaryEnablingsolver.java
@@ -1,14 +1,9 @@
 package fr.lip6.move.gal.gal2smt.bmc;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
-import org.smtlib.IExpr;
 import org.smtlib.SMT.Configuration;
 
-import fr.lip6.move.gal.BooleanExpression;
-import fr.lip6.move.gal.Transition;
 import fr.lip6.move.gal.gal2smt.Result;
 import fr.lip6.move.gal.gal2smt.Solver;
 
@@ -17,32 +12,38 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 	public NecessaryEnablingsolver(Configuration smtConfig, Solver engine) {
 		super(smtConfig, engine, false);
 	}
+	
 
-	public int [] computeEnablers (List<Transition> trans, BooleanExpression guard) {
-		int [] toret = new int[trans.size()];
+	public int [] computeEnablers (int target) {
+		int [] toret = new int[nbTransition];
 		
 		// push a context
 		solver.push(1);
 		
-		List<IExpr> asserts = new ArrayList<IExpr>();
-		// assert guard in initial 
-		asserts.add(et.translateBool(guard, efactory.numeral(0)));
-		// not guard in successor
-		asserts.add(efactory.fcn(efactory.symbol("not"), et.translateBool(guard, efactory.numeral(1))));
-		IExpr sprop = efactory.fcn(efactory.symbol("and"), asserts);
-		solver.assertExpr(sprop);
+		if (isPresburger) {
+			addFlowConstraints(1);
+		}
 		
-		int i=0;
-		for (Transition t :  trans) {
+		// assert not enabled in initial
+		solver.assertExpr(efactory.fcn(efactory.symbol("not"),
+				efactory.fcn(efactory.symbol(ENABLED+target), efactory.numeral(0))));
+
+		// assert enabled in successor step 1
+		solver.assertExpr(
+				efactory.fcn(efactory.symbol(ENABLED+target), efactory.numeral(1)));
+		
+		for (int i =0; i < nbTransition ; i++) {
 			solver.push(1);
-			solver.assertExpr(efactory.fcn(efactory.symbol(t.getName()),efactory.numeral(0)));
+			solver.assertExpr(efactory.fcn(efactory.symbol("tr"+i),efactory.numeral(0)));
 		
-			Logger.getLogger("fr.lip6.move.gal").fine("Checking "+t.getName());
+			Logger.getLogger("fr.lip6.move.gal").fine("Checking enabling of "+i);
 			Result res = checkSat();
 			if (res == Result.SAT) {
 				toret[i] = 1;
-			} else {
+			} else if (res == Result.UNSAT){
 				toret[i] = 0;
+			} else {
+				throw new RuntimeException("SMT solver raised an error in enabler solving :"+res);
 			}
 			solver.pop(1);
 			i++;
