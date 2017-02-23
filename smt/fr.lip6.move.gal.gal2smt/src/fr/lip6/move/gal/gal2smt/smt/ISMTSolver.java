c0ac2559fd596268cc1b9443b96839d8e0a39e6f
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2017-02-23 02:04:42 +0000

    use a INextBuilder as input

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/smt/ISMTSolver.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/smt/ISMTSolver.java
@@ -3,6 +3,7 @@ package fr.lip6.move.gal.gal2smt.smt;
 import fr.lip6.move.gal.Property;
 import fr.lip6.move.gal.Specification;
 import fr.lip6.move.gal.gal2smt.Result;
+import fr.lip6.move.gal.semantics.INextBuilder;
 
 public interface ISMTSolver {
 
@@ -10,8 +11,13 @@ public interface ISMTSolver {
 	 * Initialize the solver with : logic/headers, definition of variables S[step] (as arrays indexed by step), definition of transition relation Next(step)
 	 * @param spec a specification, we expect "main" instance to be a GAL
 	 */
-	public abstract void init(Specification spec);
+	@Deprecated
+	default void init(Specification spec) {
+		init(INextBuilder.build(spec));
+	}
 
+	void init(INextBuilder spec);	
+	
 	/**
 	 * Kill the solver cleanly and finish the SMT session.
 	 */
@@ -31,5 +37,7 @@ public interface ISMTSolver {
 	 */
 	public Result verify(Property prop);
 	
-	public void setShowSatState(boolean shouldShow);	
+	public void setShowSatState(boolean shouldShow);
+
+	
 }
