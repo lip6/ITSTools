b66f2aeb84ee1de1da0c48d262a5418ca9001ac3
Yann Thierry-Mieg <Yann.thierry-Mieg@lip6.fr>
2017-02-28 21:43:47 +0000

    add may disable pre test, add comments

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NecessaryEnablingsolver.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NecessaryEnablingsolver.java
@@ -148,7 +148,7 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 
 	
 	
-	public List<int[]> computeDoNotAccord (List<int[]> coEnabled) {
+	public List<int[]> computeDoNotAccord (List<int[]> coEnabled, List<int[]> mayDisable) {
 		List<int[]> dnaMatrix = new ArrayList<>(nbTransition);
 		for (int tindex = 0; tindex < nbTransition ; tindex++) {
 			dnaMatrix.add(new int[nbTransition]);
@@ -177,22 +177,38 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 					// put 0 in dna(t1,t2) = do nothing
 					continue;
 				}
+				if (mayDisable.get(t1)[t2]==1 || mayDisable.get(t2)[t1]==1) {
+					// mutual disabling criterion not met
+					// t1 and t2 do not accord
+					dnaMatrix.get(t1)[t2] = 1;
+					dnaMatrix.get(t2)[t1] = 1;
+					continue;
+				}
 				solver.push(1);
 				
 				// Express  assertions 
+				// s1  --t1--> s2
+				//     --t2--> s3
 				solver.assertExpr(efactory.fcn(efactory.symbol(TRANSSRC+t1), s1, s2));
 				solver.assertExpr(efactory.fcn(efactory.symbol(TRANSSRC+t2), s1, s3));
 				
-				IExpr or = efactory.fcn(efactory.symbol("or"), 
-						efactory.fcn(efactory.symbol("not"),  efactory.fcn(efactory.symbol(ENABLEDSRC+t1), s3)),
-						efactory.fcn(efactory.symbol("not"),  efactory.fcn(efactory.symbol(ENABLEDSRC+t2), s2)),
-						efactory.fcn(efactory.symbol("and"), 
+
+				// these additional conditions enforce a mutual disabling test, already done above
+				//				efactory.fcn(efactory.symbol("or"), 
+				//						efactory.fcn(efactory.symbol("not"),  efactory.fcn(efactory.symbol(ENABLEDSRC+t1), s3)),
+				//						efactory.fcn(efactory.symbol("not"),  efactory.fcn(efactory.symbol(ENABLEDSRC+t2), s2)),
+
+				// add commutativity test
+				// s1  --t1--> s2 --t2--> s5
+				//     --t2--> s3 --t1--> s4
+				// can s4 != s5 be SAT ?
+				// this test relies on deterministic behavior of transitions.
+				IExpr tocheck = efactory.fcn(efactory.symbol("and"), 
 								efactory.fcn(efactory.symbol(TRANSSRC+t1), s3, s4),
-								efactory.fcn(efactory.symbol(TRANSSRC+t2), s3, s5),
+								efactory.fcn(efactory.symbol(TRANSSRC+t2), s2, s5),
 								efactory.fcn(efactory.symbol("not"), efactory.fcn(efactory.symbol("="), s4, s5))
-								)
-						);
-				solver.assertExpr(or);
+								);						
+				solver.assertExpr(tocheck);
 				
 				Result res = checkSat();
 				Logger.getLogger("fr.lip6.move.gal").info("Checking Do Not Accords relation of "+t1 + " and " + t2 + " : " + res);
@@ -200,9 +216,12 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 				solver.pop(1);
 				
 				if (res == Result.SAT) {
+					// we found s4 != s5 through t1.t2 and t2.t1
 					dnaMatrix.get(t1)[t2] = 1;
 					dnaMatrix.get(t2)[t1] = 1;
 				} else if (res == Result.UNSAT){
+					// we could not find s4 != s5 through t1.t2 and t2.t1
+					// so they are indeed commutative, accords is true
 					dnaMatrix.get(t1)[t2] = 0;
 					dnaMatrix.get(t2)[t1] = 0;
 				} else {
