028df09109d808d2cd8febdf955dcb855116cd9e
ythierry <Yann.Thierry-Mieg@lip6.fr>
2017-03-06 14:43:01 +0100

    get rid of tags

--- tags/1.0/fr.lip6.move.gal.website/documentation/en/contentLeft.php
+++
@@ -1,9 +0,0 @@
-
-  <div id="sectionLinks">
-    <h3>Links</h3>
-    <ul>
-      <li><a href="#">Home Page </a></li>
-      <li><a href="#">Documentation</a></li>
-      <li><a href="#">Downloads</a></li>
-    </ul>
-  </div>
--- tags/1.0/fr.lip6.move.gal.website/documentation/fr/contentFooter.php
+++
@@ -1,1 +0,0 @@
- <img src="" width="44" height="22" /> <a href="#">About Us</a> | <a href="#">Site Map</a> | <a href="#">Privacy Policy</a> | <a href="#">Contact Us</a> | &copy;2003 Company Name 
