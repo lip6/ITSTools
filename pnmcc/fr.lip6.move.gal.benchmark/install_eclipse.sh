f36e9bbea2db2505ab1e5c0d592c21f3f333f361
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-03-28 14:35:29 +0200

    go neon build + github repository

--- pnmcc/fr.lip6.move.gal.benchmark/install_eclipse.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/install_eclipse.sh
@@ -3,8 +3,9 @@
 set -x
 
 if [ ! -d eclipse ] ; then 
-  if [ ! -f eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz ] ; then 
-    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.6.2-201611241400/eclipse-platform-4.6.2-linux-gtk-x86_64.tar.gz
+  if [ ! -f eclipse-platform-4.6.3-linux-gtk-x86_64.tar.gz ] ; then 
+    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.6.3-201703010400/eclipse-platform-4.6.3-linux-gtk-x86_64.tar.gz
+#   wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.6.2-201611241400/eclipse-platform-4.6.2-linux-gtk-x86_64.tar.gz
 #    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.5.2-201602121500/eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz
 #    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.5.1-201509040015/eclipse-platform-4.5.1-linux-gtk-x86_64.tar.gz
 #    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.4.2-201502041700/eclipse-platform-4.4.2-linux-gtk-x86_64.tar.gz
@@ -29,6 +30,6 @@ cd eclipse
 -clean -purgeHistory \
 -application org.eclipse.equinox.p2.director \
 -noSplash \
--repository http://download.eclipse.org/releases/mars,http://ddd.lip6.fr/update-site \
+-repository http://download.eclipse.org/releases/neon,http://lip6.github.io/ITSTools \
 -installIUs fr.lip6.move.gal.feature.pnmcc.feature.group
 
