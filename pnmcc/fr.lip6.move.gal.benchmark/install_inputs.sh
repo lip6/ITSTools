1ae49e5b5bc71ea3e1e5d6d474df9a5ab2a2601a
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-03-28 14:35:46 +0200

    work with 2016 models for now

--- pnmcc/fr.lip6.move.gal.benchmark/install_inputs.sh
+++ pnmcc/fr.lip6.move.gal.benchmark/install_inputs.sh
@@ -7,7 +7,7 @@ if [ ! -d INPUTS ] ; then
     for i in MCC-INPUTS.tgz MCC-INPUTS_MODIFIED.tgz MCC-INPUTS_MODIFIED-2.tgz ;
     do 
 	if [ ! -f $i ] ; then 
-	    wget http://mcc.lip6.fr/archives/$i
+	    wget http://mcc.lip6.fr/2016/archives/$i
 	fi
 	tar xvzf $i
     done
