871f59dbb9927108987235b1ecc84d59e879311d
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-03-31 23:54:47 +0200

    refactored traces, use dep matrix to avoid sat calls

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NecessaryEnablingsolver.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NecessaryEnablingsolver.java
@@ -9,34 +9,52 @@ import org.smtlib.SMT.Configuration;
 
 import fr.lip6.move.gal.gal2smt.Result;
 import fr.lip6.move.gal.gal2smt.Solver;
+import fr.lip6.move.gal.semantics.DependencyMatrix;
 import fr.lip6.move.gal.semantics.INextBuilder;
 
 public class NecessaryEnablingsolver extends KInductionSolver {
 
+	private int sat=0;
+	private int unsat=0;
+	private long timestamp=0;
+	
 	public NecessaryEnablingsolver(Configuration smtConfig, Solver engine) {
 		super(smtConfig, engine, false);		
+	}
+	
+	private void clearStats() {
+		timestamp=System.currentTimeMillis();
+		sat=0;
+		unsat=0;
+	}
+	
+	private void printStats() {
+		Logger.getLogger("fr.lip6.move.gal").info("Computation took " + ( System.currentTimeMillis() - timestamp ) + " ms. Total solver calls (SAT/UNSAT): "+ (sat+unsat) + "("+ sat + "/" + unsat + ")");
 	}
 	
 	@Override
 	public void init(INextBuilder nextb) {
 		super.init(nextb);
+		addKnownInvariants(0);
 		addKnownInvariants(1);
 	}
 	
-	public List<int[]> computeAblingMatrix (boolean isEnabler) {
+	public List<int[]> computeAblingMatrix (boolean isEnabler, DependencyMatrix dm) {
 		List<int[]> matrix = new ArrayList<int[]>(nbTransition);
 
+		Logger.getLogger("fr.lip6.move.gal").info("Computing symmetric may "+ (isEnabler ? "enable" : "disable")+ " matrix : " + nbTransition + " transitions.");
+		clearStats();
 		for (int tindex = 0 ; tindex < nbTransition ; tindex++) {
 			if (isEnabler)
-				matrix.add(computeEnablers(tindex));
+				matrix.add(computeEnablers(tindex,dm));
 			else
-				matrix.add(computeDisablers(tindex));
+				matrix.add(computeDisablers(tindex,dm));
 		}
-
+		printStats();
 		return matrix;
 	}
 
-	private int [] computeAbling (int target, boolean isEnabler) {
+	private int [] computeAbling (int target, boolean isEnabler, DependencyMatrix dm) {
 		int [] toret = new int[nbTransition];
 		
 		solver.push(1);
@@ -60,19 +78,26 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 			
 		}
 		for (int i =0; i < nbTransition ; i++) {
-			solver.push(1);
-			solver.assertExpr(efactory.fcn(efactory.symbol(TRANSNAME+i),efactory.numeral(0)));
-		
-			Result res = checkSat();
-			Logger.getLogger("fr.lip6.move.gal").info("Checking if "+i +" "+ (isEnabler ? "enables" : "disables") +"  "+target + " :" + res);
-			if (res == Result.SAT) {
-				toret[i] = 1;
-			} else if (res == Result.UNSAT){
-				toret[i] = 0;
+			if (! dm.getWrite(i).intersects(dm.getRead(target)) ) {
+				// no need for SAT call
+				toret[i] = 0;				
 			} else {
-				throw new RuntimeException("SMT solver raised an error in enabler solving :"+res);
+				solver.push(1);
+				solver.assertExpr(efactory.fcn(efactory.symbol(TRANSNAME+i),efactory.numeral(0)));
+
+				Result res = checkSat();
+				// Logger.getLogger("fr.lip6.move.gal").info("Checking if "+i +" "+ (isEnabler ? "enables" : "disables") +"  "+target + " :" + res);
+				if (res == Result.SAT) {
+					toret[i] = 1;
+					sat++;
+				} else if (res == Result.UNSAT){
+					toret[i] = 0;
+					unsat++;
+				} else {
+					throw new RuntimeException("SMT solver raised an error in enabler solving :"+res);
+				}
+				solver.pop(1);
 			}
-			solver.pop(1);
 		}
 
 		solver.pop(1);
@@ -109,11 +134,13 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 		}
 		
 		Result res = checkSat();
-		Logger.getLogger("fr.lip6.move.gal").info("Checking co enabling of "+t1 + " and " + t2 + " : " + res);
+		//Logger.getLogger("fr.lip6.move.gal").info("Checking co enabling of "+t1 + " and " + t2 + " : " + res);
 		solver.pop(1);
 		if (res == Result.SAT) {
+			sat++;
 			return true;
 		} else if (res == Result.UNSAT){
+			unsat++;
 			return false;
 		} else {
 			throw new RuntimeException("SMT solver raised an error in enabler solving :"+res);
@@ -122,12 +149,12 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 	
 	
 	
-	private int [] computeDisablers (int target) {
-		return computeAbling(target, false);
+	private int [] computeDisablers (int target, DependencyMatrix dm) {
+		return computeAbling(target, false, dm);
 	}
 	
-	private int [] computeEnablers (int target) {
-		return computeAbling(target, true);
+	private int [] computeEnablers (int target, DependencyMatrix dm) {
+		return computeAbling(target, true, dm);
 	}
 
 	public List<int[]> computeCoEnablingMatrix() {
@@ -135,6 +162,8 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 		for (int tindex = 0; tindex < nbTransition ; tindex++) {
 			coEnabled.add(new int[nbTransition]);
 		}
+		Logger.getLogger("fr.lip6.move.gal").info("Computing symmetric co enabling matrix : " + nbTransition + " transitions.");
+		clearStats();
 		for (int t1 = 0 ; t1 < nbTransition ; t1++) {
 			for (int t2 = t1 ; t2 < nbTransition ; t2++) {
 				if ( canBeCoenabled(t1, t2) ) {
@@ -143,12 +172,13 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 				}
 			}
 		}
+		printStats();
 		return coEnabled;
 	}
 
 	
 	
-	public List<int[]> computeDoNotAccord (List<int[]> coEnabled, List<int[]> mayDisable) {
+	public List<int[]> computeDoNotAccord (List<int[]> coEnabled, List<int[]> mayDisable, DependencyMatrix dm) {
 		List<int[]> dnaMatrix = new ArrayList<>(nbTransition);
 		for (int tindex = 0; tindex < nbTransition ; tindex++) {
 			dnaMatrix.add(new int[nbTransition]);
@@ -170,10 +200,20 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 		addKnownInvariants(s5);
 
 		
+		Logger.getLogger("fr.lip6.move.gal").info("Computing Do-Not-Accords matrix : " + nbTransition + " transitions.");
+		clearStats();
 		for (int t1 = 0 ; t1 < nbTransition ; t1++) {
 			for (int t2 = t1+1 ; t2 < nbTransition ; t2++) {
 				if (coEnabled.get(t1)[t2] == 0) {
 					// we meet accords requirement : the implication is true if there is no coenabling
+					// put 0 in dna(t1,t2) = do nothing
+					continue;
+				}
+				if ( ! ( dm.getRead(t1).intersects(dm.getWrite(t2))
+						 || dm.getWrite(t1).intersects(dm.getWrite(t2))
+						 || dm.getWrite(t1).intersects(dm.getRead(t2)))) {
+					// these transitions cannot interfere, at best they share some read only variables
+					// we meet accords requirement 
 					// put 0 in dna(t1,t2) = do nothing
 					continue;
 				}
@@ -211,7 +251,7 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 				solver.assertExpr(tocheck);
 				
 				Result res = checkSat();
-				Logger.getLogger("fr.lip6.move.gal").info("Checking Do Not Accords relation of "+t1 + " and " + t2 + " : " + res);
+			//	Logger.getLogger("fr.lip6.move.gal").info("Checking Do Not Accords relation of "+t1 + " and " + t2 + " : " + res);
 				
 				solver.pop(1);
 				
@@ -219,11 +259,13 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 					// we found s4 != s5 through t1.t2 and t2.t1
 					dnaMatrix.get(t1)[t2] = 1;
 					dnaMatrix.get(t2)[t1] = 1;
+					sat++;
 				} else if (res == Result.UNSAT){
 					// we could not find s4 != s5 through t1.t2 and t2.t1
 					// so they are indeed commutative, accords is true
 					dnaMatrix.get(t1)[t2] = 0;
 					dnaMatrix.get(t2)[t1] = 0;
+					unsat++;
 				} else {
 					throw new RuntimeException("SMT solver raised an error in enabler solving :"+res);
 				}
@@ -231,6 +273,7 @@ public class NecessaryEnablingsolver extends KInductionSolver {
 		}
 		
 		solver.pop(1);
+		printStats();
 		
 		return dnaMatrix;
 	}
