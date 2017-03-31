ec3129a0172bc5e0e1d1f4a9239417e5fa5b1ee0
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-01 00:30:35 +0200

    very effective OTF simplification at low level of nested store/select
    allows to handle larger specs.
    Problem was nested stores in sequence of instructions, rule reduces :
    (select (store state indj rhs) indi) 
    with indi, indj integers and indi != inj
    ->
    (select state indi)

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/GalExpressionTranslator.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/GalExpressionTranslator.java
@@ -1,6 +1,8 @@
 package fr.lip6.move.gal.gal2smt.bmc;
 
 import org.smtlib.IExpr;
+import org.smtlib.IExpr.IFcnExpr;
+import org.smtlib.IExpr.INumeral;
 import org.smtlib.IExpr.ISymbol;
 import org.smtlib.SMT.Configuration;
 
@@ -27,11 +29,40 @@ public class GalExpressionTranslator extends ExpressionTranslator {
 			VariableReference vref = (VariableReference) ref;
 			ISymbol select = efactory.symbol("select");
 			IExpr indexlhs = efactory.numeral(index.getIndex(vref.getRef().getName()));
+			int indi = -1;
+			if (indexlhs instanceof INumeral) {
+				indi = ((INumeral) indexlhs).intValue();
+			}
 			if (vref.getIndex() != null) {
 				IExpr offset = translate(vref.getIndex(), state);
-
+				if (offset instanceof INumeral) {
+					indi += ((INumeral) offset).intValue();
+				} else {
+					indi = -1;
+				}
 				indexlhs = efactory.fcn(efactory.symbol("+"), indexlhs, offset);
 			}
+			if (indi != -1) {
+				// looking for :
+				// (select (store state indj rhs) indi); and indi/indj are ints, and indi!=indj
+//				(select (store state indj rhs) indi) 
+//				with indi, indj integers and indi != inj
+//				->
+//				(select state indi)
+
+				if (state instanceof IFcnExpr) {
+					IFcnExpr sfcn = (IFcnExpr) state;
+					if (sfcn.head().headSymbol().toString().equals("store") 
+							&& sfcn.args().get(1) instanceof INumeral) {
+						if (((INumeral) sfcn.args().get(1)).intValue() != indi) {
+							// skip one level
+							// recurse on nested array
+							return handleReference(ref,sfcn.args().get(0));
+						}
+					}
+				}
+			}
+			
 			return efactory.fcn(select, state, indexlhs);
 		}
 		throw new UnsupportedOperationException("Cannot handle qualified refs in GAL !");
