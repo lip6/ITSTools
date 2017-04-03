5aee11a96b6a57984d59b6cb36bf381fdee8cbfd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-03 20:16:49 +0200

    use apache.math (wip)

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/FlowMatrix.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/FlowMatrix.java
@@ -5,13 +5,19 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 
+import org.apache.commons.math.linear.RealMatrix;
+
 
 public class FlowMatrix {
 	// To represent the flow matrix, if we can build it. We use a sparse representation.
 	// Map variable index -> Transition index -> update to variable (a relative integer)
 	private Map<Integer, Map<Integer,Integer>> flow = new TreeMap<>();
 
+	private int maxT=0, maxV=0;
+	
 	public void addEffect(int tindex, int vindex, int val) {
+		maxT = Math.max(maxT, tindex);
+		maxV = Math.max(maxV, vindex);
 		Map<Integer, Integer> line = flow.get(vindex);
 		if (line == null) {
 			line  = new TreeMap<>();
@@ -29,4 +35,10 @@ public class FlowMatrix {
 		return flow.entrySet();
 	}
 	
+	
+	public RealMatrix getIncidenceMatrix () {
+		RealMatrix mat = null;
+		
+		return mat;
+	}
 }
