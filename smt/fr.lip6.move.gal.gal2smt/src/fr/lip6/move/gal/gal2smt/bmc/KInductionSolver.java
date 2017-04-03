e93d7f7f9bca33e4db01b3085e63170838a051ac
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-03 19:20:33 +0200

    unused import suppressed

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/KInductionSolver.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/KInductionSolver.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.logging.Logger;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
