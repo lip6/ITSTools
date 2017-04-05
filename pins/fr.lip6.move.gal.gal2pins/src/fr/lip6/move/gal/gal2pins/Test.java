4b3febbeebbd9b1d0fcf7b8e535d2b212741598b
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-06 00:54:52 +0200

    real tests for real men

--- pins/fr.lip6.move.gal.gal2pins/src/fr/lip6/move/gal/gal2pins/Test.java
+++ pins/fr.lip6.move.gal.gal2pins/src/fr/lip6/move/gal/gal2pins/Test.java
@@ -11,8 +11,9 @@ public class Test {
 	
 	public static void main(String[] args) {
 		SerializationUtil.setStandalone(true);
-		Specification spec = SerializationUtil.fileToGalSystem("tests/kanban5.gal");
+	//	Specification spec = SerializationUtil.fileToGalSystem("tests/kanban5.gal");
 	//	Specification spec = SerializationUtil.fileToGalSystem("tests/csRepeat-2-col.pnml.flat.gal");
+		Specification spec = SerializationUtil.fileToGalSystem("tests/Lotos-Garavel.gal");
 		//Specification spec = SerializationUtil.fileToGalSystem("tests/gear.1.flat.gal");
 		//Specification spec = SerializationUtil.fileToGalSystem("tests/elevator.3.flat.gal");
 		Gal2PinsTransformerNext g2p = new Gal2PinsTransformerNext();
