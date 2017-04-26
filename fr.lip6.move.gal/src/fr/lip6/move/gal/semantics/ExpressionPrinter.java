66d13a9444ee393aab398639e5a883207faca4cd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-26 19:14:59 +0200

    hacking qualified refs for now

--- fr.lip6.move.gal/src/fr/lip6/move/gal/semantics/ExpressionPrinter.java
+++ fr.lip6.move.gal/src/fr/lip6/move/gal/semantics/ExpressionPrinter.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayOutputStream;
 
 import org.eclipse.emf.ecore.EObject;
 
+import fr.lip6.move.gal.BooleanExpression;
+import fr.lip6.move.gal.Reference;
 import fr.lip6.move.gal.VariableReference;
 import fr.lip6.move.serialization.BasicGalSerializer;
 
@@ -19,7 +21,7 @@ public class ExpressionPrinter {
 	 * Return a string representing the provided object (Assignment, Bool or Int expression), resolving variables using the indexer (i.e. a single big array for all the state variables).
 	 * @param ass a GAL element to print such as an assignment
 	 * @param stateName a string the is used to denote the current state.
-	 * @param index resolve ariables to indexes in the state vector
+	 * @param index resolve variables to indexes in the state vector
 	 * @return a pretty string
 	 */
 	public static String print(EObject ass, String stateName, VariableIndexer index) {
@@ -43,6 +45,29 @@ public class ExpressionPrinter {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		bgs.setStream(baos, 2);
 		bgs.doSwitch(ass);
+		bgs.close();
+		return baos.toString();
+	}
+
+	public static String printQualifiedExpression(BooleanExpression atomicProp, String stateName, INextBuilder nb) {
+		BasicGalSerializer bgs = new BasicGalSerializer() {
+
+			@Override
+			public Boolean caseReference(Reference vref) {
+				pw.print(stateName + "[" + nb.getIndex(vref) + "]");
+				return true;
+			}
+			
+			@Override
+			public Boolean caseVariableReference(VariableReference vref) {
+				pw.print(stateName + "[" + nb.getIndex(vref) + "]");
+				return true;
+			}
+
+		};
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		bgs.setStream(baos, 2);
+		bgs.doSwitch(atomicProp);
 		bgs.close();
 		return baos.toString();
 	}
