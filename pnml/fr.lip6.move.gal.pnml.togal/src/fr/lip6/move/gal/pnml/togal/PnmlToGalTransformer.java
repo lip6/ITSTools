42dbfa3422866153977492ab4078dbc1f6a94298
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-27 11:36:32 +0200

    Recover even if NUPN order is incomplete (e.g. not covering)
    We now create a new partition element for missing places in NUPN info.

--- pnml/fr.lip6.move.gal.pnml.togal/src/fr/lip6/move/gal/pnml/togal/PnmlToGalTransformer.java
+++ pnml/fr.lip6.move.gal.pnml.togal/src/fr/lip6/move/gal/pnml/togal/PnmlToGalTransformer.java
@@ -4,14 +4,21 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import fr.lip6.move.gal.GALTypeDeclaration;
 import fr.lip6.move.gal.GalFactory;
 import fr.lip6.move.gal.Specification;
+import fr.lip6.move.gal.Variable;
 import fr.lip6.move.gal.nupn.NotAPTException;
 import fr.lip6.move.gal.nupn.PTNetReader;
+import fr.lip6.move.gal.order.CompositeGalOrder;
 import fr.lip6.move.gal.order.IOrder;
+import fr.lip6.move.gal.order.IOrderVisitor;
+import fr.lip6.move.gal.order.VarOrder;
+import fr.lip6.move.gal.support.Support;
 import fr.lip6.move.pnml.framework.general.PnmlImport;
 import fr.lip6.move.pnml.framework.hlapi.HLAPIRootClass;
 import fr.lip6.move.pnml.framework.utils.ModelRepository;
@@ -96,15 +103,45 @@ public class PnmlToGalTransformer {
 			try {
 				GALTypeDeclaration gal = trans.transform(ptnet);
 				spec.getTypes().add(gal);
+				// Scan for nupn tool specific unit info
+				if (ptreader.getOrder() != null) {
+					getLog().info("Found NUPN structural information;");
+					order = ptreader.getOrder();
+					
+					Set<String> exist = order.getAllVars();
+					Set<String> notexist = new HashSet<>();
+					Support supp = new Support();
+					// Make sure info is complete
+					for (Variable var : gal.getVariables()) {
+						if (! exist.contains(var.getName())) {
+							supp.add(var);
+							notexist.add(var.getName());
+						}
+					}
+					if (! notexist.isEmpty() ) {
+						getLog().info("Completing missing partition info from NUPN : creating a component with " + supp);
+						VarOrder vo = new VarOrder(supp, "missing");
+						order.accept( new IOrderVisitor<Void>() {
+
+							@Override
+							public Void visitComposite(CompositeGalOrder o) {
+								o.getChildren().add(vo);
+								return null;
+							}
+
+							@Override
+							public Void visitVars(VarOrder varOrder) {
+								varOrder.getVars().addAll(notexist);
+								return null;
+							}
+						});
+					}
+				}
+
 			} catch (ArithmeticException e) {
 				throw new IOException("Annotations (e.g. markings) use too many bits cannot handle transformation accurately.");
 			}
 
-			// Scan for nupn tool specific unit info
-			if (ptreader.getOrder() != null) {
-				getLog().info("Found NUPN structural information;");
-				order = ptreader.getOrder();
-			}
 		}
 
 		//	BoundsBuilder.boundVariable(spec, 4);
