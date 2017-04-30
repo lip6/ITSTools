40bef43ee00dae9139f5336ac88410a0da28baf8
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-30 18:42:59 +0200

    allow to copy err to out

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/Runner.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/Runner.java
@@ -14,14 +14,18 @@ import fr.lip6.move.gal.itstools.ProcessController.TimeOutException;
 
 public class Runner {
 	private static final String ID = "fr.lip6.move.gal";
-	
+
 	public static IStatus runTool(int timeout, CommandLine cl) throws IOException, TimeOutException {
+		return runTool(timeout, cl, false);
+	}
+	
+	public static IStatus runTool(int timeout, CommandLine cl, boolean errToOut) throws IOException, TimeOutException {
 		ByteArrayOutputStream stdOutput = new ByteArrayOutputStream();
 
-		return runTool(timeout, cl, stdOutput);
+		return runTool(timeout, cl, stdOutput, errToOut);
 	}
 
-	public static IStatus runTool(int timeout, CommandLine cl, OutputStream stdout) throws IOException, TimeOutException {
+	public static IStatus runTool(int timeout, CommandLine cl, OutputStream stdout, boolean errToOut) throws IOException, TimeOutException {
 		ByteArrayOutputStream errorOutput = new ByteArrayOutputStream();
 
 		final ProcessController controller = new ProcessController(timeout * 1000, cl.getArgs(), null,cl.getWorkingDir());
@@ -29,8 +33,12 @@ public class Runner {
 		controller.forwardOutput(stdout);
 		int exitCode = controller.execute();
 		if (errorOutput.size() > 0) {
+			if (errToOut) {
+				stdout.write(errorOutput.toByteArray());
+			}
 			Logger.getLogger(ID).info("Standard error output from running tool "+cl + "\n" + errorOutput.toString());
 		}
+		System.out.println("Exit code :"+exitCode);
 		if (exitCode != 0) {
 			if (errorOutput.size() > 0) {
 				return new Status(IStatus.WARNING, ID, errorOutput.toString());
