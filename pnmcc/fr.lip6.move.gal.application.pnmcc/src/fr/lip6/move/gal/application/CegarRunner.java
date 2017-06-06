d891ba9a0913be918c83efe00efc111f4d097580
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-06-06 15:56:25 +0200

    refactor to new IRunner API

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/CegarRunner.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/CegarRunner.java
@@ -13,10 +13,15 @@ import fr.lip6.move.gal.instantiate.CompositeBuilder;
 import fr.lip6.move.gal.instantiate.GALRewriter;
 import fr.lip6.move.gal.instantiate.Simplifier;
 
-public class CegarRunner {
-
+public class CegarRunner extends AbstractRunner implements IRunner {
 	
-	public static Thread runCegar(final Specification specNoProp, final String pwd, final Ender ender) {
+	private String pwd;
+
+	public CegarRunner(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public static Thread runCegar(final Specification spec, final String pwd, final Ender ender) {
 
 		Thread cegarRunner = new Thread(new Runnable() {
 
@@ -24,17 +29,17 @@ public class CegarRunner {
 			public void run() {
 				// current implem cannot deal with arrays
 				// degeneralize, should be ok for Petri nets at least
-				GALRewriter.flatten(specNoProp, true);
+				GALRewriter.flatten(spec, true);
 				CompositeBuilder cb = CompositeBuilder.getInstance();
-				cb.rewriteArraysAsVariables(specNoProp);
-				Simplifier.simplify(specNoProp);
+				cb.rewriteArraysAsVariables(spec);
+				Simplifier.simplify(spec);
 
-				final List<Property> properties = new ArrayList<Property>(specNoProp.getProperties());
+				final List<Property> properties = new ArrayList<Property>(spec.getProperties());
 				for (Property prop : properties) {
-					specNoProp.getProperties().clear();
-					specNoProp.getProperties().add(prop);
+					spec.getProperties().clear();
+					spec.getProperties().add(prop);
 					try {
-						IResult res = CegarFrontEnd.processGal(specNoProp, pwd);
+						IResult res = CegarFrontEnd.processGal(spec, pwd);
 						String ress = "FALSE";
 						if (res.isPropertyTrue()) {
 							ress = "TRUE";
@@ -63,4 +68,56 @@ public class CegarRunner {
 		return Logger.getLogger("fr.lip6.move.gal");
 		
 	}
+
+	@Override
+	public void solve(Ender ender ) {
+		runnerThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// current implem cannot deal with arrays
+				// degeneralize, should be ok for Petri nets at least
+				GALRewriter.flatten(spec, true);
+				CompositeBuilder cb = CompositeBuilder.getInstance();
+				cb.rewriteArraysAsVariables(spec);
+				Simplifier.simplify(spec);
+
+				final List<Property> properties = new ArrayList<Property>(spec.getProperties());
+				for (Property prop : properties) {
+					if (! doneProps.contains( prop.getName())) {
+						spec.getProperties().clear();
+						spec.getProperties().add(prop);
+						try {
+							IResult res = CegarFrontEnd.processGal(spec, pwd);
+							String ress = "FALSE";
+							if (res.isPropertyTrue()) {
+								ress = "TRUE";
+							}
+
+							System.out.println("FORMULA "+prop.getName()+ " "+ ress + " TECHNIQUES DECISION_DIAGRAMS COLLATERAL_PROCESSING TOPOLOGICAL CEGAR ");
+
+						} catch (IOException e) {
+							e.printStackTrace();
+							getLog().warning("Aborting CEGAR due to an exception");
+							return;
+						} catch (RuntimeException re) {
+							re.printStackTrace();
+							getLog().warning("Aborting CEGAR check of property " + prop.getName() + " due to an exception when running procedure.");
+						}
+					}
+				}
+				for (Property prop : properties) {
+					if (! doneProps.contains(prop.getName())) {
+						// still some work to do
+						return;
+					}
+				}
+				ender.killAll();
+				
+			}
+		});
+		runnerThread.start();
+	}
+	
+	
 }
