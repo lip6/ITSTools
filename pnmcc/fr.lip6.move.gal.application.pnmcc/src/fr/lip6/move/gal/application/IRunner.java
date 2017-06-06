c2073f59416794a19ce76d860186d75414271286
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-06-06 16:47:34 +0200

    exceptions

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/IRunner.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/IRunner.java
@@ -1,5 +1,6 @@
 package fr.lip6.move.gal.application;
 
+import java.io.IOException;
 import java.util.Set;
 
 import fr.lip6.move.gal.Specification;
@@ -10,7 +11,7 @@ public interface IRunner {
 
 	void join() throws InterruptedException;
 
-	void configure(Specification z3Spec, Set<String> doneProps);
+	void configure(Specification z3Spec, Set<String> doneProps) throws IOException;
 
 	void solve(Ender application);
 	
