708864a2b4ac5152f74f4d88912204e82a48e0ce
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-06-06 16:47:45 +0200

    exceptions

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/AbstractRunner.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/AbstractRunner.java
@@ -1,5 +1,6 @@
 package fr.lip6.move.gal.application;
 
+import java.io.IOException;
 import java.util.Set;
 
 import fr.lip6.move.gal.Specification;
@@ -28,7 +29,7 @@ public abstract class AbstractRunner implements IRunner  {
 	}
 
 	@Override
-	public void configure(Specification z3Spec, Set<String> doneProps) {
+	public void configure(Specification z3Spec, Set<String> doneProps) throws IOException {
 		this.spec = z3Spec ;
 		this.doneProps = doneProps;
 	}
