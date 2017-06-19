09c3f0f8d7abaf2f337bfb56aa716732939bb451
mamoussou <manuelamous@gmail.com>
2017-06-19 18:57:51 +0200

    Deleting useless files

--- itstools/fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/devTools/Attribute.java
+++
@@ -1,9 +0,0 @@
-package fr.lip6.move.gal.itstools.launch.devTools;
-
-import java.util.Set;
-
-public interface Attribute<T> {
-
-	public void setDefaultAttribute(T a);
-	public Set<T> getAttributes();
-}
