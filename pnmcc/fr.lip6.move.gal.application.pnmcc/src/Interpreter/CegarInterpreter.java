abac8e75c7e7b1c84034224fc98f6747d28a24d0
Anissa Kheireddine <ani.kheireddine@hotmail.com>
2017-06-19 15:54:43 +0200

    test

---
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/Interpreter/CegarInterpreter.java
@@ -0,0 +1,18 @@
+package Interpreter;
+
+
+import fr.lip6.move.gal.itscl.application.IListener;
+import fr.lip6.move.gal.itscl.application.Listener;
+
+public class CegarInterpreter extends Listener implements Runnable, IListener{
+
+	public CegarInterpreter(int pipeSize) {
+		super(pipeSize);
+	}
+
+	public void run() {
+		
+	}
+
+
+}
