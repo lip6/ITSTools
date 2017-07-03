b93c072939fc533def236e6d0308da6eaf34dfe0
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-07-03 15:29:16 +0200

    comment API

--- itstools/fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/devtools/AbstractOption.java
+++ itstools/fr.lip6.move.gal.itstools.launch/src/fr/lip6/move/gal/itstools/launch/devtools/AbstractOption.java
@@ -1,10 +1,14 @@
 package fr.lip6.move.gal.itstools.launch.devtools;
 
+
+/**
+ * Abstract class carrying common elements to all IOption<T> implementations.
+ */
 public abstract class AbstractOption<T> implements IOption<T> {
 
-	private String name;
-	private String toolTip;
-	private T defaultValue;
+	private final String name;
+	private final String toolTip;
+	private final T defaultValue;
 
 	public AbstractOption(String name, String toolTip, T defaultValue) {
 		this.name = name;
@@ -12,16 +16,25 @@ public abstract class AbstractOption<T> implements IOption<T> {
 		this.defaultValue = defaultValue;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public final T getDefaultValue() {
 		return defaultValue;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public final String getToolTip() {
 		return toolTip;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public final String getName() {
 		return name;
