fd6d2e1d8882d2a1bbab14fd2d50d0e442779ea0
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-10-06 15:06:48 +0200

    fuse identical statements as we export to C

--- pins/fr.lip6.move.gal.gal2pins/src/fr/lip6/move/gal/gal2pins/ToCPrinter.java
+++ pins/fr.lip6.move.gal.gal2pins/src/fr/lip6/move/gal/gal2pins/ToCPrinter.java
@@ -1,6 +1,9 @@
 package fr.lip6.move.gal.gal2pins;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import fr.lip6.move.gal.semantics.Alternative;
 import fr.lip6.move.gal.semantics.Assign;
@@ -25,10 +28,11 @@ import fr.lip6.move.gal.semantics.Sequence;
  *
  */
 public class ToCPrinter implements NextVisitor<Integer> {
-	
+
 	private PrintWriter pw;
 	private int index = 0;
-	
+	private Map<String,Integer> unique = new HashMap<>();
+
 	public ToCPrinter(PrintWriter pw) {
 		this.pw = pw;
 	}
@@ -36,86 +40,124 @@ public class ToCPrinter implements NextVisitor<Integer> {
 	public int getIndex() {
 		return index;
 	}
-	
+
 	@Override
 	public Integer visit (Assign ass) {
-		pw.append("static inline state_t * nextStatement"+(index++)+" (state_t * current) {\n");
-		pw.append("  for (state_t * it = current; it ; it = it->next) {\n");
-		pw.append("    int * dst = it->state;  // the next state values\n");
-		pw.append(ExpressionPrinter.print(ass.getAssignment(),"dst", ass.getIndexer()));
-		pw.append("  }\n");
-		pw.append("  return current;\n");
-		pw.append("}\n");		
-		return index-1;
+		String assText = ExpressionPrinter.print(ass.getAssignment(),"dst", ass.getIndexer());
+		Integer ind = unique.get(assText);
+		int curindex = index;
+		if (ind == null) {
+			unique.put(assText, curindex);
+			index++;
+
+			pw.append("static inline state_t * nextStatement"+ curindex+" (state_t * current) {\n");
+			pw.append("  for (state_t * it = current; it ; it = it->next) {\n");
+			pw.append("    int * dst = it->state;  // the next state values\n");
+			pw.append(assText);
+			pw.append("  }\n");
+			pw.append("  return current;\n");
+			pw.append("}\n");
+		} else {
+			curindex = ind;
+		}
+		return curindex;
 	}
 
 	@Override
 	public Integer visit(Predicate pred) {
-		pw.append("static inline state_t * nextStatement"+(index++)+" (state_t * current) {\n");
-		pw.append("  state_t ** pcur = & current;\n");
-		pw.append("  state_t * next = NULL;\n");
-		pw.append("  for (state_t * it = current; it ; it = next) {\n");
-		pw.append("    int * src = it->state;\n");
-		pw.append("    if (");
-		pw.append(         ExpressionPrinter.print(pred.getGuard(),"src", pred.getIndexer()));
-		pw.append(         ") {\n");
-		pw.append("      *pcur = it; pcur = & it->next ; next = it->next ; *pcur = NULL ;  \n");		
-		pw.append("    } else {\n");
-		pw.append("      next = it->next;\n");		
-		pw.append("      free(it);\n");
-		pw.append("    }\n");
-		pw.append("  }\n");
-		pw.append("  *pcur=NULL;\n");
-		pw.append("  return current;\n");
-		pw.append("}\n");
-		return index-1;
+		String assText = ExpressionPrinter.print(pred.getGuard(),"src", pred.getIndexer());
+		Integer ind = unique.get(assText);
+		int curindex = index;
+		if (ind == null) {
+			unique.put(assText, curindex);
+			index++;
+
+			pw.append("static inline state_t * nextStatement"+curindex+" (state_t * current) {\n");
+			pw.append("  state_t ** pcur = & current;\n");
+			pw.append("  state_t * next = NULL;\n");
+			pw.append("  for (state_t * it = current; it ; it = next) {\n");
+			pw.append("    int * src = it->state;\n");
+			pw.append("    if (");
+			pw.append(         assText);
+			pw.append(         ") {\n");
+			pw.append("      *pcur = it; pcur = & it->next ; next = it->next ; *pcur = NULL ;  \n");		
+			pw.append("    } else {\n");
+			pw.append("      next = it->next;\n");		
+			pw.append("      free(it);\n");
+			pw.append("    }\n");
+			pw.append("  }\n");
+			pw.append("  *pcur=NULL;\n");
+			pw.append("  return current;\n");
+			pw.append("}\n");
+		} else {
+			curindex = ind;
+		}
+		return curindex;
 	}
 
 	@Override
 	public Integer visit(Alternative alt) {
-		int curindex = index ++;
 		int max = alt.getAlternatives().size();
 		int [] indexes = new int[max];
 		int ii=0;
 		for (INext act : alt.getAlternatives()) {
 			indexes[ii++] = act.accept(this);
 		}
+		String key = "ALT" + Arrays.toString(indexes);		
+		Integer ind = unique.get(key);
+		int curindex = index;
+		if (ind == null) {
+			unique.put(key, curindex);
+			index++;
 
-		pw.append("static inline state_t * nextStatement"+curindex+" (state_t * current) {\n");
-		pw.append("  state_t * copy = NULL;\n");
-		pw.append("  state_t * result = NULL;\n");		
-		pw.append("  state_t ** it;\n");
-		for (int i=0 ; i < max ; i++) {
-			pw.append("  /* Alternative "+ i +" */\n");
-			pw.append("  copy = NULL; it = &copy; \n");
-			pw.append("  for (state_t * jt = current ; jt ; jt = jt->next)  \n"
-					+ "    { state_t * new = malloc(sizeof(state_t)); \n * new = *jt ; new->next = NULL ; \n  *it = new ; \n  it = & new->next; }\n");
-			pw.append("  copy = nextStatement"+ (indexes[i])+"(copy);\n");
-			pw.append("  for (it = &copy ; *it ; it = & (*it)->next) /*NOP*/;\n");
-			pw.append("  *it = result; result = copy;\n\n");	
+			pw.append("static inline state_t * nextStatement"+curindex+" (state_t * current) {\n");
+			pw.append("  state_t * copy = NULL;\n");
+			pw.append("  state_t * result = NULL;\n");		
+			pw.append("  state_t ** it;\n");
+			for (int i=0 ; i < max ; i++) {
+				pw.append("  /* Alternative "+ i +" */\n");
+				pw.append("  copy = NULL; it = &copy; \n");
+				pw.append("  for (state_t * jt = current ; jt ; jt = jt->next)  \n"
+						+ "    { state_t * new = malloc(sizeof(state_t)); \n * new = *jt ; new->next = NULL ; \n  *it = new ; \n  it = & new->next; }\n");
+				pw.append("  copy = nextStatement"+ (indexes[i])+"(copy);\n");
+				pw.append("  for (it = &copy ; *it ; it = & (*it)->next) /*NOP*/;\n");
+				pw.append("  *it = result; result = copy;\n\n");	
+			}
+			pw.append("  for (state_t * jt=current ; jt ; ) { state_t * tmp = jt; jt = jt->next ; free(tmp);  }\n");
+			pw.append("  return result;\n");
+			pw.append("}\n");
+		} else {
+			curindex = ind;
 		}
-		pw.append("  for (state_t * jt=current ; jt ; ) { state_t * tmp = jt; jt = jt->next ; free(tmp);  }\n");
-		pw.append("  return result;\n");
-		pw.append("}\n");
 		return curindex;
 	}
-	
+
 
 	@Override
 	public Integer visit(Sequence seq) {
-		int curindex = index ++;
 		int max = seq.getActions().size();
 		int [] indexes = new int[max];
 		int ii=0;
 		for (INext act : seq.getActions()) {
 			indexes[ii++] = act.accept(this);
 		}
-		pw.append("static inline state_t * nextStatement"+curindex+" (state_t * current) {\n");
-		for (int i = 0 ;  i < max ; i++) {
-			pw.append("  current = nextStatement"+indexes[i]+"(current);\n");
+
+		String key = "SEQ" + Arrays.toString(indexes);		
+		Integer ind = unique.get(key);
+		int curindex = index;
+		if (ind == null) {
+			unique.put(key, curindex);
+			index++;
+
+			pw.append("static inline state_t * nextStatement"+curindex+" (state_t * current) {\n");
+			for (int i = 0 ;  i < max ; i++) {
+				pw.append("  current = nextStatement"+indexes[i]+"(current);\n");
+			}
+			pw.append("  return current;\n");
+			pw.append("}\n");
+		} else {
+			curindex = ind;
 		}
-		pw.append("  return current;\n");
-		pw.append("}\n");
 		return curindex;
 	}
 
