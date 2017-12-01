8cea1eb0cbdd39009576f444207ccf5269afb5d0
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-12-01 11:37:57 +0100

    unused import

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NextBMCSolver.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/bmc/NextBMCSolver.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 import org.smtlib.IPrinter;
 import org.smtlib.IResponse;
