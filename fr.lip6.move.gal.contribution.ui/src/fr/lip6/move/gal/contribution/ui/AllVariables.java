c1ff0e99a58e57736830263e1034be7022744809
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-02-16 16:09:06 +0100

    new step : call greatspn !

--- fr.lip6.move.gal.contribution.ui/src/fr/lip6/move/gal/contribution/ui/AllVariables.java
+++ fr.lip6.move.gal.contribution.ui/src/fr/lip6/move/gal/contribution/ui/AllVariables.java
@@ -1,8 +1,10 @@
 package fr.lip6.move.gal.contribution.ui;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import fr.lip6.move.gal.contribution.ui.handlers.OrderHandler;
+import fr.lip6.move.gal.greatspn.order.GreatSPNRunner;
 import fr.lip6.move.gal.pnml.togreatspn.PNMLToGreatSPN;
 import fr.lip6.move.pnml.ptnet.PetriNet;
 
@@ -17,8 +19,16 @@ public class AllVariables extends OrderHandler {
 	@Override
 	public void workOnSpec(PetriNet petriNet, String outpath) throws IOException {
 		
+		String model =  outpath + "model";
+		
 		PNMLToGreatSPN ptg = new PNMLToGreatSPN();
-		ptg.transform(petriNet, outpath);
+		ptg.transform(petriNet,model);
+		
+		String workFolder = outpath;
+		GreatSPNRunner run = new GreatSPNRunner(workFolder, model);
+		run.run();
+		
+		System.out.println(Arrays.toString(run.getOrder()));
 	}
 
 }
