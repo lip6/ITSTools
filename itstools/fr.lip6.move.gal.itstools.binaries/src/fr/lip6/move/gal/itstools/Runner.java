dc43083f50ebd7d32b0411b7e8613d944daee62e
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-03-07 19:23:36 +0100

    new implem based on Java 1.8 APIs

--- itstools/fr.lip6.move.gal.itstools.binaries/src/fr/lip6/move/gal/itstools/Runner.java
+++ itstools/fr.lip6.move.gal.itstools.binaries/src/fr/lip6/move/gal/itstools/Runner.java
@@ -1,50 +1,100 @@
 package fr.lip6.move.gal.itstools;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.logging.Logger;
-
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import fr.lip6.move.gal.itstools.CommandLine;
-import fr.lip6.move.gal.itstools.ProcessController;
-import fr.lip6.move.gal.itstools.ProcessController.TimeOutException;
 
 public class Runner {
 	private static final String ID = "fr.lip6.move.gal";
 
-	public static IStatus runTool(long timeout, CommandLine cl) throws IOException, TimeOutException {
-		return runTool(timeout, cl, false);
+	/**
+	 * Runs a process and wait for its termination. Outputs and errors are not captured.
+	 * Returns the process exit value or throws if timeout is reached.
+	 * @param timeout a timeout in seconds
+	 * @param cl the command line to run, carries working folder and command line arguments.
+	 * @return the exit value of the process when it finished normally
+	 * @throws IOException in case of problems executing the command itself (starting a process)
+	 * @throws TimeoutException in case timeout is exceeded
+	 * @throws InterruptedException in case the thread calling this method is interrupted (e.g. some outside thread decided this run was no longer useful).
+	 */
+	public static int runTool(long timeout, CommandLine cl) throws IOException, TimeoutException, InterruptedException {
+		ProcessBuilder pb = new ProcessBuilder(cl.getArgs());
+		pb.directory(cl.getWorkingDir());
+		pb.redirectError(Redirect.INHERIT);
+		pb.redirectOutput(Redirect.INHERIT);
+		Process process = pb.start();
+		return waitForOrTimeout(timeout, TimeUnit.SECONDS, cl, process);
+	}
+
+	/**
+	 * Wait for a process to end ; if it doesn't end by timeout, kill it.
+	 * If we are interrupted, kill it.
+	 * @param timeout timeout expressed in givien time unit
+	 * @param tu the time unit
+	 * @param cl command line being run by the process, used to label exceptions with readable messages.
+	 * @param process the process we are monitoring
+	 * @return the exit code of the process if it finished normally
+	 * @throws TimeoutException if timeout was reached. We kill the process if this happens.
+	 * @throws InterruptedException if somebody interrupted the thread. We kill the process when that happens.
+	 */
+	public static int waitForOrTimeout(long timeout, TimeUnit tu, CommandLine cl, Process process)
+			throws TimeoutException, InterruptedException {
+		try {
+			if (process.waitFor(timeout, tu)) {
+				// normal timely termination
+				return process.exitValue();
+			} else {
+				// timeout
+				killProcess(process);
+				throw new TimeoutException("Subprocess running "+cl+" killed by timeout after "+timeout +" " + TimeUnit.SECONDS);
+			}
+		} catch (InterruptedException e) {
+			// forcefully asked to die			
+			killProcess(process);
+			throw e;
+		}
+	}
+
+	/**
+	 * Try to play nice and ask the process to die, but kill -9 if it does not respond within 100 ms.
+	 * @param process a process to kill
+	 * @throws InterruptedException if we were interrupted within the critical 100 ms between soft and hard kill.
+	 */
+	private static void killProcess(Process process) throws InterruptedException {
+		process.destroy();
+		if (! process.waitFor(100, TimeUnit.MILLISECONDS)) {
+			process.destroyForcibly();
+		}
 	}
 	
-	public static IStatus runTool(long timeout, CommandLine cl, boolean errToOut) throws IOException, TimeOutException {
-		ByteArrayOutputStream stdOutput = new ByteArrayOutputStream();
-
-		return runTool(timeout, cl, stdOutput, errToOut);
-	}
-
-	public static IStatus runTool(long timeout, CommandLine cl, OutputStream stdout, boolean errToOut) throws IOException, TimeOutException {
-		ByteArrayOutputStream errorOutput = new ByteArrayOutputStream();
-
-		final ProcessController controller = new ProcessController(timeout * 1000, cl.getArgs(), null,cl.getWorkingDir());
-		controller.forwardErrorOutput(errorOutput);
-		controller.forwardOutput(stdout);
-		int exitCode = controller.execute();
-		if (errorOutput.size() > 0) {
-			if (errToOut) {
-				stdout.write(errorOutput.toByteArray());
-			}
-			Logger.getLogger(ID).info("Standard error output from running tool "+cl + "\n" + errorOutput.toString());
+	
+	/**
+	 * Runs a process and wait for its termination. Outputs and errors are captured (appended) into the provided file.
+	 * If errToOut is set to false, errors are set to INHERIT, i.e. will go to stderr of current process.
+	 * Returns the process exit value or throws if timeout is reached.
+	 * @param timeout a timeout in seconds
+	 * @param cl the command line to run, carries working folder and command line arguments.
+	 * @param stdout a writeable File for the tool to append its output
+	 * @param errToOut if true, merges stderr into the output file, if false INHERIT stderr.
+	 * @return the exit value of the process when it finished normally
+	 * @throws IOException in case of problems executing the command itself (starting a process), writing to the requested file
+	 * @throws TimeoutException in case timeout is exceeded
+	 * @throws InterruptedException in case the thread calling this method is interrupted (e.g. some outside thread decided this run was no longer useful).
+	 */
+	public static int runTool(long timeout, CommandLine cl, File stdout, boolean errToOut) throws IOException, TimeoutException, InterruptedException {
+		ProcessBuilder pb = new ProcessBuilder(cl.getArgs());
+		pb.directory(cl.getWorkingDir());
+		if (errToOut) {
+			pb.redirectErrorStream(true);
+		} else {
+			pb.redirectError(Redirect.INHERIT);
 		}
-		System.out.println("Exit code :"+exitCode);
-		if (exitCode != 0) {
-			if (errorOutput.size() > 0) {
-				return new Status(IStatus.WARNING, ID, exitCode, errorOutput.toString(),null);
-			}
-		}
-		stdout.close();
-		return Status.OK_STATUS;
+		pb.redirectOutput(stdout);
+		Process process = pb.start();		
+		return waitForOrTimeout(timeout, TimeUnit.SECONDS, cl, process);
 	}
 }
