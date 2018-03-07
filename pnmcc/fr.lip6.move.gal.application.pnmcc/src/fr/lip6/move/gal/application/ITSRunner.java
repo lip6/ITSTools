61a069403375a9d6092467fd71f6e74edcf6b259
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-03-07 19:24:46 +0100

    significantly simplified with new Runner API

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/ITSRunner.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/ITSRunner.java
@@ -5,12 +5,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.util.TreeIterator;
@@ -34,18 +33,20 @@ public class ITSRunner extends AbstractRunner {
 	private boolean doITS;
 	private boolean onlyGal;
 	private String workFolder;
-	
+
 	private Thread itsReader;
-	
-	
-	
+	private long timeout;
+
+
+
 	public ITSRunner(String examination, MccTranslator reader, boolean doITS, boolean onlyGal,
-			String workFolder) {
+			String workFolder, long timeout) {
 		this.examination = examination;
 		this.reader = reader;
 		this.doITS = doITS;
 		this.onlyGal = onlyGal;
 		this.workFolder = workFolder;
+		this.timeout = timeout;
 	}
 
 
@@ -66,37 +67,37 @@ public class ITSRunner extends AbstractRunner {
 			}
 		} else if (examination.startsWith("CTL")) {
 			// reader.removeAdditionProperties();
-			
-			
+
+
 			if (doITS || onlyGal) {								
 				String outpath = outputGalFile(); 
-				
+
 				String ctlpath = outputPropertyFile(); 
-				
+
 				cl = buildCommandLine(outpath, Tool.ctl);
 
 				cl.addArg("-ctl");
 				cl.addArg(ctlpath);	
-				
+
 				//cl.addArg("--backward");
 			}
 		} else if (examination.startsWith("LTL")) {
-						
+
 			if (doITS || onlyGal) {
 				String outpath = outputGalFile();
 				String ltlpath = outputPropertyFile();
-				
-				
+
+
 				cl = buildCommandLine(outpath, Tool.ltl);
 				cl.addArg("-LTL");
 				cl.addArg(ltlpath);	
 
 				cl.addArg("-c");
 				//cl.addArg("-SSLAP-FSA");
-				
+
 				cl.addArg("-stutter-deadlock");
 			}
-				
+
 		} else if (examination.startsWith("Reachability") || examination.contains("Bounds")) {
 			if (doITS || onlyGal) {				
 				// decompose + simplify as needed
@@ -118,7 +119,7 @@ public class ITSRunner extends AbstractRunner {
 			cl.setWorkingDir(new File(workFolder));
 		}
 	}
-	
+
 	@Override
 	public void interrupt() {
 		super.interrupt();
@@ -126,7 +127,7 @@ public class ITSRunner extends AbstractRunner {
 			itsReader.interrupt();
 		}
 	}
-	
+
 	@Override
 	public void join() throws InterruptedException {
 		super.join();
@@ -136,7 +137,7 @@ public class ITSRunner extends AbstractRunner {
 	}
 
 	class ITSInterpreter implements Runnable {
-	
+
 		private BufferedReader in;
 		//private Map<String, List<Property>> boundProps;
 		private String examination;
@@ -145,7 +146,7 @@ public class ITSRunner extends AbstractRunner {
 		private Set<String> seen;
 		private Set<String> todoProps;
 		private Ender ender;
-		
+
 
 		public ITSInterpreter(String examination, boolean withStructure, MccTranslator reader, Set<String> doneProps, Set<String> todoProps, Ender ender) {			
 			this.examination = examination;
@@ -162,7 +163,7 @@ public class ITSRunner extends AbstractRunner {
 
 		@Override
 		public void run() {
-			
+
 
 			try {
 				for (String line = ""; line != null ; line=in.readLine() ) {
@@ -192,7 +193,7 @@ public class ITSRunner extends AbstractRunner {
 					}
 					if ( line.matches("System contains.*deadlocks.*")) {
 						if (examination.equals("ReachabilityDeadlock")) {
-							
+
 							Property dead = reader.getSpec().getProperties().get(0);
 							String pname = dead.getName();
 							double nbdead = Double.parseDouble(line.split("\\s+")[2]);
@@ -212,7 +213,7 @@ public class ITSRunner extends AbstractRunner {
 							String [] tab = line.split("<=");
 
 							String sbound = tab[2].replaceAll("\\s", "");
-							
+
 							int bound = Integer.parseInt(sbound);
 							Property target = null;
 							for (Property prop : reader.getSpec().getProperties()) {
@@ -260,7 +261,7 @@ public class ITSRunner extends AbstractRunner {
 							seen.add(pname);
 						}
 					}
-					
+
 					if ( line.matches(".*-"+examination+"-\\d+.*")) {
 						//System.out.println(line);
 						String res;
@@ -292,45 +293,9 @@ public class ITSRunner extends AbstractRunner {
 				ender.killAll();
 			}
 		}
-		
-	}
-	
-	class ITSRealRunner implements Runnable {
-		private OutputStream pout;
-		private CommandLine cl;
-		
-		public ITSRealRunner(OutputStream pout, CommandLine cl) {
-			this.pout = pout;
-			this.cl = cl;
-		}
 
-		@Override
-		public void run() {
-
-			try {	
-				System.out.println("Invoking ITS tools like this :" + cl );
-				Runner.runTool(3500, cl, pout, false);
-			} catch (TimeOutException e) {
-				System.out.println("Detected timeout of ITS tools.");
-				return;
-				//					return new Status(IStatus.ERROR, ID,
-				//							"Check Service process did not finish in a timely way."
-				//									+ errorOutput.toString());
-			} catch (IOException e) {
-				System.out.println("Failure when invoking ITS tools.");
-				return;
-				//					return new Status(IStatus.ERROR, ID,
-				//							"Unexpected exception executing service."
-				//									+ errorOutput.toString(), e);
-			} finally {
-				try {
-					pout.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
 	}
+
 	private CommandLine buildCommandLine(String modelff) throws IOException {
 		return buildCommandLine(modelff,Tool.reach);
 	}
@@ -341,8 +306,8 @@ public class ITSRunner extends AbstractRunner {
 		cl.setModelType("CGAL");
 		return cl.getCommandLine();
 	}
-	
-	
+
+
 	public String outputPropertyFile() throws IOException {
 		String proppath = workFolder +"/" + examination ;
 		if (examination.contains("CTL")) {
@@ -356,50 +321,50 @@ public class ITSRunner extends AbstractRunner {
 			proppath += ".prop";
 			SerializationUtil.serializePropertiesForITSTools(getOutputPath(), spec.getProperties(), proppath);
 		}
-		
-		
+
+
 		return proppath;
 	}
-	
-//	private void buildProperty (File file) throws IOException {
-//		if (file.getName().endsWith(".xml") && file.getName().contains("Reachability") ) {
-//			
-//			// normal case
-//			{
-////				Properties props = fr.lip6.move.gal.logic.util.SerializationUtil.fileToProperties(file.getLocationURI().getPath().toString());
-//				// TODO : is the copy really useful ?
-//				Properties props = PropertyParser.fileToProperties(file.getPath().toString(), EcoreUtil.copy(spec));
-//				
-//				Specification specWithProps = ToGalTransformer.toGal(props);
-//
-//				if (order != null) {
-//					CompositeBuilder.getInstance().decomposeWithOrder((GALTypeDeclaration) specWithProps.getTypes().get(0), order.clone());
-//				}
-//				// compute constants
-//				Support constants = GALRewriter.flatten(specWithProps, true);
-//
-//				File galout = new File( file.getParent() +"/" + file.getName().replace(".xml", ".gal"));
-//				fr.lip6.move.serialization.SerializationUtil.systemToFile(specWithProps, galout.getAbsolutePath());
-//			} 
-//			// Abstraction case 
-//			if (file.getParent().contains("-COL-")) {
-//				ToGalTransformer.setWithAbstractColors(true);
-//				Properties props = PropertyParser.fileToProperties(file.getPath().toString(), EcoreUtil.copy(spec));
-//
-//				Specification specnocol = ToGalTransformer.toGal(props);
-//				Instantiator.instantiateParametersWithAbstractColors(specnocol);
-//				GALRewriter.flatten(specnocol, true);
-//
-//				File galout = new File( file.getParent() +"/" + file.getName().replace(".xml", ".nocol.gal"));
-//				fr.lip6.move.serialization.SerializationUtil.systemToFile(specnocol, galout.getAbsolutePath());
-//
-//				ToGalTransformer.setWithAbstractColors(false);
-//			}
-//
-//		}		
-//	}
 
-	
+	//	private void buildProperty (File file) throws IOException {
+	//		if (file.getName().endsWith(".xml") && file.getName().contains("Reachability") ) {
+	//			
+	//			// normal case
+	//			{
+	////				Properties props = fr.lip6.move.gal.logic.util.SerializationUtil.fileToProperties(file.getLocationURI().getPath().toString());
+	//				// TODO : is the copy really useful ?
+	//				Properties props = PropertyParser.fileToProperties(file.getPath().toString(), EcoreUtil.copy(spec));
+	//				
+	//				Specification specWithProps = ToGalTransformer.toGal(props);
+	//
+	//				if (order != null) {
+	//					CompositeBuilder.getInstance().decomposeWithOrder((GALTypeDeclaration) specWithProps.getTypes().get(0), order.clone());
+	//				}
+	//				// compute constants
+	//				Support constants = GALRewriter.flatten(specWithProps, true);
+	//
+	//				File galout = new File( file.getParent() +"/" + file.getName().replace(".xml", ".gal"));
+	//				fr.lip6.move.serialization.SerializationUtil.systemToFile(specWithProps, galout.getAbsolutePath());
+	//			} 
+	//			// Abstraction case 
+	//			if (file.getParent().contains("-COL-")) {
+	//				ToGalTransformer.setWithAbstractColors(true);
+	//				Properties props = PropertyParser.fileToProperties(file.getPath().toString(), EcoreUtil.copy(spec));
+	//
+	//				Specification specnocol = ToGalTransformer.toGal(props);
+	//				Instantiator.instantiateParametersWithAbstractColors(specnocol);
+	//				GALRewriter.flatten(specnocol, true);
+	//
+	//				File galout = new File( file.getParent() +"/" + file.getName().replace(".xml", ".nocol.gal"));
+	//				fr.lip6.move.serialization.SerializationUtil.systemToFile(specnocol, galout.getAbsolutePath());
+	//
+	//				ToGalTransformer.setWithAbstractColors(false);
+	//			}
+	//
+	//		}		
+	//	}
+
+
 	private String getOutputPath() {
 		return workFolder + "/"+ examination +".pnml.gal";
 	}
@@ -416,27 +381,42 @@ public class ITSRunner extends AbstractRunner {
 		}
 		return outpath;
 	}
-	
+
 	@Override
 	public void solve(Ender ender) {
-		final PipedInputStream pin = new PipedInputStream(4096);
-		PipedOutputStream pout= null;
+
+		ProcessBuilder pb = new ProcessBuilder(cl.getArgs());
+		pb.directory(cl.getWorkingDir());
+		pb.redirectErrorStream(true);
+
+		System.out.println("Invoking ITS tools like this :" + cl );
 		try {
-			pout = new PipedOutputStream(pin);
-		} catch (IOException e1) {
-			e1.printStackTrace();
+			final Process process = pb.start();	
+
+
+			Set<String> todoProps = reader.getSpec().getProperties().stream().map(p -> p.getName()).collect(Collectors.toSet());
+
+			runnerThread = new Thread (() -> {
+				try {
+					Runner.waitForOrTimeout(timeout, TimeUnit.SECONDS, cl, process); 
+				} catch (InterruptedException e) {
+					System.out.println("ITS tools runner thread asked to quit. Dying gracefully.");
+					return;
+				} catch (TimeoutException e) {
+					System.out.println("Detected timeout of ITS tools.");
+					return;
+				}
+			});
+
+			ITSInterpreter interp = new ITSInterpreter(examination, reader.hasStructure(), reader, doneProps, todoProps, ender);
+			interp.setInput(process.getInputStream());
+			itsReader = new Thread (interp);
+			itsReader.start();
+			runnerThread.start();
+		} catch (IOException e) {
+			System.out.println("Failure when invoking ITS tools."+e);
 			return;
 		}
-		
-		Set<String> todoProps = reader.getSpec().getProperties().stream().map(p -> p.getName()).collect(Collectors.toSet());
-		
-		runnerThread = new Thread (new ITSRealRunner(pout,cl));
-
-		ITSInterpreter interp = new ITSInterpreter(examination, reader.hasStructure(), reader, doneProps, todoProps, ender);
-		interp.setInput(pin);
-		itsReader = new Thread (interp);
-		itsReader.start();
-		runnerThread.start();
 	}
 
 }
