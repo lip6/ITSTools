a71207b4f95ec81d13dfe5543bfffa2615ccd91c
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-03-15 16:09:36 +0100

    also compute read matrix, to be able to distinguish read arcs.

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/FlowMatrix.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/FlowMatrix.java
@@ -1,4 +1,4 @@
-package fr.lip6.move.gal.gal2smt.bmc;
+package fr.lip6.move.gal.structural;
 
 import java.util.Map;
 import java.util.Map.Entry;
@@ -13,10 +13,11 @@ public class FlowMatrix {
 	// To represent the flow matrix, if we can build it. We use a sparse representation.
 	// Map variable index -> Transition index -> update to variable (a relative integer)
 	private Map<Integer, Map<Integer,Integer>> flow = new TreeMap<>();
+	private Map<Integer, Map<Integer,Integer>> read = new TreeMap<>();
 
 	private int maxT=0, maxV=0;
 	
-	public void addEffect(int tindex, int vindex, int val) {
+	public void addWriteEffect(int tindex, int vindex, int val) {
 		maxT = Math.max(maxT, tindex);
 		maxV = Math.max(maxV, vindex);
 		Map<Integer, Integer> line = flow.get(vindex);
@@ -32,6 +33,20 @@ public class FlowMatrix {
 		line.put(tindex, cur);
 	}
 
+	public void addReadEffect(int tindex, int vindex, int val) {
+		Map<Integer, Integer> line = read.get(vindex);
+		if (line == null) {
+			line  = new TreeMap<>();
+			read.put(vindex, line);
+		}
+		Integer cur = line.get(tindex);		
+		if (cur==null) {
+			cur=0;
+		}
+		cur= Math.max(cur,val);
+		line.put(tindex, cur);
+	}
+	
 	public Set<Entry<Integer, Map<Integer, Integer>>> entrySet() {
 		return flow.entrySet();
 	}
@@ -64,4 +79,18 @@ public class FlowMatrix {
 		
 		return mat;
 	}
+	public int[][] getReadMatrix() {
+		int  [][] mat = new int[maxV+1][maxT+1];
+		for (Entry<Integer, Map<Integer, Integer>> e : read.entrySet()) {
+			int row = e.getKey();
+			for (Entry<Integer, Integer> ee : e.getValue().entrySet()) {
+				int col = ee.getKey();
+				int val = ee.getValue();
+				mat [row][col] =  val;
+			}
+		}
+		
+		return mat;
+	}
+	
 }
