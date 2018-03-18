07e666951bef95578ce25b7f67c03fa5ad307010
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-03-18 17:51:34 +0100

    shortcuts for deterministic case, add synchronized

--- fr.lip6.move.gal/src/fr/lip6/move/gal/semantics/DeterministicNextBuilder.java
+++ fr.lip6.move.gal/src/fr/lip6/move/gal/semantics/DeterministicNextBuilder.java
@@ -3,6 +3,7 @@ package fr.lip6.move.gal.semantics;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,20 +16,99 @@ public class DeterministicNextBuilder extends NextBuilderDecorator implements ID
 	private List<List<INext>> deterministic = null;
 	private DependencyMatrix dm = null;
 
-	public List<List<INext>> getDeterministicNext() {
+	public synchronized List<List<INext>> getDeterministicNext() {
 		if (deterministic == null) {
 			List<INext> nextRel = getNextForLabel("");
-			INext allTrans = Alternative.alt(nextRel);
+			int total = 0;
+			int[] nondet = new int[nextRel.size()];
+			NextVisitor<Integer> counter = new NextVisitor<Integer>() {
 
-			List<INext> bootstrap = new ArrayList<>();
-			Determinizer det = new Determinizer(Collections.singleton(bootstrap).stream());
-			Stream<List<INext>> nextStream = allTrans.accept(det);
-			deterministic = nextStream.collect(Collectors.toList());
+				@Override
+				public Integer visit(Assign ass) {
+					return 1;
+				}
+
+				@Override
+				public Integer visit(Predicate pred) {
+					return 1;
+				}
+
+				@Override
+				public Integer visit(Alternative alt) {
+					int total = 1;
+					for (INext act : alt.getAlternatives()) {
+						total += act.accept(this);
+					}
+					return total;
+				}
+
+				@Override
+				public Integer visit(Sequence seq) {
+					int total = 1;
+					for (INext act : seq.getActions()) {
+						total *= act.accept(this);
+					}
+					return total;
+				}
+			};
+			int index =0;
+			for (INext  n : nextRel) {
+				int val = n.accept(counter);
+				nondet[index++]=val;
+				total += val;
+			}
+			if (total == nextRel.size()) {
+				deterministic = new ArrayList<>();
+				
+				for (INext n : nextRel) {
+					final List<INext> list = new ArrayList<>();
+					NextVisitor<Void> normalizer = new NextVisitor<Void>() {
+
+						@Override
+						public Void visit(Assign ass) {
+							list.add(ass);
+							return null;
+						}
+
+						@Override
+						public Void visit(Predicate pred) {
+							list.add(pred);
+							return null;
+						}
+
+						@Override
+						public Void visit(Alternative alt) {
+							throw new IllegalArgumentException();
+						}
+
+						@Override
+						public Void visit(Sequence seq) {
+							for (INext n : seq.getActions()) {
+								n.accept(this);
+							}
+							return null;
+						}
+						
+					};
+					n.accept(normalizer);
+					deterministic.add(list);
+				}
+				Logger.getLogger("fr.lip6.move.gal").info("Input system was already deterministic with "+deterministic.size()+" transitions.");
+			} else {
+				Logger.getLogger("fr.lip6.move.gal").info("Input system was not deterministic with "+nextRel.size()+" transitions. Expanding to a total of " + total + " determinsitic transitions.");
+
+				INext allTrans = Alternative.alt(nextRel);
+
+				List<INext> bootstrap = new ArrayList<>();
+				Determinizer det = new Determinizer(Collections.singleton(bootstrap).stream());
+				Stream<List<INext>> nextStream = allTrans.accept(det);
+				deterministic = nextStream.collect(Collectors.toList());
+			}
 		}
 		return deterministic;
 	}
 
-	public DependencyMatrix getDeterministicDependencyMatrix() {
+	public synchronized DependencyMatrix getDeterministicDependencyMatrix() {
 		if (dm == null) {
 			dm = new DependencyMatrix(getDeterministicNext(),size());
 		}
