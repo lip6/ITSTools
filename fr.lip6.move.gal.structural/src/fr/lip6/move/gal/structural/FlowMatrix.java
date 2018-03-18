804bb87176ba1ea9c4fe398fe2fc5a34e9a03bc8
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-03-18 15:24:43 +0100

    more index bugs

--- fr.lip6.move.gal.structural/src/fr/lip6/move/gal/structural/FlowMatrix.java
+++ fr.lip6.move.gal.structural/src/fr/lip6/move/gal/structural/FlowMatrix.java
@@ -8,22 +8,26 @@ public class FlowMatrix {
 	// Map variable index -> Transition index -> update to variable (a relative integer)
 	private MatrixCol flow ;
 	private MatrixCol read ;
+	private MatrixCol flowPT ;
+	private MatrixCol flowTP ;
 
 	public FlowMatrix (int nbvar, int nbtrans) {
 		flow = new MatrixCol(nbvar, nbtrans);
+		flowPT = new MatrixCol(nbvar, nbtrans);
+		flowTP = new MatrixCol(nbvar, nbtrans);
 		read = new MatrixCol(nbvar, nbtrans);
 	}
 	
 	public void addWriteEffect(int tindex, int vindex, int val) {
 		SparseIntArray line = flow.getColumn(tindex);
-		int cur = line.get(tindex);				
+		int cur = line.get(vindex);				
 		cur+=val;
 		line.put(vindex, cur);
 	}
 
 	public void addReadEffect(int tindex, int vindex, int val) {
 		SparseIntArray line = flow.getColumn(tindex);
-		int cur = line.get(tindex);				
+		int cur = line.get(vindex);				
 		cur=Math.max(cur,val);
 		line.put(vindex, cur);
 	}
