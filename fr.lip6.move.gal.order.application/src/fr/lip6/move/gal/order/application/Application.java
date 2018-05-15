6a2654f37a06af0d3f1af8c0ad95ab428c9c7255
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-15 15:48:31 +0200

    import code from MCC to deal with StateSpace

--- fr.lip6.move.gal.order.application/src/fr/lip6/move/gal/order/application/Application.java
+++ fr.lip6.move.gal.order.application/src/fr/lip6/move/gal/order/application/Application.java
@@ -3,23 +3,28 @@ package fr.lip6.move.gal.order.application;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
+import fr.lip6.move.gal.application.Ender;
+import fr.lip6.move.gal.application.ITSRunner;
+import fr.lip6.move.gal.application.MccTranslator;
 import fr.lip6.move.gal.nupn.PTNetReader;
 import fr.lip6.move.gal.order.flag.OrderHeuristic;
 import fr.lip6.move.gal.order.ordergenerator.IOrderGenerator;
 import fr.lip6.move.gal.order.ordergenerator.OrderGeneratorFactory;
 import fr.lip6.move.gal.pnml.togreatspn.PNMLToGreatSPN;
 import fr.lip6.move.pnml.ptnet.PetriNet;
+import fr.lip6.move.serialization.SerializationUtil;
 import fr.lip6.move.gal.order.order.IOrder;
-import fr.lip6.move.gal.order.PTGALTransformer;;
 
-public class Application implements IApplication {
+public class Application implements IApplication, Ender {
 	private static final String APPARGS = "application.args";
 	private static final String INPUT_FILE = "-i";
 	private static final String INPUT_TYPE = "-t";
@@ -80,14 +85,58 @@ public class Application implements IApplication {
 			orders.add(og.compute());
 		for(IOrder o : orders)
 			o.printOrder(workFolder);
+
+		// EMF registration 
+		SerializationUtil.setStandalone(true);
+		
+		// setup a "reader" that parses input property files correctly and efficiently
+		MccTranslator reader = new MccTranslator(pwd,"StateSpace");
+		
+		
+		try {			
+				// parse the model from PNML to GAL using PNMLFW for COL or fast SAX for PT models
+				reader.transformPNML();
+		} catch (IOException e) {
+			System.err.println("Incorrect file or folder " + pwd + "\n Error :" + e.getMessage());
+			if (e.getCause() != null) {
+				e.getCause().printStackTrace();
+			} else {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		// for debug and control COL files are small, otherwise 1MB PNML limit (i.e. roughly 200kB GAL max).
+		if (pwd.contains("COL") || new File(pwd + "/model.pnml").length() < 1000000) {
+			String outpath2 = pwd + "/model.pnml.img.gal";
+			SerializationUtil.systemToFile(reader.getSpec(), outpath2);
+		}
+		// ITS is the only method we will run.
+		boolean withHierarchy = false;
+		reader.flattenSpec(withHierarchy);
+
+		// decompose + simplify as needed
+		ITSRunnerGSPN itsRunner = new ITSRunnerGSPN("StateSpace", reader, true, false, reader.getFolder(),3600);
+		itsRunner.configure(reader.getSpec(), new HashSet<>());
+		
+
+		itsRunner.solve(this);
+		itsRunner.join();				
+
+		
 		return IApplication.EXIT_OK;
 	}
 	
 
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#stop()
-	 */
+
+	@Override
+	public void killAll() {
+	}
+
+
+
+	@Override
 	public void stop() {
+		
 	}
 }
