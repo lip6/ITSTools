6d96c270d73ce502319929e9609932571cb94c01
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-16 11:50:39 +0200

    a graph is a list of edges

---
+++ louvain/fr.lip6.move.gal.louvain/src/fr/lip6/move/gal/louvain/Graph.java
@@ -0,0 +1,26 @@
+package fr.lip6.move.gal.louvain;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Graph implements Iterable<Edge>{
+
+	private List<Edge> edges = new ArrayList<>();
+
+	public void forEach(Consumer<? super Edge> action) {
+		edges.forEach(action);
+	}
+
+	public Iterator<Edge> iterator() {
+		return edges.iterator();
+	}
+
+	public boolean add(Edge e) {
+		return edges.add(e);
+	}
+	
+	
+	
+}
