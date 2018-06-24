b3c03afa6c3af1280a77963206dd2966a6e3d900
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-06-24 20:26:08 +0200

    folder for include dir

--- ltsmin/fr.lip6.move.gal.ltsmin.preference/src/fr/lip6/move/gal/ltsmin/preference/PreferenceInitializer.java
+++ ltsmin/fr.lip6.move.gal.ltsmin.preference/src/fr/lip6/move/gal/ltsmin/preference/PreferenceInitializer.java
@@ -22,14 +22,22 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = LTSminPreferencesActivator.getDefault().getPreferenceStore();
 		
-//		store.setDefault(PreferenceConstants.GCTHRESHOLD, 1300000);
-		
 		try {
 			store.setDefault(PreferenceConstants.LTSMINSEQ_EXE, BinaryToolsPlugin.getProgramURI(Tool.seq).getPath().toString() );
 		} catch (IOException e) {
 			store.setDefault(PreferenceConstants.LTSMINSEQ_EXE, "Your platform seems to be unsupported." );
 		}
-
+		try {
+			store.setDefault(PreferenceConstants.LTSMINMC_EXE, BinaryToolsPlugin.getProgramURI(Tool.mc).getPath().toString() );
+		} catch (IOException e) {
+			store.setDefault(PreferenceConstants.LTSMINMC_EXE, "Your platform seems to be unsupported." );
+		}
+		
+		try {
+			store.setDefault(PreferenceConstants.LTSMININC_DIR, BinaryToolsPlugin.getIncludeFolderURI().getPath().toString());
+		} catch (IOException e) {
+			store.setDefault(PreferenceConstants.LTSMININC_DIR, "Your platform seems to be unsupported." );
+		}
 	}
 
 }
