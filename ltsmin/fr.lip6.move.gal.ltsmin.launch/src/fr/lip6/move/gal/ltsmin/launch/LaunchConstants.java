60ab8e8489ae6f027e3753eacf1b86fc19e9c642
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-06-24 16:48:22 +0200

    simplifying

--- ltsmin/fr.lip6.move.gal.ltsmin.launch/src/fr/lip6/move/gal/ltsmin/launch/LaunchConstants.java
+++ ltsmin/fr.lip6.move.gal.ltsmin.launch/src/fr/lip6/move/gal/ltsmin/launch/LaunchConstants.java
@@ -2,15 +2,8 @@ package fr.lip6.move.gal.ltsmin.launch;
 
 public class LaunchConstants {
 
-	public static final String MODEL_FILE = "Model File";
 	// make sure this matches id of LaunchConfigurationType in plugin declaration
 	public static final String ID_LAUNCH_TYPE= "fr.lip6.move.gal.ltsmin.launch.launchConfigurationType";
-	public static final String PROJECT = "project";
-	
-	
-	public static final String PLAY_TRACE = "trace";
-	public static final String CEGAR_PROP = "cegar prop";
-	public static final String QUIET = "quiet";
 	
 	public static final String COMMON_FLAGS = "cflags";
 	public static final String SEQ_FLAGS = "seqflags";
