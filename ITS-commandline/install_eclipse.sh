73636bd0baed6f0e26fb74d7d96dcbf2ecdc433e
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2019-02-28 15:40:45 +0100

    use new 2018-12 release (4.10)

--- ITS-commandline/install_eclipse.sh
+++ ITS-commandline/install_eclipse.sh
@@ -3,8 +3,9 @@
 set -x
 
 if [ ! -f eclipse/eclipse ] ; then 
-  if [ ! -f eclipse-platform-4.7.2-linux-gtk-x86_64.tar.gz ] ; then 
-    wget --progress=dot:mega http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.7.2-201711300510/eclipse-platform-4.7.2-linux-gtk-x86_64.tar.gz
+    if [ ! -f eclipse-platform-4.7.2-linux-gtk-x86_64.tar.gz ] ; then 
+	wget --progress=dot:mega http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.10-201812060815/eclipse-platform-4.10-linux-gtk-x86_64.tar.gz
+								      
 #    wget --progress=dot:mega http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.6.3-201703010400/eclipse-platform-4.6.3-linux-gtk-x86_64.tar.gz
 #   wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.6.2-201611241400/eclipse-platform-4.6.2-linux-gtk-x86_64.tar.gz
 #    wget http://mirror.ibcp.fr/pub/eclipse//eclipse/downloads/drops4/R-4.5.2-201602121500/eclipse-platform-4.5.2-linux-gtk-x86_64.tar.gz
@@ -31,8 +32,9 @@ cd eclipse
 -clean -purgeHistory \
 -application org.eclipse.equinox.p2.director \
 -noSplash \
--repository http://yanntm.github.io/Xtext-Light/,https://lip6.github.io/ITSTools \
+-repository https://lip6.github.io/ITSTools \
 -installIUs fr.lip6.move.gal.feature.pnmcc.feature.group
 
+# http://yanntm.github.io/Xtext-Light/
 #-repository http://download.eclipse.org/releases/neon,https://lip6.github.io/ITSTools \
 
