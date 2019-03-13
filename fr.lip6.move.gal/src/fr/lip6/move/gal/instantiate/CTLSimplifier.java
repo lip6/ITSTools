8f07dfee5f040c6437af0590d20951a56394b64c
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2019-03-13 15:04:32 +0100

    the function proposed in the paper of Jiri's group

--- fr.lip6.move.gal/src/fr/lip6/move/gal/instantiate/CTLSimplifier.java
+++ fr.lip6.move.gal/src/fr/lip6/move/gal/instantiate/CTLSimplifier.java
@@ -1,20 +1,26 @@
 package fr.lip6.move.gal.instantiate;
 
+import java.util.logging.Logger;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
 import fr.lip6.move.gal.AF;
 import fr.lip6.move.gal.AG;
 import fr.lip6.move.gal.AU;
+import fr.lip6.move.gal.AX;
 import fr.lip6.move.gal.And;
 import fr.lip6.move.gal.BooleanExpression;
 import fr.lip6.move.gal.CTLProp;
+import fr.lip6.move.gal.Comparison;
 import fr.lip6.move.gal.EF;
 import fr.lip6.move.gal.EG;
 import fr.lip6.move.gal.EU;
+import fr.lip6.move.gal.EX;
 import fr.lip6.move.gal.False;
 import fr.lip6.move.gal.GF2;
 import fr.lip6.move.gal.GalFactory;
+import fr.lip6.move.gal.Not;
 import fr.lip6.move.gal.Or;
 import fr.lip6.move.gal.Property;
 import fr.lip6.move.gal.Specification;
@@ -31,12 +37,130 @@ import fr.lip6.move.gal.True;
 public class CTLSimplifier {
 
 	public static void simplifyTemporal(Specification spec) {
+		int simplified = 0;
 		for (Property prop: spec.getProperties()) {
 			if (prop.getBody() instanceof CTLProp) {
 				CTLProp body = (CTLProp) prop.getBody();
 				simplify(body.getPredicate());
+				
+				int eval = evalInInitial(body.getPredicate());
+				if (eval == 1) {
+					body.setPredicate(GalFactory.eINSTANCE.createTrue());
+				} else if (eval == -1) {
+					body.setPredicate(GalFactory.eINSTANCE.createFalse());
+				}
+				if (eval != 0) {
+					simplified++;
+				}
 			}
 		}
+		if (simplified != 0 || true) {
+			Logger.getLogger("fr.lip6.move.gal").fine("Initial state reduction rules for CTL removed "+simplified+ " formulas.");
+		}
+	}
+	
+	
+	/** 
+	 * This function is an implementation of the Initial states simplification strategy proposed in Section 3 
+	 * of the paper : Simplification of CTL Formulae for Efficient Model Checking of Petri Nets
+	 * Published at IC PetriNets'2018
+	 * By Bonneland, et al. of Jiri Srba's group working on Tapaal.
+	 */
+	private static int evalInInitial (BooleanExpression predicate) {
+		if (predicate instanceof True) {
+			return 1 ;
+		} else if (predicate instanceof False) {
+			return -1;
+		} else if (predicate instanceof Comparison) {
+			if (PropertySimplifier.evalInInitialState(predicate)) {
+				return 1;
+			} else {
+				return -1;
+			}
+		} else if (predicate instanceof EX || predicate instanceof AX) {
+			return 0;
+		} else if (predicate instanceof Not) {
+			Not n = (Not) predicate;
+			return - evalInInitial(n.getValue());
+		} else if (predicate instanceof And) {
+			And and = (And) predicate;
+			
+			int l = evalInInitial(and.getLeft());
+			int r = evalInInitial(and.getRight());
+			
+			if (l==-1 || r==-1) {
+				return -1;
+			} else if (l==1 && r==1) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} else if (predicate instanceof Or) {
+			Or or = (Or) predicate;
+			
+			int l = evalInInitial(or.getLeft());
+			int r = evalInInitial(or.getRight());
+			
+			if (l==1 || r==1) {
+				return 1;
+			} else if (l==-1 && r==-1) {
+				return -1;
+			} else {
+				return 0;
+			}
+		} else if (predicate instanceof EG) {
+			EG op = (EG) predicate;
+			if (evalInInitial(op.getProp()) == -1) {
+				return -1;
+			} else {
+				return 0;
+			}
+		} else if (predicate instanceof AG) {
+			AG op = (AG) predicate;
+			if (evalInInitial(op.getProp()) == -1) {
+				return -1;
+			} else {
+				return 0;
+			}
+		} else if (predicate instanceof EF) {
+			EF op = (EF) predicate;
+			if (evalInInitial(op.getProp()) == 1) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} else if (predicate instanceof AF) {
+			AF op = (AF) predicate;
+			if (evalInInitial(op.getProp()) == 1) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} else if (predicate instanceof AU) {
+			AU op = (AU) predicate;
+			int evalr = evalInInitial(op.getRight());
+			if (evalr == 1) {
+				return 1;
+			} else if (evalr == -1) {
+				if (evalInInitial(op.getLeft()) == -1) {
+					return -1;
+				}
+			}
+			return 0;			
+		} else if (predicate instanceof EU) {
+			EU op = (EU) predicate;
+			int evalr = evalInInitial(op.getRight());
+			if (evalr == 1) {
+				return 1;
+			} else if (evalr == -1) {
+				if (evalInInitial(op.getLeft()) == -1) {
+					return -1;
+				}
+			}
+			return 0;			
+		}
+		Logger.getLogger("fr.lip6.move.gal").warning("Unexpected operator in CTL formula :" + predicate);
+		return 0;		
 	}
 
 	private static void simplify(BooleanExpression predicate) {
