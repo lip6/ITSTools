3e019013de7f8540a97b275574ab2953f34cf59c
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2019-03-22 16:07:22 +0100

    a class to run greatspn in

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/GreatSPNRunner.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/GreatSPNRunner.java
@@ -5,10 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 import fr.lip6.move.gal.process.CommandLine;
@@ -21,26 +18,14 @@ public class GreatSPNRunner {
 	private String modelPath;
 	private String[] order;
 	private ArrayList<String> config;
-	private String binpath;
 
 	public GreatSPNRunner(String workFolder, String modelPath, String path) throws IOException {// , String binPath
 		this.path = path;
-		this.binpath = new File(path).getParentFile().getCanonicalPath();
 		this.workFolder = workFolder;
 		this.modelPath = modelPath;
 		config = new ArrayList<String>();
 	}
-
-	public GreatSPNRunner(String workFolder, String modelPath) {// , String binPath
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		this.path = s;
-		this.workFolder = workFolder;
-		this.modelPath = modelPath;
-		config = new ArrayList<String>();
-		sc.close();
-	}
-
+	
 	public void configure(List<String> ohs) {
 		for (String oh : ohs)
 			config.add(oh);
@@ -50,27 +35,18 @@ public class GreatSPNRunner {
 		config.add(oh);
 	}
 
-	public void run() {
+	public void run() throws TimeoutException {
 		CommandLine cl = new CommandLine();
 		cl.setWorkingDir(new File(workFolder));
 		cl.addArg(path);
 		cl.addArg(modelPath);
 		for (String oh : config)
 			cl.addArg(oh);
-		cl.addArg("-varord");
+		cl.addArg("-varord-only");
 		System.out.println("Running greatSPN : " + cl);
 		try {
 			String stdOutput = workFolder + "/outPut.txt";
-			Map<String, String> envLib = new HashMap<>();
-			String LIB = "LD_LIBRARY_PATH";
-			String env = binpath;
-			String old = System.getenv(LIB);
-			if (old != null) {
-				env = binpath + ":" + old;
-				env = env + ":" + binpath + "../../../lib";
-			}
-			envLib.put(LIB, env);
-			int status = Runner.runTool(100, cl, new File(stdOutput), true, envLib);
+			int status = Runner.runTool(100, cl, new File(stdOutput), true);
 			System.out.println("Run of greatSPN captured in " + stdOutput);
 			BufferedReader reader = new BufferedReader(new FileReader(stdOutput));
 			String line;
@@ -82,9 +58,8 @@ public class GreatSPNRunner {
 					this.order = orders;
 				}
 			}
+			reader.close();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (TimeoutException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
