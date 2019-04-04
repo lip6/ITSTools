aea4f33c979ef43c5de6cbf33eda957afd59382b
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2019-04-04 17:27:22 +0200

    drop unused import

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/BlissRunner.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/BlissRunner.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.concurrent.TimeoutException;
 
 import fr.lip6.move.gal.process.CommandLine;
