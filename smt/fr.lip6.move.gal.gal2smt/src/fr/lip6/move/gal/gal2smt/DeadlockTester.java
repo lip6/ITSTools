2b33ad693753455c0ada24e1e222a7e87edbf8bb
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2019-04-11 16:34:49 +0200

    Lots of changes, add state equation, ...
    * discard redundant constraints, a lot of transitions can have similar
    effects flow wise
    * some separation into blocks to avoid hogging memory
    * Based on code adapted from KInduction class, add a state equation
    constraint which is stronger than just the invariants
    * Allow to query and extract the parikh vector

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/DeadlockTester.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/gal2smt/DeadlockTester.java
@@ -74,8 +74,21 @@ public class DeadlockTester {
 			}
 		}
 		MatrixCol sumMatrix = new MatrixCol(sr.getPnames().size(), 0);
-		for (int i=0 ; i < sr.getFlowPT().getColumnCount() ; i++) {
-			sumMatrix.appendColumn(SparseIntArray.sumProd(-1, sr.getFlowPT().getColumn(i), 1, sr.getFlowTP().getColumn(i)));
+		List<String> tnames = new ArrayList<>();
+		{
+			int discarded=0;
+			Set<SparseIntArray> seen = new HashSet<>();
+			for (int i=0 ; i < sr.getFlowPT().getColumnCount() ; i++) {
+				SparseIntArray combined = SparseIntArray.sumProd(-1, sr.getFlowPT().getColumn(i), 1, sr.getFlowTP().getColumn(i));
+				if (seen.add(combined)) {
+					sumMatrix.appendColumn(combined);
+					tnames.add(sr.getTnames().get(i));
+				} else
+					discarded++;
+			}
+			if (discarded >0) {
+				Logger.getLogger("fr.lip6.move.gal").info("Flow matrix only has "+sumMatrix.getColumnCount() +" transitions (discarded "+ discarded +" similar events)");
+			}
 		}
 
 		long timestamp2 = System.currentTimeMillis();
@@ -97,24 +110,31 @@ public class DeadlockTester {
 				additional ++;
 			}
 		}
-		Set<SparseIntArray> preconds = new HashSet<>();
-		for (int i = 0; i < sr.getFlowPT().getColumnCount() ; i++)
-			preconds.add(sr.getFlowPT().getColumn(i));
-		for (SparseIntArray arr : preconds) {
-			List<IExpr> conds = new ArrayList<>();
-			for (int  i=0; i < arr.size() ; i++) {
-				conds.add( efactory.fcn(efactory.symbol("<"), efactory.symbol("s"+arr.keyAt(i)), efactory.numeral(arr.valueAt(i))));
-			}
-			IExpr res;
-			if (conds.size() == 1) {
-				res = conds.get(0);
-			} else {
-				res = efactory.fcn(efactory.symbol("or"), conds);
-			}
-			script.add(new C_assert(res));
-		}		
+		// deliberate block to help gc.
+		{
+			Set<SparseIntArray> preconds = new HashSet<>();
+			for (int i = 0; i < sr.getFlowPT().getColumnCount() ; i++)
+				preconds.add(sr.getFlowPT().getColumn(i));
+			for (SparseIntArray arr : preconds) {
+				List<IExpr> conds = new ArrayList<>();
+				for (int  i=0; i < arr.size() ; i++) {
+					conds.add( efactory.fcn(efactory.symbol("<"), efactory.symbol("s"+arr.keyAt(i)), efactory.numeral(arr.valueAt(i))));
+				}
+				IExpr res;
+				if (conds.size() == 1) {
+					res = conds.get(0);
+				} else {
+					res = efactory.fcn(efactory.symbol("or"), conds);
+				}
+				script.add(new C_assert(res));
+			}			
+		}
 		timestamp2 = System.currentTimeMillis();
 		IResponse res = script.execute(solver);		
+		
+		// free up memory
+		script = null;
+		
 		IResponse res2 = solver.check_sat();
 		IPrinter printer = smt.smtConfig.defaultPrinter;
 		String textReply = printer.toString(res2);
@@ -128,10 +148,71 @@ public class DeadlockTester {
 			res2 = solver.check_sat();
 			textReply = printer.toString(res2);
 			Logger.getLogger("fr.lip6.move.gal").info("Absence of deadlock check using  "+normal+" positive and " + additional +" generalized place invariants in "+ (System.currentTimeMillis()-timestamp2) +" ms returned " + textReply);
+			// help gc
+			scriptMore = null;
 		}
+		
+		if (textReply.equals("sat")) {
+			timestamp2 = System.currentTimeMillis();
+			Logger.getLogger("fr.lip6.move.gal").info("Adding state equation constraints to refine reachable states.");
+			script = new Script();
+			
+			// declare a set of variables for holding Parikh count of the transition
+			List<ISymbol> transitions = new ArrayList<>(); 
+			for (int i =0 ; i < sumMatrix.getColumnCount() ; i++) {				
+				ISymbol ti = efactory.symbol("t"+i);
+				transitions.add(ti);
+				script.add(new org.smtlib.command.C_declare_fun(
+						ti,
+						Collections.emptyList(),
+						ints								
+						));
+				script.add(new C_assert(efactory.fcn(efactory.symbol(">="), ti, efactory.numeral(0))));
+			}
+			// we work with one constraint for each place => use transposed
+			MatrixCol mat = sumMatrix.transpose();
+			for (int varindex = 0 ; varindex < mat.getColumnCount() ; varindex++) {
+				
+				SparseIntArray line = mat.getColumn(varindex);
+				// assert : x = m0.x + X0*C(t0,x) + ...+ XN*C(Tn,x)
+				List<IExpr> exprs = new ArrayList<IExpr>();
+
+				// m0.x
+				exprs.add(efactory.numeral(sr.getMarks().get(varindex)));
+
+				//  Xi*C(ti,x)
+				for (int i = 0 ; i < line.size() ; i++) {
+					int val = line.valueAt(i);
+					int trindex = line.keyAt(i);
+					IExpr nbtok ;
+					if (val > 0) 
+						nbtok = efactory.numeral(val);
+					else if (val < 0)
+						nbtok = efactory.fcn(efactory.symbol("-"), efactory.numeral(-val));
+					else 
+						continue;
+					exprs.add(efactory.fcn(efactory.symbol("*"), 
+							transitions.get(trindex),
+							nbtok));
+				}
+				
+				script.add(
+						new C_assert(
+						efactory.fcn(efactory.symbol("="), 
+						efactory.symbol("s"+varindex),
+						// = m0.x + X0*C(t0,x) + ...+ XN*C(Tn,x)
+						efactory.fcn(efactory.symbol("+"), exprs))));
+			}
+			res = script.execute(solver);
+			res2 = solver.check_sat();
+			textReply = printer.toString(res2);
+			Logger.getLogger("fr.lip6.move.gal").info("Absence of deadlock check using state equation in "+ (System.currentTimeMillis()-timestamp2) +" ms returned " + textReply);
+		}
+		
 		
 		if (textReply.equals("sat")) {			
 			queryState(efactory, sr, solver);
+			queryParikh(efactory, tnames, solver);
 		}
 		
 		solver.exit();
@@ -188,7 +269,7 @@ public class DeadlockTester {
 					ISeq pair = (ISeq) sexpr;
 					if (pair.sexprs().size() == 2) {
 						if (pair.sexprs().get(0) instanceof ISymbol && pair.sexprs().get(1) instanceof INumeral) {
-							int varindex = Integer.parseInt( ((ISymbol) pair.sexprs().get(0)).value().replaceFirst("s","") );
+							int varindex = Integer.parseInt( ((ISymbol) pair.sexprs().get(0)).value().substring(1) );
 							int varvalue = ((INumeral) pair.sexprs().get(1)).intValue();
 							res.append(varindex, varvalue);
 						}
@@ -200,16 +281,36 @@ public class DeadlockTester {
 	}
 	
 	private static void queryState(IFactory efactory, StructuralReduction sr, ISolver solver) {
-		
 		List<IExpr> places = new ArrayList<>();
 		for (int i =0 ; i < sr.getPnames().size() ; i++) {
 			ISymbol si = efactory.symbol("s"+i);
 			places.add(si);
 		}
 		ICommand getVals = new C_get_value(places);
-		IResponse state = getVals.execute(solver);
+		IResponse state = getVals.execute(solver);		
 		SparseIntArray s = extractState(state);
 		Logger.getLogger("fr.lip6.move.gal").info("Deadlock seems possible (SAT) in state :" + s);
+	}
+	
+	private static void queryParikh(IFactory efactory, List<String> pnames, ISolver solver) {
+		List<IExpr> places = new ArrayList<>();
+		for (int i =0 ; i < pnames.size() ; i++) {
+			ISymbol si = efactory.symbol("t"+i);
+			places.add(si);
+		}
+		ICommand getVals = new C_get_value(places);
+		IResponse state = getVals.execute(solver);		
+		SparseIntArray s = extractState(state);
+		StringBuilder sb = new StringBuilder();
+		for (int  i=0 ; i < s.size() ; i++) {
+			int ti = s.keyAt(i);
+			int vi = s.valueAt(i);
+			if (i >0) {
+				sb.append(", ");
+			}
+			sb.append(pnames.get(ti)).append("=").append(vi);
+		}
+		Logger.getLogger("fr.lip6.move.gal").info("Deadlock seems possible with Parikh count :" + sb.toString());
 	}
 	
 }
