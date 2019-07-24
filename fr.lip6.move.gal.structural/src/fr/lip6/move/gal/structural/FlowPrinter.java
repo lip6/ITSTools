2d194b48ab96876d019d729488cddfc4211454ca
ythierry <ythierry@oxygen.rsr.lip6.fr>
2019-07-24 13:57:51 +0200

    extend API to enable nicer highlights + titles

--- fr.lip6.move.gal.structural/src/fr/lip6/move/gal/structural/FlowPrinter.java
+++ fr.lip6.move.gal.structural/src/fr/lip6/move/gal/structural/FlowPrinter.java
@@ -5,7 +5,9 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.BitSet;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import android.util.SparseIntArray;
 import fr.lip6.move.gal.util.MatrixCol;
@@ -14,21 +16,40 @@ public class FlowPrinter {
 
 	static int nbWritten = 1000;
 	
-	public static String drawNet (StructuralReduction sr)  {
-		return drawNet(sr.getFlowPT(),sr.getFlowTP(),sr.getMarks(),sr.getPnames(),sr.getTnames(), sr.getUntouchable());
+	public static String drawNet (StructuralReduction sr, String title)  {
+		return drawNet(sr, title, Collections.emptySet(), Collections.emptySet());
 	}
-
-	public static String drawNet (MatrixCol flowPT, MatrixCol flowTP, List<Integer> marks, List<String> pnames, List<String> tnames, BitSet untouchable) {
+	
+	public static String drawNet (StructuralReduction sr, String title, Set<Integer> hlPlaces, Set<Integer> hlTrans)  {
+		return drawNet(sr.getFlowPT(),sr.getFlowTP(),sr.getMarks(),sr.getPnames(),sr.getTnames(), sr.getUntouchable(), "places: "+sr.getPnames().size() + " trans:"+ sr.getTnames().size()+ " " + title, hlPlaces, hlTrans);
+	}
+	
+	public static String drawNet (MatrixCol flowPT, MatrixCol flowTP, List<Integer> marks, List<String> pnames, List<String> tnames, BitSet untouchable, String title, Set<Integer> hlPlaces, Set<Integer> hlTrans) {
 		try {
 			Path out = Files.createTempFile("petri"+nbWritten++ +"_", ".dot");
 			PrintWriter pw = new PrintWriter(out.toFile());
 
 			pw.println("digraph {");
+			pw.println("  labelloc=\"t\";");
+			pw.println("label=\""+ title +"\";");
+			
 			for (int i=0 ; i < pnames.size() ; i++) {
-				pw.println("  p"+i+ " [shape=\"circle\",label=\""+pnames.get(i) +(marks.get(i)!=0?"("+marks.get(i)+")":"") + "\"" + (untouchable.get(i) ? ",color=\"red\"" :"")  +"];");
+				String col = "";
+				if (untouchable.get(i) && hlPlaces.contains(i)) {
+					col = ",color=\"violet\"";
+				} else if (untouchable.get(i)) {
+					col = ",color=\"red\"";
+				} else if (hlPlaces.contains(i)) {
+					col = ",color=\"blue\"";
+				}
+				pw.println("  p"+i+ " [shape=\"oval\",label=\""+pnames.get(i) +(marks.get(i)!=0?"("+marks.get(i)+")":"") + "\"" + col +"];");
 			}
 			for (int i=0 ; i < tnames.size() ; i++) {
-				pw.println("  t"+i+ " [shape=\"rectangle\",label=\""+tnames.get(i)+"\"];");
+				String col = "";
+				if (hlTrans.contains(i)) {
+					col = ",color=\"blue\"";
+				}
+				pw.println("  t"+i+ " [shape=\"rectangle\",label=\""+tnames.get(i)+"\"" + col +"];");
 			}
 			for (int ti = 0 ; ti < flowPT.getColumnCount() ; ti++) {
 				SparseIntArray col = flowPT.getColumn(ti);
