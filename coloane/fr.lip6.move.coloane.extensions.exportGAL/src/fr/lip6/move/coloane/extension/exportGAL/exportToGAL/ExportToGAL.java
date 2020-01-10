862ea03fc8902598b9c935dc60f93b6bcf75b3bc
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-01-10 21:11:49 +0100

    static invocation

--- coloane/fr.lip6.move.coloane.extensions.exportGAL/src/fr/lip6/move/coloane/extension/exportGAL/exportToGAL/ExportToGAL.java
+++ coloane/fr.lip6.move.coloane.extensions.exportGAL/src/fr/lip6/move/coloane/extension/exportGAL/exportToGAL/ExportToGAL.java
@@ -450,7 +450,7 @@ public class ExportToGAL implements IExportTo {
 		
 		try {
 			
-			new Simplifier().simplify(gal);
+			Simplifier.simplify(gal);
 			Specification spec = gf.createSpecification();
 			spec.getTypes().add(gal);
 			SerializationUtil.systemToFile(spec, filePath);
