9c276f97305d15c8ac75c2a5eb0b519cddb57614
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-02-22 21:51:22 +0100

    tag properties with their type at parse time

--- fr.lip6.move.gal.structural/src/fr/lip6/move/gal/mcc/properties/PropertyParser.java
+++ fr.lip6.move.gal.structural/src/fr/lip6/move/gal/mcc/properties/PropertyParser.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.IOException;
 
 import fr.lip6.move.gal.structural.PetriNet;
+import fr.lip6.move.gal.structural.Property;
+import fr.lip6.move.gal.structural.PropertyType;
 
 public class PropertyParser {
 	
-	public static int  fileToProperties(String path, PetriNet ptnet) throws IOException {
+	public static int  fileToProperties(String path, PetriNet ptnet, PropertyType propertyType) throws IOException {
 		PropReader.readXMLPropertiesIntoProps(new File(path), ptnet);
-				
+		for (Property prop : ptnet.getProperties()) {
+			prop.setType(propertyType);
+		}
 		return ptnet.getProperties().size();
 	}
 
