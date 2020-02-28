78957c70f9cf698c782b40935b081770b490b6fe
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-02-28 17:38:19 +0100

    improve traces + extract code to Simplifier class

--- fr.lip6.move.gal.structural/src/fr/lip6/move/gal/structural/SparsePetriNet.java
+++ fr.lip6.move.gal.structural/src/fr/lip6/move/gal/structural/SparsePetriNet.java
@@ -16,6 +16,7 @@ import fr.lip6.move.gal.structural.expr.BinOp;
 import fr.lip6.move.gal.structural.expr.Expression;
 import fr.lip6.move.gal.structural.expr.NaryOp;
 import fr.lip6.move.gal.structural.expr.Op;
+import fr.lip6.move.gal.structural.expr.Simplifier;
 import fr.lip6.move.gal.structural.expr.VarRef;
 import fr.lip6.move.gal.util.MatrixCol;
 
@@ -299,7 +300,7 @@ public class SparsePetriNet extends PetriNet {
 				trem.add(tnames.remove(tid));				
 			}
 		}
-		if (!prem.isEmpty() || !trem.isEmpty())
+		if (totalp>0 || !todelTrans.isEmpty())
 			System.out.println("Reduce places removed "+totalp + " places and " + todelTrans.size() + " transitions. " + (DEBUG>=1 ? ("Places : " + prem + " Transitions:" + trem):""));
 
 		return totaltok;
@@ -408,271 +409,10 @@ public class SparsePetriNet extends PetriNet {
 
 	public void simplifyLogic() {
 		for (Property prop : getProperties()) {
-			prop.setBody(pushNegation(prop.getBody(),false));
-			prop.setBody(simplifyBoolean(prop.getBody(),new HashSet<>(),new HashSet<>(),new HashSet<>()));
+			prop.setBody(Simplifier.pushNegation(prop.getBody()));
+			prop.setBody(Simplifier.simplifyBoolean(prop.getBody()));
 		}
 		rewriteConstantSums();
-	}
-
-	private Expression simplifyBoolean(Expression expr, Set<Expression> seenPos, Set<Expression> seenNeg, Set<Expression> dominant) {
-		if (expr == null) {
-			return expr;
-		}
-		switch (expr.getOp()) {
-		case CARD: case ENABLED: case BOUND:
-		case GEQ: case GT : case EQ : case NEQ : case LT : case LEQ :
-		{
-			if (seenPos.contains(expr)) {
-				return Expression.constant(true);
-			} else if (seenNeg.contains(expr)) {
-				return Expression.constant(false);
-			} else {
-				if (expr instanceof BinOp) {
-					BinOp cmp = (BinOp) expr;
-					if (cmp.left.equals(cmp.right)) {
-						switch (expr.getOp()) {
-						case GEQ: case EQ : case LEQ : 
-						{
-							return Expression.constant(true);
-						}
-						case GT : case NEQ : case LT :
-						{
-							return Expression.constant(false);
-						}
-						default :
-							break;
-						}
-					}
-				}
-				return expr;
-				
-			}
-		}
-		case NOT:
-		{
-			Expression l = ((BinOp) expr).left;
-			if (seenPos.contains(l)) {
-				return Expression.constant(false);
-			} else if (seenNeg.contains(l)) {
-				return Expression.constant(true);
-			} else {
-				return expr;
-			}
-		}	
-		case BOOLCONST:
-			return expr;
-		default :
-			break;
-		}
-		if (expr instanceof BinOp) {
-			BinOp bin = (BinOp) expr;
-			Expression l = simplifyBoolean(bin.left,new HashSet<>(),new HashSet<>(),new HashSet<>()); 
-			Expression r = simplifyBoolean(bin.right,new HashSet<>(),new HashSet<>(),new HashSet<>());
-			if (l == bin.left && r == bin.right) {
-				return expr;
-			}
-			return Expression.op(bin.op, l, r);			
-		} else if (expr instanceof NaryOp) {
-			NaryOp nop = (NaryOp) expr;
-			List<Expression> resc = new ArrayList<>();
-						
-			boolean changed = false;
-			if (nop.getOp() == Op.AND) {
-				List<Expression> tovisit = new ArrayList<>();				
-				Set<Expression> nseenPos = new HashSet<>(seenPos);
-				Set<Expression> nseenNeg = new HashSet<>(seenNeg);
-				for (Expression child : nop.getChildren()) {
-					if (child.getOp() == Op.NOT) {
-						Expression e = ((BinOp)child).left;
-						if (nseenNeg.contains(e)) {
-							continue;
-						} else if (nseenPos.contains(e)) {
-							return Expression.constant(false);
-						} else {
-							nseenNeg.add(e);
-							resc.add(child);
-						}						
-					} else if (child.getOp() == Op.OR) {
-						tovisit.add(child);
-					} else if (child.getOp() == Op.BOOLCONST) {
-						if (child.getValue() == 1) {
-							continue;
-						} else {
-							return Expression.constant(false);
-						}						
-					} else {
-						if (nseenPos.contains(child)) {
-							continue;
-						} else if (nseenNeg.contains(child)) {
-							return Expression.constant(false);
-						} else {
-							nseenPos.add(child);
-							resc.add(child);
-						}						
-					}
-				}
-				for (Expression e : tovisit) {					
-					Expression img = simplifyBoolean(e, nseenPos, nseenNeg, dominant);
-					if (img.getOp() == Op.BOOLCONST) {
-						if (img.getValue() == 1) {
-							continue;
-						} else {
-							return Expression.constant(false);
-						}
-					}
-					resc.add(img);
-				}
-				return Expression.nop(nop.getOp(), resc);
-			} else if (nop.getOp() == Op.OR) {
-				List<Expression> tovisit = new ArrayList<>();				
-				Set<Expression> ndominant = new HashSet<>(dominant);
-				Set<Expression> nseenPos = new HashSet<>();
-				Set<Expression> nseenNeg = new HashSet<>();
-				for (Expression child : nop.getChildren()) {
-					if (child.getOp() == Op.NOT) {
-						Expression e = ((BinOp)child).left;
-						if (seenNeg.contains(e)) {
-							return Expression.constant(true);
-						} else if (seenPos.contains(e)) {
-							continue;
-						} else if (nseenPos.contains(e)){
-							return Expression.constant(true);
-						} else {
-							if (nseenNeg.add(e)) {
-								ndominant.add(child);							
-								resc.add(child);
-							}
-						}						
-					} else if (child.getOp() == Op.AND) {
-						tovisit.add(child);
-					} else if (child.getOp() == Op.BOOLCONST) {
-						if (child.getValue() == 0) {
-							continue;
-						} else {
-							return Expression.constant(true);
-						}						
-					} else {
-						if (seenPos.contains(child)) {
-							return Expression.constant(true);
-						} else if (seenNeg.contains(child)) {
-							continue;
-						} else if (nseenNeg.contains(child)){
-							return Expression.constant(true);
-						} else {
-							if (nseenPos.add(child)) {						
-								ndominant.add(child);
-								resc.add(child);
-							}
-						}						
-					}
-				}
-				for (Expression e : tovisit) {					
-					Expression img = simplifyBoolean(e, seenPos, seenNeg, ndominant);
-					if (img.getOp() == Op.BOOLCONST) {
-						if (img.getValue() == 0) {
-							continue;
-						} else {
-							return Expression.constant(true);
-						}
-					} else if (img.getOp() == Op.NOT) {
-						if (! nseenNeg.add(((BinOp)img).left)) {
-							continue;
-						}
-					} else {
-						if (! nseenPos.add(img)) {
-							continue;
-						}
-					}
-					resc.add(img);
-				}
-				return Expression.nop(nop.getOp(), resc);
-			}
-			return expr;
-		} 
-		return expr;
-		
-	}
-
-	private Expression pushNegation(Expression expr, boolean isNeg) {
-		if (expr == null) {
-			return expr;
-		} else if (expr.getOp() == Op.BOOLCONST) {
-			if (!isNeg) {
-				return expr;
-			} else {
-				return Expression.constant(expr.getValue()==0);
-			}		
-		} else if (expr instanceof BinOp) {
-			BinOp bin = (BinOp) expr;
-			switch (bin.getOp()) {
-			case GEQ: case GT: case LEQ: case LT: case EQ: case NEQ:
-				if (isNeg) {
-					return Expression.not(bin);
-				} else {
-					return bin;
-				}
-			default : 
-				break;
-			}
-			if (bin.getOp() == Op.NOT) {
-				return pushNegation(bin.left,!isNeg);
-			}
-			Expression l = pushNegation(bin.left,false);
-			Expression r = pushNegation(bin.right,false);
-			if (l == bin.left && r == bin.right) {
-				if (!isNeg) {
-					return expr;
-				} else {
-					return Expression.not(expr);
-				}
-			}
-			if (!isNeg) {
-				return Expression.op(bin.op, l, r);
-			} else {
-				return Expression.not(Expression.op(bin.op, l, r));
-			}
-		} else if (expr instanceof NaryOp) {
-			NaryOp nop = (NaryOp) expr;
-			List<Expression> resc = new ArrayList<>();
-			
-			switch (nop.getOp()) {
-			case CARD: case ENABLED: case BOUND:
-			{
-				if (!isNeg)
-					return expr;
-				else 
-					return Expression.not(expr);
-			}
-			default:
-				break;
-			}
-			
-			boolean changed = false;
-			for (Expression child : nop.getChildren()) {
-				Expression nc = pushNegation(child,isNeg);
-				resc.add(nc);
-				if (nc != child) {
-					changed = true;
-				}
-			}
-			if (!changed) {
-				if (!isNeg)
-					return expr;
-				else 
-					return Expression.not(expr);
-			}
-			if (isNeg) {
-				if (nop.getOp() == Op.OR) {
-					return Expression.nop(Op.AND, resc);
-				} else if (nop.getOp() == Op.AND) {
-					return Expression.nop(Op.OR, resc);
-				} else {
-					return Expression.not(Expression.nop(nop.getOp(), resc));
-				}
-			}
-			return Expression.nop(nop.getOp(), resc);
-		} 
-		return expr;
 	}
 
 	public BitSet computeSupport() {
@@ -737,8 +477,7 @@ public class SparsePetriNet extends PetriNet {
 					todrop.add(i);
 				} 
 			}
-			if (! todrop.isEmpty()) {
-				System.out.println("Reduce no effect transitions removed "+ todrop.size() +" transitions.");
+			if (! todrop.isEmpty()) {				
 				reduced += todrop.size();
 				for (int tid : todrop) {
 					flowPT.deleteColumn(tid);
@@ -748,7 +487,9 @@ public class SparsePetriNet extends PetriNet {
 			}
 		}
 		reduced += ensureUnique(flowPT, flowTP, tnames); 
-	
+		if (reduced > 0) {
+			System.out.println("Reduce redundant transitions removed "+ reduced +" transitions.");
+		}
 	}
 	
 	public Specification rebuildSpecification () {
