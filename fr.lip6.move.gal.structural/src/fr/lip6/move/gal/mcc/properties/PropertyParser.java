0f4975c6502f28c30c88dc82bf8a405a12b1f5cc
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-05-06 17:20:19 +0200

    PropertyParser now in charge of building formulas if necessary

--- fr.lip6.move.gal.structural/src/fr/lip6/move/gal/mcc/properties/PropertyParser.java
+++ fr.lip6.move.gal.structural/src/fr/lip6/move/gal/mcc/properties/PropertyParser.java
@@ -6,13 +6,24 @@ import java.io.IOException;
 import fr.lip6.move.gal.structural.PetriNet;
 import fr.lip6.move.gal.structural.Property;
 import fr.lip6.move.gal.structural.PropertyType;
+import fr.lip6.move.gal.structural.expr.Expression;
+import fr.lip6.move.gal.structural.expr.Op;
 
 public class PropertyParser {
 	
 	public static int  fileToProperties(String path, PetriNet ptnet, PropertyType propertyType) throws IOException {
-		PropReader.readXMLPropertiesIntoProps(new File(path), ptnet, propertyType == PropertyType.LTL);
-		for (Property prop : ptnet.getProperties()) {
-			prop.setType(propertyType);
+		File ff = new File(path);
+		if (ff.exists()) {
+			PropReader.readXMLPropertiesIntoProps(ff, ptnet, propertyType == PropertyType.LTL);
+			for (Property prop : ptnet.getProperties()) {
+				prop.setType(propertyType);
+			}
+		} else {
+			if (propertyType == PropertyType.DEADLOCK) {
+				fr.lip6.move.gal.structural.Property deadlockProp = 
+						new fr.lip6.move.gal.structural.Property(Expression.op(Op.EF,Expression.op(Op.DEAD, null, null),null), propertyType ,"ReachabilityDeadlock");
+				ptnet.getProperties().add(deadlockProp);
+			}
 		}
 		return ptnet.getProperties().size();
 	}
