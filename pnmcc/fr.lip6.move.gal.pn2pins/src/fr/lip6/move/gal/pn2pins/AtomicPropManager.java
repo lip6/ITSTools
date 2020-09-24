19ceb73960c2f32e0267d1c70a0d9a0003d64168
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-09-24 15:19:34 +0200

    source cleanup : whitespace only

--- pnmcc/fr.lip6.move.gal.pn2pins/src/fr/lip6/move/gal/pn2pins/AtomicPropManager.java
+++ pnmcc/fr.lip6.move.gal.pn2pins/src/fr/lip6/move/gal/pn2pins/AtomicPropManager.java
@@ -15,20 +15,20 @@ import fr.lip6.move.gal.structural.expr.Op;
 
 public class AtomicPropManager {
 	private List<AtomicProp> atoms = new ArrayList<>();
-	private Map<Expression, AtomicProp> atomMap = new HashMap<Expression, AtomicProp>();	
-	
-	public void loadAtomicProps (List<Property> props) {
+	private Map<Expression, AtomicProp> atomMap = new HashMap<Expression, AtomicProp>();
+
+	public void loadAtomicProps(List<Property> props) {
 		atoms.clear();
 		atomMap.clear();
-		Map<String,AtomicProp> uniqueMap = new HashMap<>();
+		Map<String, AtomicProp> uniqueMap = new HashMap<>();
 		// look for atomic propositions
-		if (! props.isEmpty()) {
+		if (!props.isEmpty()) {
 			for (Property prop : props) {
 				if (prop.getType() == PropertyType.INVARIANT) {
-					Expression be = ((BinOp)prop.getBody()).left;
+					Expression be = ((BinOp) prop.getBody()).left;
 					if (prop.getBody().getOp() == Op.EF) {
 						be = Expression.not(be);
-					}					
+					}
 					atoms.add(new AtomicProp(prop.getName().replaceAll("-", ""), be));
 				} else if (prop.getType() == PropertyType.LTL || prop.getType() == PropertyType.CTL) {
 					collectAP(prop.getBody(), uniqueMap);
@@ -38,10 +38,10 @@ public class AtomicPropManager {
 		}
 
 	}
-	
-	public Void collectAP(Expression obj, Map<String,AtomicProp> uniqueMap) {
+
+	private Void collectAP(Expression obj, Map<String, AtomicProp> uniqueMap) {
 		if (isPureBool(obj)) {
-			// helps to recognize that  !AP is the negation of AP
+			// helps to recognize that !AP is the negation of AP
 			// Can reduce number of AP as well as help simplifications
 			if (obj.getOp() == Op.NOT) {
 				obj = obj.childAt(0);
@@ -49,7 +49,7 @@ public class AtomicPropManager {
 			String stringProp = toString(obj);
 			AtomicProp atom = uniqueMap.get(stringProp);
 			if (atom == null) {
-				atom = new AtomicProp("p"+atoms.size(), obj);
+				atom = new AtomicProp("p" + atoms.size(), obj);
 				atoms.add(atom);
 				uniqueMap.put(stringProp, atom);
 			}
@@ -59,49 +59,50 @@ public class AtomicPropManager {
 		}
 		return null;
 	}
-	
+
 	public static String toString(Expression e) {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		{
-			CExpressionPrinter printer = new CExpressionPrinter(new PrintWriter(baos),"src");
+			CExpressionPrinter printer = new CExpressionPrinter(new PrintWriter(baos), "src");
 			e.accept(printer);
 			printer.close();
-		}		
+		}
 		return baos.toString();
 	}
-	
 
-
-	
 	private static boolean isPureBool(Expression obj) {
 		if (obj == null) {
 			return true;
 		} else {
 			switch (obj.getOp()) {
-			case AND :
-			case OR :
-			case NOT :
-			{
-				for (int i = 0, ie = obj.nbChildren() ; i < ie ; i++) {
+			case AND:
+			case OR:
+			case NOT: {
+				for (int i = 0, ie = obj.nbChildren(); i < ie; i++) {
 					Expression child = obj.childAt(i);
-					if (! isPureBool(child)) {
+					if (!isPureBool(child)) {
 						return false;
 					}
 				}
 				return true;
 			}
-			case GT : case GEQ : case EQ : case NEQ : case LT : case LEQ :
+			case GT:
+			case GEQ:
+			case EQ:
+			case NEQ:
+			case LT:
+			case LEQ:
 				return true;
-			default : 
+			default:
 				return false;
-			}			
+			}
 		}
 	}
 
-	
 	public List<AtomicProp> getAtoms() {
 		return atoms;
 	}
+
 	public Map<Expression, AtomicProp> getAtomMap() {
 		return atomMap;
 	}
