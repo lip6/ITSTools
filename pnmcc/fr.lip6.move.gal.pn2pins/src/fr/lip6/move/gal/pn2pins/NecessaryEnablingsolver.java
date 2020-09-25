4b6f261837282377a514da676d9f93a9626e400f
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-09-25 16:00:20 +0200

    DNA for Petri nets is pretty trivial : commutativity is free

--- pnmcc/fr.lip6.move.gal.pn2pins/src/fr/lip6/move/gal/pn2pins/NecessaryEnablingsolver.java
+++ pnmcc/fr.lip6.move.gal.pn2pins/src/fr/lip6/move/gal/pn2pins/NecessaryEnablingsolver.java
@@ -338,104 +338,27 @@ public class NecessaryEnablingsolver {
 //		script.add(new C_assert(bodyExpr));
 //	}
 
-//	public MatrixCol computeDoNotAccord (MatrixCol coEnabled, MatrixCol mayEnable, DependencyMatrix dm) {
-//		MatrixCol dnaMatrix = new MatrixCol(nbTransition,nbTransition);
-//		// push a context
-//		solver.push(1);
-//
-//
-//		// We need 4 states : s1, s2, s3, s4, s5
-//		IExpr s1 = accessStateAt(0);
-//		IExpr s2 = accessStateAt(1);
-//		IExpr s3 = accessStateAt(2);
-//		IExpr s4 = accessStateAt(3);
-//		IExpr s5 = accessStateAt(4);
-//
-//		// s1 and s2 already constrained by invariants
-//		addKnownInvariants(s3);
-//		addKnownInvariants(s4);
-//		addKnownInvariants(s5);
-//
-//
-//		Logger.getLogger("fr.lip6.move.gal").info("Computing Do-Not-Accords matrix : " + nbTransition + " transitions.");
-//		clearStats();
-//		for (int t1 = 0 ; t1 < nbTransition ; t1++) {
-//			for (int t2 = t1+1 ; t2 < nbTransition ; t2++) {
-//				if (mayEnable.getColumn(t1).get(t2)==1 || mayEnable.getColumn(t2).get(t1)==1) {
-//					// mutual disabling criterion not met
-//					// t1 and t2 do not accord
-//					dnaMatrix.getColumn(t1).put(t2, 1);
-//					dnaMatrix.getColumn(t2).put(t1, 1);
-//					continue;
-//				}
-//				if (coEnabled.getColumn(t1).get(t2) == 0) {
-//					// we meet accords requirement : the implication is true if there is no coenabling
-//					// put 0 in dna(t1,t2) = do nothing
-//					continue;
-//				}
-//				if ( ! ( dm.getRead(t1).intersects(dm.getWrite(t2))
-//						|| dm.getWrite(t1).intersects(dm.getWrite(t2))
-//						|| dm.getWrite(t1).intersects(dm.getRead(t2)))) {
-//					// these transitions cannot interfere, at best they share some read only variables
-//					// we meet accords requirement 
-//					// put 0 in dna(t1,t2) = do nothing
-//					continue;
-//				}
-//
-//				solver.push(1);
-//
-//				// Express  assertions 
-//				// s1  --t1--> s2
-//				//     --t2--> s3
-//				solver.assertExpr(efactory.fcn(efactory.symbol(TRANSSRC+t1), s1, s2));
-//				solver.assertExpr(efactory.fcn(efactory.symbol(TRANSSRC+t2), s1, s3));
-//
-//
-//				// these additional conditions enforce a mutual disabling test, already done above
-//				//				efactory.fcn(efactory.symbol("or"), 
-//				//						efactory.fcn(efactory.symbol("not"),  efactory.fcn(efactory.symbol(ENABLEDSRC+t1), s3)),
-//				//						efactory.fcn(efactory.symbol("not"),  efactory.fcn(efactory.symbol(ENABLEDSRC+t2), s2)),
-//
-//				// add commutativity test
-//				// s1  --t1--> s2 --t2--> s5
-//				//     --t2--> s3 --t1--> s4
-//				// can s4 != s5 be SAT ?
-//				// this test relies on deterministic behavior of transitions.
-//				IExpr tocheck = efactory.fcn(efactory.symbol("and"), 
-//						efactory.fcn(efactory.symbol(TRANSSRC+t1), s3, s4),
-//						efactory.fcn(efactory.symbol(TRANSSRC+t2), s2, s5),
-//						efactory.fcn(efactory.symbol("not"), efactory.fcn(efactory.symbol("="), s4, s5))
-//						);						
-//				solver.assertExpr(tocheck);
-//
-//				Result res = checkSat();
-//				//	Logger.getLogger("fr.lip6.move.gal").info("Checking Do Not Accords relation of "+t1 + " and " + t2 + " : " + res);
-//
-//				solver.pop(1);
-//
-//				if (res == Result.SAT) {
-//					// we found s4 != s5 through t1.t2 and t2.t1
-//					dnaMatrix.getColumn(t1).put(t2, 1);
-//					dnaMatrix.getColumn(t2).put(t1, 1);
-//					sat++;
-//				} else if (res == Result.UNSAT){
-//					// we could not find s4 != s5 through t1.t2 and t2.t1
-//					// so they are indeed commutative, accords is true
-//					// let it stay at 0
-////					dnaMatrix.get(t1)[t2] = 0;
-////					dnaMatrix.get(t2)[t1] = 0;
-//					unsat++;
-//				} else {
-//					throw new RuntimeException("SMT solver raised an error in enabler solving :"+res);
-//				}
-//			}
-//		}
-//
-//		solver.pop(1);
-//		printStats(true,"Completed DNA matrix.");
-//
-//		return dnaMatrix;
-//	}
+	public MatrixCol computeDoNotAccord (MatrixCol mayEnable) {
+		int nbTransition = net.getTransitionCount();
+		MatrixCol dnaMatrix = new MatrixCol(nbTransition,nbTransition);
+		Logger.getLogger("fr.lip6.move.gal").info("Computing Do-Not-Accords matrix : " + nbTransition + " transitions.");
+		clearStats();
+		for (int t1 = 0 ; t1 < nbTransition ; t1++) {
+			for (int t2 = t1+1 ; t2 < nbTransition ; t2++) {
+				if (mayEnable.getColumn(t1).get(t2)==1 || mayEnable.getColumn(t2).get(t1)==1) {
+					// mutual disabling criterion not met
+					// t1 and t2 do not accord
+					dnaMatrix.getColumn(t1).put(t2, 1);
+					dnaMatrix.getColumn(t2).put(t1, 1);
+					continue;
+				}
+				// otherwise : we are fine, Petri net transitions commute naturally.
+			}
+		}
+		
+		printStats(true,"Completed DNA matrix.");
+		return dnaMatrix;
+	}
 
 	boolean[] computeLoHiEdges(Expression expr, SparseIntArray state) {
 		boolean[] ret = new boolean[2];
