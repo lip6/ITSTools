0cfab68e3ac79c52da5c17d725680696ec8b0e6f
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-03-08 13:54:38 +0100

    we are a kind of IOException obviously

--- ltl/fr.lip6.ltl.jhoaf/src/jhoafparser/parser/ParseException.java
+++ ltl/fr.lip6.ltl.jhoaf/src/jhoafparser/parser/ParseException.java
@@ -1,7 +1,9 @@
 package jhoafparser.parser;
 
+import java.io.IOException;
+
 @SuppressWarnings("serial")
-public abstract class ParseException extends Exception {
+public abstract class ParseException extends IOException {
 
 	public ParseException(String initialise) {
 		super(initialise);
