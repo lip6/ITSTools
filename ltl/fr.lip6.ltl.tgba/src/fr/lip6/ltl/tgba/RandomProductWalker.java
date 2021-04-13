8d4415b7e9fbc6a78cd31f384419fc0fdb5b647f
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-04-13 18:47:41 +0200

    refactoring for PPOR, introduce one TGBA and one system per state

--- ltl/fr.lip6.ltl.tgba/src/fr/lip6/ltl/tgba/RandomProductWalker.java
+++ ltl/fr.lip6.ltl.tgba/src/fr/lip6/ltl/tgba/RandomProductWalker.java
@@ -1,5 +1,6 @@
 package fr.lip6.ltl.tgba;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -10,47 +11,49 @@ import fr.lip6.move.gal.structural.expr.Expression;
 
 public class RandomProductWalker {
 
-	private WalkUtils wu;
+	private List<WalkUtils> wus = new ArrayList<>();
 
 	public RandomProductWalker(ISparsePetriNet spn) {
-		this.wu = new WalkUtils(spn);
+		this.wus.add(new WalkUtils(spn));
 	}
-
-	public void runProduct (TGBA tgba, int nbSteps, int timeout) throws LTLException {
-		
-		Product product = new Product(wu,tgba);
-		
-		boolean [] acceptAll = new boolean[tgba.getEdges().size()];
-		{
-			Expression tt = Expression.constant(true);
-			for (int eid=0, eide=tgba.getEdges().size() ; eid < eide ; eid++) {
-				for (TGBAEdge e : tgba.getEdges().get(eid)) {
-					if (e.getSrc() == e.getDest() && e.getAcceptance().size() == tgba.getNbAcceptance() && e.getCondition().equals(tt)) {
-						acceptAll [eid] = true;
-						break;
-					}
-				}
-			}
+	
+	int initialTGBA ;
+	List<TGBA> tgbas = new ArrayList<>();
+	public void setTGBA (TGBA tgba) {
+		tgbas.clear();
+		WalkUtils wu = wus.get(0);
+		wus.clear();
+		for (int i=0; i < tgba.getEdges().size() ; i++) {
+			tgbas.add(tgba);
+			wus.add(wu);
 		}
+		initialTGBA = tgba.getInitial();		
+	}
+	
+	public void runProduct (int nbSteps, int timeout) throws LTLException {
+				
+		boolean[] acceptAll = computeFullyAcceptingStates(tgbas.get(initialTGBA));
 		ThreadLocalRandom rand = ThreadLocalRandom.current();
 		long time = System.currentTimeMillis();
 		int reset = 0;
-		ProductState cur = product.getInitial();
-		int [] enabled = wu.computeEnabled(cur.getPNState());
+		ProductState initial = new ProductState(initialTGBA, getWU(initialTGBA).getInitial());
+		ProductState cur = initial;
+		int [] enabled = getWU(initialTGBA).computeEnabled(cur.getPNState());
 		
 		
 		for (int i=0; i < nbSteps ; i++) {
-			List<Integer> tgbaArcs = product.computeSuccTGBAEdges(cur);					
+			List<Integer> tgbaArcs = computeSuccTGBAEdges(cur,tgbas.get(cur.getTGBAState()));					
 									
+			int tgbaState = cur.getTGBAState();
 			if (tgbaArcs.isEmpty()) {
-				if (cur.getPNState().equals(wu.getInitial()) && cur.getTGBAState() == tgba.getInitial() ) {
+				if (cur.getPNState().equals(getWU(initialTGBA).getInitial()) && tgbaState == initialTGBA ) {
 					System.out.println("Initial state of product has no viable successors after "+i+" steps with "+reset+" reset in "+(System.currentTimeMillis()-time)+ " ms.");
 					
 					throw new EmptyProductException();					
 				} else {
 					reset ++;
-					cur = product.getInitial();
-					enabled = wu.computeEnabled(cur.getPNState());
+					cur = initial;
+					enabled = getWU(initialTGBA).computeEnabled(cur.getPNState());
 					continue;
 				}
 			} else if (tgbaArcs.stream().anyMatch(ps -> acceptAll[ps])) {
@@ -59,16 +62,16 @@ public class RandomProductWalker {
 				throw new AcceptedRunFoundException();
 			} else {
 				
-				if (enabled[0]==0 || wu.canStutter(enabled)) {
-					if (product.getTgba().getInfStutter().get(cur.getTGBAState()).eval(cur.getPNState())==1) {
+				if (enabled[0]==0 || getWU(tgbaState).canStutter(enabled)) {
+					if (tgbas.get(tgbaState).getInfStutter().get(tgbaState).eval(cur.getPNState())==1) {
 						System.out.println("Stuttering criterion allowed to conclude after "+i+" steps with "+reset+" reset in "+(System.currentTimeMillis()-time)+ " ms.");
 						throw new AcceptedRunFoundException();
 					}
 					if (enabled[0]==0) {
 						// deadlock in KS
 						reset ++;
-						cur = product.getInitial();
-						enabled = wu.computeEnabled(cur.getPNState());
+						cur = initial;
+						enabled = getWU(initialTGBA).computeEnabled(cur.getPNState());
 						continue;
 					}				
 				}
@@ -77,14 +80,19 @@ public class RandomProductWalker {
 				int r = rand.nextInt(enabled[0])+1;
 				int tfired = enabled[r];			
 
-				SparseIntArray newstate = wu.fire ( tfired, cur.getPNState());				
-				// NB : does not discard empty events
-				wu.updateEnabled(newstate, enabled, tfired,false);
-
 				int rq = rand.nextInt(tgbaArcs.size());
+				int newq = tgbaArcs.get(rq);
 				
-				cur = new ProductState(tgbaArcs.get(rq), newstate);
+				SparseIntArray newstate = getWU(tgbaState).fire(tfired, cur.getPNState());
 
+				if (getWU(newq) == getWU(tgbaState)) {
+					getWU(newq).updateEnabled(newstate, enabled, tfired, false);
+				} else {
+					newstate = image(tgbaState, newq, newstate);
+					enabled = getWU(newq).computeEnabled(newstate);
+				}
+				cur = new ProductState(newq, newstate);
+				
 				if (i % 10 == 0) {
 					long curtime = System.currentTimeMillis();
 					if (curtime - time > 1000*timeout) {
@@ -97,5 +105,43 @@ public class RandomProductWalker {
 		
 		System.out.println("Product exploration explored " + nbSteps +" steps with "+reset+" reset in "+(System.currentTimeMillis()-time)+ " ms.");
 		
+	}
+
+	private SparseIntArray image(int tgbaState, int newq, SparseIntArray newstate) {		
+		return newstate;
+	}
+
+	private WalkUtils getWU(int newq) {
+		return wus.get(newq);
+	}
+
+	public static List<Integer> computeSuccTGBAEdges (ProductState source, TGBA tgba) {
+		List<TGBAEdge> arcs = tgba.getEdges().get(source.getTGBAState());
+		SparseIntArray srcPN = source.getPNState();
+		
+		List<Integer> canFire = new ArrayList<> ();
+		for (TGBAEdge arc:arcs) {
+			if (arc.getCondition().eval(srcPN) == 1) {
+				canFire.add(arc.getDest());
+			}
+		}
+		return canFire;
+	}
+
+	
+	private boolean[] computeFullyAcceptingStates(TGBA tgba) {
+		boolean [] acceptAll = new boolean[tgba.getEdges().size()];
+		{
+			Expression tt = Expression.constant(true);
+			for (int eid=0, eide=tgba.getEdges().size() ; eid < eide ; eid++) {
+				for (TGBAEdge e : tgba.getEdges().get(eid)) {
+					if (e.getSrc() == e.getDest() && e.getAcceptance().size() == tgba.getNbAcceptance() && e.getCondition().equals(tt)) {
+						acceptAll [eid] = true;
+						break;
+					}
+				}
+			}
+		}
+		return acceptAll;
 	}
 }
