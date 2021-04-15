8bc4b6aa55ae3d27e89e37db2a2b99307c3ce593
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-04-15 04:17:49 +0200

    api to access stored results

--- fr.lip6.move.gal.structural/src/fr/lip6/move/gal/mcc/properties/DoneProperties.java
+++ fr.lip6.move.gal.structural/src/fr/lip6/move/gal/mcc/properties/DoneProperties.java
@@ -9,14 +9,15 @@ public interface DoneProperties {
 
 	// A boolean result
 	Boolean put(String prop, Boolean value, String techniques);
+
 	// A numeric result
 	Boolean put(String prop, Integer value, String techniques);
 
-	
 	Set<Entry<String, Boolean>> entrySet();
 
 	boolean containsKey(Object arg0);
 
 	Set<String> keySet();
-	
+
+	public Boolean getValue(String prop);
 }
