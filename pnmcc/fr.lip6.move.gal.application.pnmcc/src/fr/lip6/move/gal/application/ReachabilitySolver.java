09b09e9cacc0d790d4e42e048051e12f03e171c5
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-04-18 11:27:56 +0200

    do not reduce transitions when going for liveness

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/ReachabilitySolver.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/ReachabilitySolver.java
@@ -390,7 +390,7 @@ public class ReachabilitySolver {
 			System.out.println("Implicit Place search using SMT "+ (useStateEq?"with State Equation":"only with invariants") +" took "+ (System.currentTimeMillis() -t) +" ms to find "+implicitPlaces.size()+ " implicit places.");
 		}
 
-		if (reduced == 0 || iteration==0) {
+		if (rt != ReductionType.LIVENESS && (reduced == 0 || iteration==0)) {
 			List<Integer> tokill = DeadlockTester.testImplicitTransitionWithSMT(sr, solverPath);
 			if (! tokill.isEmpty()) {
 				System.out.println("Found "+tokill.size()+ " redundant transitions using SMT." );
