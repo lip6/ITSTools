6de38872cde23bfa15b505d371e70c0dbab8eb92
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-04-20 16:51:09 +0200

    catch stupid sizes for HLplace and throw OverlargeException
    lets us get 1229/1229 on OneSafe

--- pnml/fr.lip6.move.gal.pnml.togal/src/fr/lip6/move/gal/pnml/togal/OverlargeMarkingException.java
+++ pnml/fr.lip6.move.gal.pnml.togal/src/fr/lip6/move/gal/pnml/togal/OverlargeMarkingException.java
@@ -1,9 +1,7 @@
 package fr.lip6.move.gal.pnml.togal;
 
-import java.io.IOException;
-
 @SuppressWarnings("serial")
-public class OverlargeMarkingException extends IOException {
+public class OverlargeMarkingException extends RuntimeException {
 
 	public OverlargeMarkingException() {
 		super("Annotations (e.g. markings) use too many bits cannot handle transformation accurately.");
