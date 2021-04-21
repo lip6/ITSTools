a5f11c4e15b36abb9eb1f097ec2931ca377742ca
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-04-21 11:29:24 +0200

    make sure we never print two answers for the same query

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/MccDonePropertyPrinter.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/MccDonePropertyPrinter.java
@@ -7,14 +7,22 @@ public class MccDonePropertyPrinter extends ConcurrentHashDoneProperties {
 	
 	@Override
 	public Boolean put(String prop, Boolean value, String techniques) {
-		System.out.println("FORMULA "+prop+(value?" TRUE":" FALSE")+ " TECHNIQUES "+techniques);
-		return super.put(prop, value, techniques);
+		Boolean b = super.put(prop, value, techniques);
+		if (b == null) {
+			System.out.println("FORMULA "+prop+(value?" TRUE":" FALSE")+ " TECHNIQUES "+techniques);			
+		} else if (b != value) {
+			System.out.println("TestFail conflict detected : techniques "+techniques + " answered differently ("+value+ ")on formula " + prop);
+		}
+		return b;
 	}
 
 	
 	@Override
 	public Boolean put(String prop, Integer value, String techniques) {
-		System.out.println("FORMULA "+prop+" "+value+ " TECHNIQUES "+techniques);
-		return super.put(prop, value, techniques);
+		Boolean b = super.put(prop, value, techniques);
+		if (b == null) {
+			System.out.println("FORMULA "+prop+" "+value+ " TECHNIQUES "+techniques);
+		} 
+		return b;
 	}
 }
