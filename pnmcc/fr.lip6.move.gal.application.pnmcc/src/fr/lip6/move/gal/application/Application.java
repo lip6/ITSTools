8a0a421ad3452d4076022e527fc3a85a16d8d2f7
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-04-24 16:36:37 +0200

    make sure we don't lose tokens when going through simplifications

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/Application.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/Application.java
@@ -310,7 +310,11 @@ public class Application implements IApplication, Ender {
 				SparsePetriNet spn = reader.getSPN();
 				StructuralReduction sr = new StructuralReduction(spn);
 				ReachabilitySolver.applyReductions(sr,ReductionType.STATESPACE,solverPath,true,true);
+				int curtok = spn.getMarks().stream().mapToInt(i->i).sum();
+				int newtok = sr.getMarks().stream().mapToInt(i->i).sum();				
 				spn.readFrom(sr);
+				reader.setMissingTokens( (curtok-newtok) + reader.countMissingTokens());
+				
 			}
 			System.out.println("Final net has " + reader.getSPN().getPlaceCount() + " places and "
 					+ reader.getSPN().getTransitionCount() + " transitions.");
