f3ecff3d88be42a4557bbcf60ffec595258ed6f6
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2022-03-07 16:32:14 +0100

    transmit an SPN rather than a reader

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/solver/ltl/LTLPropertySolver2.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/solver/ltl/LTLPropertySolver2.java
@@ -90,7 +90,7 @@ public class LTLPropertySolver2 {
 			skel.getProperties().removeIf(p -> ! Simplifier.allEnablingsAreNegated(p.getBody()));
 			reader.setSpn(skel,true);
 			ReachabilitySolver.checkInInitial(reader.getSPN(), doneProps);
-			new AtomicReducerSR().strongReductions(solverPath, reader, doneProps);
+			new AtomicReducerSR().strongReductions(solverPath, reader.getSPN(), doneProps);
 			reader.getSPN().simplifyLogic();
 			ReachabilitySolver.checkInInitial(reader.getSPN(), doneProps);
 			reader.rebuildSpecification(doneProps);
@@ -134,7 +134,7 @@ public class LTLPropertySolver2 {
 		solved += ReachabilitySolver.checkInInitial(reader.getSPN(),doneProps);
 		solved += GALSolver.runGALReductions(reader, doneProps);
 		solved += ReachabilitySolver.checkInInitial(reader.getSPN(),doneProps);					
-		solved += new AtomicReducerSR().strongReductions(solverPath, reader, doneProps);
+		solved += new AtomicReducerSR().strongReductions(solverPath, reader.getSPN(), doneProps);
 		reader.getSPN().simplifyLogic();
 		solved += reader.getSPN().testInInitial();
 		solved += ReachabilitySolver.checkInInitial(reader.getSPN(),doneProps);
