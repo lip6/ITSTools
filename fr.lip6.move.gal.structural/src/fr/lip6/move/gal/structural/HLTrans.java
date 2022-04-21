d8b7ee1fbadddd084e0e8a8ad383d9a1832ce8cc
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2022-04-21 14:30:00 +0200

    make HLTrans into a public class

---
+++ fr.lip6.move.gal.structural/src/fr/lip6/move/gal/structural/HLTrans.java
@@ -0,0 +1,55 @@
+package fr.lip6.move.gal.structural;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.lip6.move.gal.structural.expr.Expression;
+import fr.lip6.move.gal.structural.expr.Param;
+
+public class HLTrans {
+	private String name;
+	private List<Param> params;
+	private Expression guard;
+	
+	private List<HLArc> pre= new ArrayList<>();
+	private List<HLArc> post=new ArrayList<>();
+	
+	public HLTrans(String name, List<Param> params, Expression guard) {
+		this.name = name;
+		this.params = params;
+		this.guard = guard;
+	}
+	@Override
+	public String toString() {
+		return "HLTrans [name=" + name + ", params=" + params + ", guard=" + guard + ", pre=" + pre + ", post=" + post
+				+ "]\n";
+	}
+	public String getName() {
+		return name;
+	}
+	public Expression getGuard() {
+		return guard;
+	}
+	public void setGuard(Expression guard) {
+		this.guard = guard;
+	}
+	public List<Param> getParams() {
+		return params;
+	}
+	public void setParams(List<Param> params) {
+		this.params = params;
+	}
+	public List<HLArc> getPre() {
+		return pre;
+	}
+	public void setPre(List<HLArc> pre) {
+		this.pre = pre;
+	}
+	public List<HLArc> getPost() {
+		return post;
+	}
+	public void setPost(List<HLArc> post) {
+		this.post = post;
+	}
+	
+}
