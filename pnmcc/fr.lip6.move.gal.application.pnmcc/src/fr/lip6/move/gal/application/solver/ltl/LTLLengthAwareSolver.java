63ba4b12bc7b75039b311868749f9fb0d995f341
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2022-10-27 15:50:58 +0200

    some refactoring to remove duplication + heavy commenting

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/solver/ltl/LTLLengthAwareSolver.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/solver/ltl/LTLLengthAwareSolver.java
@@ -14,12 +14,23 @@ import fr.lip6.move.gal.application.solver.ReachabilitySolver;
 import fr.lip6.move.gal.mcc.properties.ConcurrentHashDoneProperties;
 import fr.lip6.move.gal.mcc.properties.DoneProperties;
 import fr.lip6.move.gal.structural.GlobalPropertySolvedException;
+import fr.lip6.move.gal.structural.PetriNet;
 import fr.lip6.move.gal.structural.SparsePetriNet;
 import fr.lip6.move.gal.structural.StructuralReduction;
 import fr.lip6.move.gal.structural.StructuralReduction.ReductionType;
 import fr.lip6.move.gal.structural.expr.AtomicProp;
 import fr.lip6.move.gal.structural.expr.Expression;
 
+/**
+ * A solver realizing the FORTE'22 strategy to verify shortening or lengthening insensitive formulas (which are not both) using a reduction of the net.
+ * Using Spot it computes the sensitivity, if appropriate it then reduces the model with mode LI_LTL and analyzes the resulting model.
+ * When the polarity of the result is in the right direction, we can conclude. Otherwise we still need to look at full model.
+ * Based on :
+ * Emmanuel Paviot-Adet, Denis Poitrenaud, Etienne Renault, Yann Thierry-Mieg:
+ * LTL Under Reductions with Weaker Conditions Than Stutter Invariance. FORTE 2022: 170-187
+ * @author ythierry
+ *
+ */
 public class LTLLengthAwareSolver {
 
 	private static final int DEBUG = 0;
@@ -27,106 +38,132 @@ public class LTLLengthAwareSolver {
 	private String solverPath;
 	private String workDir;
 	private LTLPropertySolver ltlsolve;
-	
+
+	/**
+	 * Provide the context, and the LTLSolver instance used to try to solve properties on the reduced model.
+	 * @param spotPath to create a SpotRunner
+	 * @param solverPath for Z3
+	 * @param workDir where we are working
+	 * @param ltlsolve the solver we delegate the reduced model solution to.
+	 */
 	public LTLLengthAwareSolver(String spotPath, String solverPath, String workDir, LTLPropertySolver ltlsolve) {
 		this.spotPath = spotPath;
 		this.solverPath = solverPath;
 		this.workDir = workDir;
 		this.ltlsolve = ltlsolve;
 	}
-	
+
+	/**
+	 * Try to solve with FORTE'22 strategy.
+	 * @param reader
+	 * @param doneProps
+	 * @return
+	 * @throws TimeoutException
+	 * @throws LTLException
+	 */
 	public int runSLCLLTLTest(MccTranslator reader, DoneProperties doneProps)
-			throws TimeoutException, LTLException {		
+			throws TimeoutException, LTLException {
 		SpotRunner spot = new SpotRunner(spotPath, workDir, 10);
 
 		int solved =0;
 
+		// run on each property
 		for (fr.lip6.move.gal.structural.Property propPN : reader.getSPN().getProperties()) {
-			if (doneProps.containsKey(propPN.getName())) 
+
+			// some strategy running in parallel might have solved this one
+			if (doneProps.containsKey(propPN.getName()))
 				continue;
+
 			long time = System.currentTimeMillis();
 			if (DEBUG >= 1) System.out.println("Starting run for "+propPN.getName()+" :" + SpotRunner.printLTLProperty(propPN.getBody()));
+
+			// analyze sensitivity using spot + get a TGBA
 			TGBA tgba = spot.transformToTGBA(propPN);
 			try {
 				spot.analyzeCLSL(tgba);
 			} catch (IOException e) {
 				System.out.println("Warning : SL/CL computation failed.");
 			}
-			
+
 			if (! tgba.isStutterInvariant() && (tgba.isCLInvariant() || tgba.isSLInvariant())) {
-				// semi decision approach
-				
-				System.out.println("Found a " + (tgba.isSLInvariant() ? "SL":"CL") + " insensitive property : " + propPN.getName());
-				
+				// trigger semi decision approach
+
+				System.out.println("Found a " + (tgba.isSLInvariant() ? "Lengthening":"Shortening") + " insensitive property : " + propPN.getName());
+
 				// annotate it with Infinite Stutter Accepted Formulas
 				spot.computeInfStutter(tgba);
-			
-				// build a new copy of the model, with only this property				
-				List<AtomicProp> aps = tgba.getAPs();
-					
+
+				// build a new copy of the model
 				SparsePetriNet spn = new SparsePetriNet(reader.getSPN());
-				
-				// get rid of these, go for tgba
+
+				// get rid of other properties
 				spn.getProperties().clear();
 				spn.getProperties().add(propPN.copy());
-				
-				StructuralReduction sr = new StructuralReduction(spn);
-				
+
+				// compute supporting variables of the AP in the TGBA
 				BitSet support = new BitSet();
+				List<AtomicProp> aps = tgba.getAPs();
 				for (AtomicProp ap : aps) {
-					SparsePetriNet.addSupport(ap.getExpression(),support);
+					PetriNet.addSupport(ap.getExpression(),support);
 				}
-				
-				System.out.println("Support contains "+support.cardinality() + " out of " + sr.getPnames().size() + " places. Attempting structural reductions.");
 
+				System.out.println("Support contains "+support.cardinality() + " out of " + spn.getPnames().size() + " places. Attempting structural reductions.");
+
+				// run reduction rules in LengthInsensitive mode
+				StructuralReduction sr = new StructuralReduction(spn);
 				sr.setProtected(support);
-
 				try {
-					ReductionType rt = ReductionType.LI_LTL ; 
-					ReachabilitySolver.applyReductions(sr, rt, solverPath, true, true);			
+					ReductionType rt = ReductionType.LI_LTL ;
+					ReachabilitySolver.applyReductions(sr, rt, solverPath, true, true);
 				} catch (GlobalPropertySolvedException gse) {
 					System.out.println("Unexpected exception when reducing for LTL :" +gse.getMessage());
 					gse.printStackTrace();
 				}
-				
+
 				// rebuild and reinterpret the reduced net
-				// index of places may have changed, formula might be syntactically simpler 
-				// recompute fresh tgba with correctly indexed AP					
+				// index of places may have changed, formula might be syntactically simpler
+				// recompute fresh tgba with correctly indexed AP
 				List<Expression> atoms = aps.stream().map(ap -> ap.getExpression()).collect(Collectors.toList());
 				List<Expression> atoms2 = spn.readFrom(sr,atoms);
 				// we can maybe simplify some predicates now : apply some basic tests
 				spn.removeConstantPlaces(atoms2);
-				
+
 				for (int i =0,ie=atoms.size(); i<ie; i++) {
 					aps.get(i).setExpression(atoms2.get(i));
 				}
-				
-				if (doneProps.containsKey(propPN.getName())) 
+
+				// we might have solved the property syntactically thanks to reductions.
+				if (doneProps.containsKey(propPN.getName()))
 					continue;
-				
+
+				// run our usual decision procedures on the reduced model
+				// capture results in tmp rather than printing results immediately
 				DoneProperties tmpDoneProps = new ConcurrentHashDoneProperties();
 				ltlsolve.checkLTLProperty(spn.getProperties().get(0), tgba, spn, reader, tmpDoneProps , spot, time);
+
+				// could we solve the reduced model ?
 				if (tmpDoneProps.containsKey(propPN.getName())) {
 					boolean verdict = tmpDoneProps.getValue(propPN.getName());
+
+					// is it in the right polarity to be trusted ?
 					if (verdict && tgba.isCLInvariant()) {
 						// true formula + CL = real proof
-						doneProps.put(propPN.getName(), verdict, "CL_INSENSITIVE");
+						doneProps.put(propPN.getName(), verdict, "SHORTENING_INSENSITIVE");
 						solved++;
 					} else if (!verdict && tgba.isSLInvariant()) {
 						// false formula + SL = real proof
-						doneProps.put(propPN.getName(), verdict, "SL_INSENSITIVE");
+						doneProps.put(propPN.getName(), verdict, "LENGTHENING_INSENSITIVE");
 						solved++;
 					} else {
-						System.out.println("CL/SL decision was in the wrong direction : "+ (tgba.isSLInvariant() ? "SL":"CL") + " + " + verdict);					
+						System.out.println("Length sensitive decision was in the wrong direction : "+ (tgba.isSLInvariant() ? "Lengthening":"Shortening") + " insensitive + " + verdict);
 					}
 				}
 			}
+		} // foreach property
 
-			
-		}
 		return solved;
 	}
 
-	
-	
+
+
 }
--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/solver/ltl/LTLLengthSensitivityAnalyzer.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/move/gal/application/solver/ltl/LTLLengthSensitivityAnalyzer.java
@@ -8,17 +8,22 @@ import fr.lip6.ltl.tgba.TGBA;
 import fr.lip6.move.gal.application.mcc.MccTranslator;
 import fr.lip6.move.gal.application.runner.spot.SpotRunner;
 import fr.lip6.move.gal.application.solver.ReachabilitySolver;
-import fr.lip6.move.gal.mcc.properties.ConcurrentHashDoneProperties;
-import fr.lip6.move.gal.mcc.properties.PropertiesToPNML;
+import fr.lip6.move.gal.mcc.properties.MCCExporter;
 import fr.lip6.move.gal.structural.SparsePetriNet;
 import fr.lip6.move.gal.structural.StructuralReduction;
-import fr.lip6.move.gal.structural.StructuralToPNML;
 import fr.lip6.move.gal.structural.StructuralReduction.ReductionType;
 
+/**
+ * For each property of the model that is lengthening-insensitive XOR shortening-insensitive, build two versions of the model
+ * reduced with LTL and LI_LTL respectively.
+ * Also output some CSV stats on the reductions effectiveness.
+ * @author ythierry
+ *
+ */
 public class LTLLengthSensitivityAnalyzer {
 
 	/**
-	 * If property is short/length insensitive, but not both, build two model/property pairs, one reduced with SLCL_LTL, one reduced with LTL.	 * 
+	 * If property is short/length insensitive, but not both, build two model/property pairs, one reduced with SLCL_LTL, one reduced with LTL.	 *
 	 * Also produce some CSV stats. See FORTE'22 paper for more details.
 	 * @param reader the input model
 	 * @param spot to run spot scripts
@@ -34,25 +39,33 @@ public class LTLLengthSensitivityAnalyzer {
 		boolean isSI,isSh,isLen;
 		boolean reducedLTL=false;
 		boolean reducedSLCL=false;
-		
+
 		try {
+			// read the LTL in
 			reader.loadProperties(examination);
 
+			// unfold the net if colored, do very basic simplifications.
 			reader.createSPN();
 			SparsePetriNet spn = reader.getSPN();
+
+			// ok so this is our input size.
 			nbP=spn.getPlaceCount();
 			nbT=spn.getTransitionCount();
 
-			// with auto flush
+			// with auto flush, start building the stats file.
 			PrintWriter out = new PrintWriter(new FileOutputStream(pwd+"/"+examination+"stats.csv"),true);
 			out.println("model,examination,property id,init places,init trans,reduced LTL,out places,out trans,reduced short long,red places,red trans,time to red(ms),Stutter Ins,Short Ins,Leng Ins,analysis time(ms)");
-			
-			for (int propid = 0; propid < spn.getProperties().size() ; propid++) {
-				MccTranslator copy = reader.copy();
-				fr.lip6.move.gal.structural.Property prop = copy.getSPN().getProperties().get(propid);
-				copy.getSPN().getProperties().clear();
-				copy.getSPN().getProperties().add(prop);
 
+			// for each property in the input
+			for (int propid = 0; propid < spn.getProperties().size() ; propid++) {
+
+				// run on a clean copy with just the current property
+				SparsePetriNet spnProp = new SparsePetriNet(reader.getSPN());
+				fr.lip6.move.gal.structural.Property prop = spnProp.getProperties().get(propid);
+				spnProp.getProperties().clear();
+				spnProp.getProperties().add(prop);
+
+				// run sensitivity analysis and report the time it took
 				{
 					long time = System.currentTimeMillis();
 					TGBA tgba = spot.transformToTGBA(prop);
@@ -65,87 +78,64 @@ public class LTLLengthSensitivityAnalyzer {
 					isSh = tgba.isCLInvariant();
 					isLen = tgba.isSLInvariant();
 					analysisTime = System.currentTimeMillis() - time;
-					
 				}
-				
-				// try some property specific reductions
-				MccTranslator reader2 = reader.copy();
+
+				// Compute a model reduced for the current property only, in LTL mode.
 				{
-					SparsePetriNet spnProp = reader2.getSPN();
-					spnProp.getProperties().clear();
-					spnProp.getProperties().add(prop.copy());
 					StructuralReduction sr = new StructuralReduction (spnProp);
 					try {
+						// only apply LTL reduction rules, so no agglomerations and no prefix/suffix slicing in particular.
 						reducedLTL = ReachabilitySolver.applyReductions(sr, ReductionType.LTL, solverPath, true, true);
-						//sr.reduce(ReductionType.LTL);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
 					spnProp.readFrom(sr);
 					spnProp.simplifyLogic();
+
+					// so this might be a slightly smaller model than the original
+					// mostly due to redundant places/transition rules or symmetric choice rule.
 					nbP2=spnProp.getPlaceCount();
 					nbT2=spnProp.getTransitionCount();
 				}
 
+				// we only actually build the output files if it is relevant for length sensitivity experiment
 				if (!isSI &&  (isSh || isLen)) {
 					String outform = pwd + "/" + examination + "." + propid + ".xml";
-					try {
-						boolean usesConstants = PropertiesToPNML.transform(reader2.getSPN(), outform, new ConcurrentHashDoneProperties());
-						if (usesConstants) {
-							// we exported constants to a place with index = current place count
-							// to be consistent now add a trivially constant place with initial marking 1
-							// token
-							System.out.println("Added a place called one to the net.");
-							spn.addPlace("one", 1);
-						}
-						String outsr = pwd + "/model."+examination+"."+propid +".pnml";
-						StructuralToPNML.transform(reader2.getSPN(), outsr);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+					String outpnml = pwd + "/model."+examination+"."+propid +".pnml";
+					MCCExporter.exportToMCCFormat(outpnml, outform, spnProp);
 				}
-				// chain reductions
-				// reader2 = reader.copy();
+
+				// we chain the LI_LTL reductions on top of previous ones, all LTL preserving rules are of course LI_LTL compliant.
 				{
-					SparsePetriNet spnProp = reader2.getSPN();
-					
 					long time = System.currentTimeMillis();
 					StructuralReduction sr = new StructuralReduction (spnProp);
 					try {
 						reducedSLCL = ReachabilitySolver.applyReductions(sr, ReductionType.LI_LTL, solverPath, true, false);
-						// sr.reduce(ReductionType.SI_LTL);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
 					spnProp.readFrom(sr);
 					spnProp.simplifyLogic();
+
+					// this is the smallest model we could build under LI_LTL assumptions
 					nbP3=spnProp.getPlaceCount();
 					nbT3=spnProp.getTransitionCount();
 					redTime = System.currentTimeMillis() - time;
 				}
 
+				// again only output the reduced model if it is relevant.
 				if (!isSI &&  (isSh || isLen)) {
 					String outform = pwd + "/" + examination + "." + propid + ".red.xml";
-					try {
-						boolean usesConstants = PropertiesToPNML.transform(reader2.getSPN(), outform, new ConcurrentHashDoneProperties());
-						if (usesConstants) {
-							// we exported constants to a place with index = current place count
-							// to be consistent now add a trivially constant place with initial marking 1
-							// token
-							System.out.println("Added a place called one to the net.");
-							spn.addPlace("one", 1);
-						}
-						String outsr = pwd + "/model."+examination+"."+ propid +".red.pnml";
-						StructuralToPNML.transform(reader2.getSPN(), outsr);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+					String outsr = pwd + "/model."+examination+"."+ propid +".red.pnml";
+					MCCExporter.exportToMCCFormat(outsr, outform, spnProp);
 				}
 
 				String name = pwd.replaceAll(".*/", "");
+				// always print the stats line, regardless of whether we were short-ins XOR length-ins or not.
 				out.println(name+","+examination+","+propid+","+nbP+","+nbT+","+reducedLTL+","+nbP2+","+nbT2+","+reducedSLCL+","+nbP3+","+nbT3+","+redTime+","+(isSI?"1":"0")+","+(isSh?"1":"0")+","+(isLen?"1":"0")+","+analysisTime);
-				
+
 			}
+			// force to flush as well.
 			out.close();
 		} catch (IOException e1) {
 			e1.printStackTrace();
