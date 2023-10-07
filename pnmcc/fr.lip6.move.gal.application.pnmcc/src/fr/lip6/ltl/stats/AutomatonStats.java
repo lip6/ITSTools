ac60ed575c1542f0ac45f55eaa143c0c75ac0326
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-10-07 15:24:26 +0200

    failure state

--- pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/ltl/stats/AutomatonStats.java
+++ pnmcc/fr.lip6.move.gal.application.pnmcc/src/fr/lip6/ltl/stats/AutomatonStats.java
@@ -17,6 +17,8 @@ public class AutomatonStats {
 	private boolean weak;
 	private boolean terminal;
 	private boolean deterministic;
+	private long time=-1;
+	private boolean hasFailed;
 
 	// Default constructor
 	public AutomatonStats(String name,String type) {
@@ -26,7 +28,7 @@ public class AutomatonStats {
 
 	// Build the CSV header line
 	public static String buildHeaderLine() {
-		return "Name,Type,|AP|,|S|,|Edges|,|Formula|,|Acc|,SI,Universal,Empty,VeryWeak,Weak,Terminal,Deterministic";
+		return "Name,Type,AP,S,Edges,Formula,Acc,SI,Universal,Empty,VeryWeak,Weak,Terminal,Deterministic,Time";
 	}
 
 	public static String buildLegend() {
@@ -34,27 +36,32 @@ public class AutomatonStats {
 				+ "|-------------|--------------------------------------|\n"
 				+ "| Name        | Name of the automaton                |\n"
 				+ "| Type        | Nature of formula                    |\n"
-				+ "| |AP|        | Num of atomic propositions           |\n"
-				+ "| |S|         | Num of states                        |\n"
-				+ "| |Edges|     | Num of edges                         |\n"
-				+ "| |Formula|   | Syntactic complexity of formulas     |\n"
-				+ "| |Acc|       | Num of acceptance sets               |\n"
+				+ "| AP        	 | Num of atomic propositions           |\n"
+				+ "| S         	 | Num of states                        |\n"
+				+ "| Edges     	 | Num of edges                         |\n"
+				+ "| Formula	 | Syntactic complexity of formulas     |\n"
+				+ "| Acc	     | Num of acceptance sets               |\n"
 				+ "| SI          | 1 if stutter insensitive, 0 otherwise|\n"
 				+ "| Universal   | 1 if universal, 0 otherwise          |\n"
 				+ "| Empty       | 1 if empty, 0 otherwise              |\n"
 				+ "| VeryWeak    | 1 if very-weak, 0 otherwise          |\n"
 				+ "| Weak        | 1 if weak, 0 otherwise               |\n"
 				+ "| Terminal    | 1 if terminal, 0 otherwise           |\n"
-				+ "| Deterministic | 1 if deterministic, 0 otherwise    |";
+				+ "| Determinist | 1 if deterministic, 0 otherwise    |\n"
+		        + "| Time        | run time in ms                       |";
 	}
 
 	// Convert the object's data to a CSV line
 	@Override
 	public String toString() {
-		return name + "," + type + ","+ numAP + "," + numStates + "," + numEdges + "," + formulaComplexity + "," + numAccSets + ","
+		if (!hasFailed) {
+			return name + "," + type + ","+ numAP + "," + numStates + "," + numEdges + "," + formulaComplexity + "," + numAccSets + ","
 				+ (stutterInsensitive ? "1" : "0") + "," + (universal ? "1" : "0") + "," + (empty ? "1" : "0") + ","
 				+ (veryWeak ? "1" : "0") + "," + (weak ? "1" : "0") + "," + (terminal ? "1" : "0") + ","
-				+ (deterministic ? "1" : "0");
+				+ (deterministic ? "1" : "0") + "," + time;
+		} else {
+			return name + "," + type + ",-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1," + time;
+		}
 	}
 
 	public void setDeterministic(boolean deterministic) {
@@ -143,6 +150,14 @@ public class AutomatonStats {
 
 	public void setStutterInsensitive(boolean stutterInsensitive) {
 		this.stutterInsensitive = stutterInsensitive;
+	}
+
+	public void setTime(long l) {
+		this.time = l;
+	}
+
+	public void setFailed(boolean hasFailed) {
+		this.hasFailed = hasFailed;		
 	}
 
 }
