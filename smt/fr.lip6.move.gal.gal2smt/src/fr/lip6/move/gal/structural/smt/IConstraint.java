33341c604f8d174fb4739d6f312026ce859f4401
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-04-20 11:39:55 +0200

    it is a constraint we can add to the solver

---
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/IConstraint.java
@@ -0,0 +1,10 @@
+package fr.lip6.move.gal.structural.smt;
+
+import org.smtlib.ICommand;
+
+public interface IConstraint {
+	VarSet getSupport();
+	ICommand getSMTConstraint();
+	boolean isDone();
+	void setDone(boolean b);
+}
