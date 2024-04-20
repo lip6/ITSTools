bb506bed88888cb1bb0be5aecd3bdbaa055c2d4f
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-04-20 11:45:20 +0200

    a single basic constraint : assert or maybe minimize

---
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/SMTConstraint.java
@@ -0,0 +1,43 @@
+package fr.lip6.move.gal.structural.smt;
+
+import org.smtlib.ICommand;
+
+/** Represents a constraint we can directly feed the solver.
+ * Also keeps track of the variables it depends on.
+ * Typical examples include an "assert" commands, or maybe a minimize/maximize command. 
+ */
+public class SMTConstraint implements IConstraint {
+
+	private ICommand constraint;
+	private VarSet support;
+	private boolean done = false;
+	
+	public SMTConstraint(ICommand constraint, VarSet support) {
+		this.constraint = constraint;
+		this.support = support;
+	}
+	
+	public ICommand getSMTConstraint() {
+		return constraint;
+	}
+	public VarSet getSupport() {
+		return support;
+	}
+
+	@Override
+	public boolean isDone() {
+		return done ;
+	}
+
+	@Override
+	public void setDone(boolean b) {
+        done = b;
+    }
+
+	@Override
+	public String toString() {
+		return constraint.toString();
+	}
+	
+	
+}
