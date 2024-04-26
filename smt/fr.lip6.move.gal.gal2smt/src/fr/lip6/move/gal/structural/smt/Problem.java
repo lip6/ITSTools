100343069f324499a7234d7601d885a570ccfc2c
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-04-26 16:55:45 +0200

    support redefinition/refinement of a problem (once only for now)

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/Problem.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/Problem.java
@@ -1,5 +1,6 @@
 package fr.lip6.move.gal.structural.smt;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.smtlib.ICommand;
@@ -8,6 +9,7 @@ import org.smtlib.IExpr.IFactory;
 import org.smtlib.IExpr.ISymbol;
 import org.smtlib.IResponse;
 import org.smtlib.SMT;
+import org.smtlib.command.C_define_fun;
 
 import android.util.SparseBoolArray;
 import fr.lip6.move.gal.structural.expr.Expression;
@@ -19,6 +21,8 @@ public class Problem {
 	private Expression predicate;
 	private VarSet support;
 	private ICommand definition;
+	
+	private ISymbol refinedSymbol = null;
 	
 	private CandidateSolution solution = new CandidateSolution();
 
@@ -32,15 +36,32 @@ public class Problem {
 		support.addVars("s", s);
 
 		IExpr smt = predicate.accept(new ExprTranslator());
+		IFactory ef = new SMT().smtConfig.exprFactory;
+		symbol = ef.symbol(name);
+		definition = declareBoolFunc(symbol, smt);
+	}
+
+	public static ICommand declareBoolFunc(ISymbol name, IExpr body) {
 		SMT fac = new SMT();
 		IFactory ef = fac.smtConfig.exprFactory;
-		org.smtlib.ISort.IApplication bools = fac.smtConfig.sortFactory.createSortExpression(ef.symbol("Bool"));
-		this.symbol = ef.symbol(name);
-		definition = new org.smtlib.command.C_define_fun(symbol, // name
+		org.smtlib.ISort.IApplication bools = fac.smtConfig.sortFactory.createSortExpression(ef.symbol("Bool"));		
+		C_define_fun def = new org.smtlib.command.C_define_fun(name, // name
 				Collections.emptyList(), // parameters
 				bools, // return type
-				smt // body
+				body
 		);
+		return def;
+	}
+	
+	public void refineDefinition(IExpr additionalConstraint, SolverState solver) {
+		if (refinedSymbol != null) {
+			throw new IllegalStateException("Refinement already done.");
+		}
+		IFactory ef = new SMT().smtConfig.exprFactory;
+		this.refinedSymbol = ef.symbol(name + "_refined");
+		IExpr refinedBody = SMTUtils.makeAnd(Arrays.asList(symbol, additionalConstraint));
+		ICommand def = declareBoolFunc(refinedSymbol, refinedBody);
+		def.execute(solver.getSolver());
 	}
 	
 	public boolean declareProblem(SolverState solver) {
@@ -80,5 +101,15 @@ public class Problem {
 		return "(" + symbol + "=" + solution + ")";
 	}
 	
+	public String getName() {
+		return name;
+	}
 	
+	public Expression getPredicate() {
+		return predicate;
+	}
+	
+	public void dropRefinement() {
+		refinedSymbol = null;
+    }
 }
