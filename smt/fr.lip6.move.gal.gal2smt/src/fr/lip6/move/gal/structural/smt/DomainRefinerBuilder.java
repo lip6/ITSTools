781a23df135d5e954bfc321ba729b10cc7ad39d4
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-04-26 16:54:57 +0200

    slight API change (drop solver), slight trace changes

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/DomainRefinerBuilder.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/DomainRefinerBuilder.java
@@ -3,23 +3,23 @@ package fr.lip6.move.gal.structural.smt;
 import org.smtlib.ICommand;
 import org.smtlib.IExpr.IFactory;
 import org.smtlib.IExpr.ISymbol;
+import org.smtlib.SMT;
 import org.smtlib.command.C_assert;
 
 public class DomainRefinerBuilder {
 
-	public static StaticRefiner enforceMinBound(String prefix, int nbvars, int min, SolverState solver) {
-		return enforceBound(prefix, nbvars, min, true, solver);
+	public static StaticRefiner enforceMinBound(String prefix, int nbvars, int min) {
+		return enforceBound(prefix, nbvars, min, true);
 	}
 
-	public static StaticRefiner enforceMaxBound(String prefix, int nbvars, int max, SolverState solver) {
-		return enforceBound(prefix, nbvars, max, false, solver);
+	public static StaticRefiner enforceMaxBound(String prefix, int nbvars, int max) {
+		return enforceBound(prefix, nbvars, max, false);
 	}
 	
-	public static StaticRefiner enforceBound(String prefix, int nbvars, int bound, boolean isMin, SolverState solver) {
-		StaticRefiner doms = new StaticRefiner("Domain");
-		{
-			IFactory ef = solver.getSMT().smtConfig.exprFactory;
-
+	public static StaticRefiner enforceBound(String prefix, int nbvars, int bound, boolean isMin) {
+		IFactory ef =  new SMT().smtConfig.exprFactory;
+		StaticRefiner doms = new StaticRefiner("Domain("+prefix+")");
+		{			
 			for (int i = 0 ; i < nbvars ; i++) {
 				// t_i >= 0
 				ISymbol si = ef.symbol(prefix + i);
