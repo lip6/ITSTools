3a20832a934a56fc4d8dacd9dcd0d43836bc0879
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-05-03 18:02:14 +0200

    Change iteration/fixpoint logic, require >=1 rather than >0

--- smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/TrapRefiner.java
+++ smt/fr.lip6.move.gal.gal2smt/src/fr/lip6/move/gal/structural/smt/TrapRefiner.java
@@ -10,6 +10,7 @@ import org.smtlib.IExpr;
 import org.smtlib.IExpr.IFactory;
 import org.smtlib.command.C_assert;
 
+import android.util.SparseIntArray;
 import fr.lip6.move.gal.structural.ISparsePetriNet;
 
 public class TrapRefiner implements IRefiner {
@@ -26,40 +27,89 @@ public class TrapRefiner implements IRefiner {
         int totalConstraints =0;
         
         int constraintsAdded = 0;
-        do {
+        
+        boolean old=false;
+		if (old) {
+            do {
+            	constraintsAdded = 0;
+                // we need witness to find traps
+                problems.updateStatus(solver);
+                for (Problem problem : problems.getUnsolved()) {
+                	if (problem.getSolution().getReply() != SMTReply.SAT) {
+                		continue;
+                	}
+                	problem.updateStatus(solver, problems.getDoneProps());  
+        			if (problem.getSolution().getReply() != SMTReply.SAT) {
+        				continue;
+        			} else {
+        				problem.updateWitness(solver, "s");
+        			}
+        			if (solver.getNumericType() ==SolutionType.Real && problem.getSolution().getReply() == SMTReply.REAL) {
+        				continue;
+        			}
+                    CandidateSolution candidate = problem.getSolution();
+                    boolean newTrapFound = findTrap(net, candidate.getState(), knownTraps, solver);            
+                }
+                constraintsAdded += knownTraps.refine(solver, problems, mode, current, timeout);
+                totalConstraints += constraintsAdded;        	        	
+            } while (constraintsAdded > 0);
+        } else {
+        
+        
+        /*do*/ {
         	constraintsAdded = 0;
-            // we need witness to find traps
-            problems.updateStatus(solver, true);
-            for (Problem problem : problems.getUnsolved()) {
-            	if (problem.getSolution().getReply() != SMTReply.SAT) {
-            		continue;
-            	}
-                CandidateSolution candidate = problem.getSolution();
-                boolean newTrapFound = findTrap(net, candidate, knownTraps, solver);            
-            }
             constraintsAdded += knownTraps.refine(solver, problems, mode, current, timeout);
-            totalConstraints += constraintsAdded;        	        	
-        } while (constraintsAdded > 0);
-    	
+            if (constraintsAdded > 0) {
+            	totalConstraints += constraintsAdded;
+            	return totalConstraints;
+            }
+        	for (Problem problem : problems.getUnsolved()) {
+        		while (true) {
+        			// we need witness to find traps
+        			if (problem.getSolution().getReply() != SMTReply.SAT) {
+        				break;
+        			}
+        			problem.updateStatus(solver, problems.getDoneProps());  
+        			if (problem.getSolution().getReply() != SMTReply.SAT) {
+        				break;
+        			} else {
+        				problem.updateWitness(solver, "s");
+        			}
+        			
+        			CandidateSolution candidate = problem.getSolution();
+        			boolean newTrapFound = findTrap(net, candidate.getState(), knownTraps, solver);
+        			if (newTrapFound) {
+        				int added = knownTraps.refine(solver, problems, mode, current, timeout);
+        				totalConstraints += added;
+        				constraintsAdded += added;
+        			} else {
+        				break;
+        			}
+        		}
+            }
+            problems.update();
+        } // while (constraintsAdded > 0);
+        }
+        
         return totalConstraints;
     }
 
-    private boolean findTrap(ISparsePetriNet net, CandidateSolution candidate, StaticRefiner known, SolverState solver) {
+    private boolean findTrap(ISparsePetriNet net, SparseIntArray state, StaticRefiner known, SolverState solver) {
         boolean trapFound = false;
         
-		if (! candidate.hasWitness()) {
+		if (state == null || state.size() == 0) {
 			return false;
 		}
         
 		// SparseBoolArray all = new SparseBoolArray(net.getPlaceCount(),true);
         // List<Integer> trap = SMTTrapUtils.testTrapWithSMT(net, candidate.getState(), all ) ;
-        List<Integer> trap = SMTTrapUtils.testTrapWithSMT(net, candidate.getState(), solver.getDeclaredVars().getVars().get("s"));
+        List<Integer> trap = SMTTrapUtils.testTrapWithSMT(net, state, solver.getDeclaredVars().getVars().get("s"));
         
         if (!trap.isEmpty()) {
             IFactory ef = solver.getSMT().smtConfig.exprFactory;
             List<IExpr> vars = trap.stream().map(n -> ef.symbol("s"+n)).collect(Collectors.toList());
             IExpr sum = buildSum(vars);
-            ICommand constraint = new C_assert(ef.fcn(ef.symbol(">"), sum , ef.numeral(0)));
+            ICommand constraint = new C_assert(ef.fcn(ef.symbol(">="), sum , ef.numeral(1)));
             VarSet support = new VarSet();
             for (Integer p : trap) {
             	support.addVar("s", p);
