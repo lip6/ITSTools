89a92fd8e359e3a1e22b2219a4804dfbd5949893
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2025-04-30 14:38:33 +0200

    avoid allocations in loop

--- fr.lip6.move.gal.structural/src/android/util/ContainerHelpers.java
+++ fr.lip6.move.gal.structural/src/android/util/ContainerHelpers.java
@@ -49,9 +49,11 @@ class ContainerHelpers {
 	}
 	// This is Arrays.binarySearch(), but doesn't do any argument validation.
     static int binarySearch(int[] array, int value, int lo, int hi) {
-        while (lo <= hi) {
-            final int mid = (lo + hi) >>> 1;
-            final int midVal = array[mid];
+    	int mid;
+    	int midVal;
+    	while (lo <= hi) {
+            mid = (lo + hi) >>> 1;
+            midVal = array[mid];
             if (midVal < value) {
                 lo = mid + 1;
             } else if (midVal > value) {
@@ -65,9 +67,11 @@ class ContainerHelpers {
     static int binarySearch(long[] array, int size, long value) {
         int lo = 0;
         int hi = size - 1;
+        int mid;
+        long midVal;
         while (lo <= hi) {
-            final int mid = (lo + hi) >>> 1;
-            final long midVal = array[mid];
+            mid = (lo + hi) >>> 1;
+            midVal = array[mid];
             if (midVal < value) {
                 lo = mid + 1;
             } else if (midVal > value) {
